b60ab489b620e6edcc5ac9e713f2d10a4860bf87
Michael Aivazis <michael.aivazis@orthologue.com>
2010-03-11 14:32:15 -0800

    Laid out the directory structure

---
+++ packages/Make.mm
@@ -0,0 +1,42 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2010 all rights reserved
+#
+
+
+PROJECT = pyre
+
+RECURSE_DIRS = \
+    $(PACKAGES)
+
+PACKAGES = \
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+#--------------------------------------------------------------------------
+#
+
+PYRE_ZIP = $(EXPORT_ROOT)/pyre-${PYRE_VERSION}.zip
+
+zip:
+	for package in $(PACKAGES); do { \
+	    ( cd $${package}; zip -r ${PYRE_ZIP} $${package}; ) \
+	} done
+
+
+# end of file 
---
+++ tests/unit/Make.mm
@@ -0,0 +1,29 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2010 all rights reserved
+#
+
+
+PROJECT = pyre
+
+RECURSE_DIRS = \
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+# end of file 
