b5bfd4a1840211df807b8ccd1f1925f331d880cf
Michael Aivazis <michael.aivazis@orthologue.com>
2010-03-15 13:55:46 -0700

    Added copyright note and license  to the runtime

--- packages/pyre/__init__.py
+++ packages/pyre/__init__.py
@@ -5,4 +5,76 @@
 # (c) 1998-2010 all rights reserved
 #
 
+"""
+pyre is a framework for building flexible applications
+
+For more details, see http://pyre.caltech.edu.
+For terms of use, see pyre.license()
+"""
+
+
+def copyright():
+    """
+    Return the pyre copyright note
+    """
+    return _pyre_copyright
+
+
+def license():
+    """
+    Return the pyre license
+    """
+    print(_pyre_license)
+    return
+
+
+def version():
+    """
+    Return the pyre version
+    """
+    return _pyre_version
+
+
+# license
+_pyre_version = "1.0"
+
+_pyre_copyright = "pyre: Copyright (c) 1998-2010 Michael A.G. Aïvázis"
+
+_pyre_license = """
+    pyre 1.0
+    Copyright (c) 1998-2010 Michael A.G. Aïvázis
+    All Rights Reserved
+
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the
+      distribution.
+
+    * Neither the name pyre nor the names of its contributors may be
+      used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+    """
+
+
 # end of file 
