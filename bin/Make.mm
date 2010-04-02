031fe3dd16d0a8d1d294c54c2d241648c060dc8c
Michael Aivazis <michael.aivazis@orthologue.com>
2010-04-01 18:08:39 -0700

    bin: added listdir

---
+++ bin/Make.mm
@@ -0,0 +1,18 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2010 all rights reserved
+#
+
+
+PROJECT = pyre
+PACKAGE = bin
+
+#--------------------------------------------------------------------------
+#
+
+all: tidy
+
+
+# end of file 
---
+++ bin/listdir.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2010 all rights reserved
+#
+
+
+"""
+Print the contents of the filesystem rooted at the current directory
+"""
+
+
+def listdir():
+    import os
+    import pyre.filesystem
+
+    finder = pyre.filesystem.newFinder()
+    cwd = pyre.filesystem.newLocalFilesystem(root=".")
+
+    me = os.path.normpath(__file__)
+    for node,path in finder.explore(cwd):
+        if path == me:
+            continue
+        print(path)
+
+    return cwd
+
+
+# main
+if __name__ == "__main__":
+    listdir()
+
+
+# end of file 
