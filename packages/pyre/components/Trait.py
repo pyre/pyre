f00c61ef410517e39f36c5384bbda24b5d2ad1e9
Michael Aivazis <michael.aivazis@orthologue.com>
2010-05-03 10:20:20 -0400

    pyre.components: added the class documentation for Trait

--- packages/pyre/components/Trait.py
+++ packages/pyre/components/Trait.py
@@ -11,6 +11,18 @@ from ..patterns.Named import Named
 
 class Trait(Named):
     """
+    This is the base class for component features that form their public interface
+
+    Traits extend the notion of a class attribute to an object that is capabable of
+    capturing information about the attribute that has no natural resting place as part of a
+    normal class declaration.
+
+    Traits enable end-user configurable state, for both simple attributes and references to
+    more elaborate objects, such as other components. Individual inventory items need a name
+    that enables access to the associated information, per-instance actual storage for the
+    attribute value, and additional meta data, such as reasonable default values when the
+    attribute is not explictly given a value during configuration, and the set of constraints
+    it should satisfy before it is considered a legal value.
     """
 
 
