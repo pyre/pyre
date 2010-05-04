de47aa34965d5d62182ce2946ec977e49deb0d17
Michael Aivazis <michael.aivazis@orthologue.com>
2010-05-03 17:47:08 -0700

    pyre.config: removed a supefluous import and some wrong comments from Variable

--- packages/pyre/config/Variable.py
+++ packages/pyre/config/Variable.py
@@ -5,18 +5,13 @@
 # (c) 1998-2010 all rights reserved
 #
 
+
 from ..calc.Node import Node
-from ..calc.Expression import Expression
 
 
 class Variable(Node):
     """
     The object used to hold the values of all configurable items
-
-    All evaluator nodes are expressions. Their values are gathered from configuration files and
-    the command line as strings and they are later converted into their target type by the
-    binding process. They are stored as expressions to enable arithmentic, access to builtin
-    functions and references to other variables
     """
 
 
