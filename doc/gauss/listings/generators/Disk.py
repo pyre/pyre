e4385b84edd9e6908aeaf4e5c7ac1332d1862b27
Michael Aivazis <michael.aivazis@orthologue.com>
2010-05-17 09:56:39 -0400

    doc/gauss: finished pulling the listings from external files

--- doc/gauss/listings/generators/Disk.py
+++ doc/gauss/listings/generators/Disk.py
@@ -21,13 +21,14 @@ class Disk(Shape):
         r2 = self.radius**2
         x0, y0 = self.center
 
-        for x,y in points:
+        for x,y in points: #@\label{line:circle:generators:loop}@
             dx = x - x0
             dy = y - y0
             if dx*dx + dy*dy <= r2:
-                yield 1
+                yield 1 #@\label{line:circle:generators:true}@
 
-        return
+        return #@\label{line:circle:generators:return}@
+
 
     # meta methods
     def __init__(self, radius=1.0, center=(0.0, 0.0)):
