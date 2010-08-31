8c3448d3ac02dd9be12f97bc232244f2b3801df6
Michael Aivazis <michael.aivazis@orthologue.com>
2010-08-30 19:48:06 -0700

    pyre.tracking: added Tracker method descriptions

--- packages/pyre/tracking/Tracker.py
+++ packages/pyre/tracking/Tracker.py
@@ -16,10 +16,16 @@ class Tracker:
 
 
     def getHistory(self, key):
+        """
+        Retrieve the historical record associated with a particular {key}
+        """
         return self.log[key]
 
 
     def track(self, key, value):
+        """
+        Add {value} to the history of {key}
+        """
         self.log[key].append(value)
         return
 
@@ -30,7 +36,6 @@ class Tracker:
         self.log = collections.defaultdict(list)
 
         return
-
 
 
 # end of file 
