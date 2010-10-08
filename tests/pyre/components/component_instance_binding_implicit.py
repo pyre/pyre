047a11d84e44bf0fe576dd5d6482c3a74aab5b9e
Michael Aivazis <michael.aivazis@orthologue.com>
2010-10-08 11:04:21 -0700

    pyre.components: added a test for implicit binding of component instances

---
+++ tests/pyre/components/component_instance_binding_implicit.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2010 all rights reserved
+#
+
+
+"""
+Verify that facilities get bound correctly when specified implicitly
+"""
+
+
+def test():
+    import pyre
+
+    # declare an interface
+    class job(pyre.interface):
+        """an interface"""
+        @pyre.provides
+        def do(self):
+            """do something"""
+
+    # declare a component
+    class component(pyre.component):
+        """a component"""
+        task = pyre.facility(interface=job, default="import://sample#worker")
+
+    # instantiate
+    c = component(name="c")
+    # check
+    assert isinstance(c.task, pyre.component)
+    assert c.task.pyre_name == "c.task"
+
+    return c, component, job
+
+
+# main
+if __name__ == "__main__":
+    test()
+
+
+# end of file 
