2db9ae157c45734c74e4322b6c514e86e472283e
Michael Aivazis <michael.aivazis@orthologue.com>
2011-01-30 17:42:18 -0800

    pyrepg: added some checks for the low level exception object

--- tests/pyre/db/pyrepg-exceptions.py
+++ tests/pyre/db/pyrepg-exceptions.py
@@ -8,13 +8,20 @@
 
 
 """
-Sanity check: verify that the module is accessible
+Sanity check: verify that the exceptions defined in the module are accessible
 """
 
 
 def test():
     import pyre.db.pyrepg
 
+    # make sure the exception object is accessible
+    error = pyre.db.pyrepg.Error
+    # make sure it is decorated correctly
+    assert error.__name__ == 'Error'
+    assert error.__module__ == 'pyrepg'
+    assert error.__bases__ == (Exception,)
+    # verify it can be caught
     try:
         raise pyre.db.pyrepg.Error
     except pyre.db.pyrepg.Error as error:
