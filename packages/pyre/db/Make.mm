237af2cff6e315237c7ce7ea92b3d33a975d583b
Michael Aivazis <michael.aivazis@orthologue.com>
2011-01-31 19:41:17 -0800

    pyre.db: laid out the DB API 2.0 exception hierarchy

--- packages/pyre/db/Make.mm
+++ packages/pyre/db/Make.mm
@@ -19,6 +19,7 @@ all: export
 # export
 
 EXPORT_PYTHON_MODULES = \
+    exceptions.py \
     __init__.py
 
 
---
+++ packages/pyre/db/exceptions.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2011 all rights reserved
+#
+
+
+"""
+Definitions for all the exceptions raised by this package
+"""
+
+from ..framework.exceptions import FrameworkError
+
+
+# db api 2.0 compliant exception hierarchy
+# not my first choice for a classification strategy, but there you go...
+
+class Warning(FrameworkError):
+    """
+    Exception raised for important warnings, such as data truncation, loss of precision and
+    other idications that the implementation engines have carried out a request in a perhaps
+    incorrect way
+    """
+
+
+class Error(FrameworkError):
+    """
+    Base class for all exceptions that are raised by this module to indicate an unrecoverable
+    error
+    """
+
+
+class InterfaceError(Error):
+    """
+    Base class for exceptions raised by the database client code, not the database back end
+    """
+
+
+class DatabaseError(Error):
+    """
+    Base class for exceptions raised by the database back end
+    """
+
+
+class DataError(DatabaseError):
+    """
+    Exception raised when a data processing error occurs
+    """
+
+
+class OperationalError(DatabaseError):
+    """
+    An exception that indicates environmental problems that are generally not related to the
+    program itself
+    """
+
+
+class IntegrityError(DatabaseError):
+    """
+    Exception raised when an operation violates the referential integrity of the data store
+    """
+
+
+class InternalError(DatabaseError):
+    """
+    Exception raised when the back end reports an internal error
+    """
+    
+
+class ProgrammingError(DatabaseError):
+    """
+    Exception raised when there is a problem with the SQL statement being executed
+    """
+    
+
+class NotSupportedError(DatabaseError):
+    """
+    Exception raised when a method or database API was used that is not supported by the
+    database client
+    """
+
+
+# end of file 
