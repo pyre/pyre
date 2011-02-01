27194469cdc1231b5f327b0461da237c5788931f
Michael Aivazis <michael.aivazis@orthologue.com>
2011-01-31 20:51:38 -0800

    pyrepg: the test now checks the entire exception hierarchy

--- tests/pyre/postgres/pyrepg-exceptions.py
+++ tests/pyre/postgres/pyrepg-exceptions.py
@@ -13,19 +13,127 @@ Sanity check: verify that the exceptions defined in the module are accessible
 
 
 def test():
-    import pyre.postgres.pyrepg
+    from pyre.postgres import pyrepg
     from pyre.framework.exceptions import FrameworkError
 
     # make sure the exception object is accessible
-    error = pyre.postgres.pyrepg.Error
+    warning = pyrepg.Warning
+    # make sure it is decorated correctly
+    assert warning.__name__ == 'Warning'
+    assert warning.__module__ == 'pyre.db.exceptions'
+    assert warning.__bases__ == (FrameworkError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.Warning('a generic database warning')
+    except pyrepg.Warning as warning:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.Error
     # make sure it is decorated correctly
     assert error.__name__ == 'Error'
     assert error.__module__ == 'pyre.db.exceptions'
     assert error.__bases__ == (FrameworkError,)
     # verify it can be caught
     try:
-        raise pyre.postgres.pyrepg.Error('a generic database error')
-    except pyre.postgres.pyrepg.Error as error:
+        raise pyrepg.Error('a generic database error')
+    except pyrepg.Error as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.InterfaceError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'InterfaceError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.Error,)
+    # verify it can be caught
+    try:
+        raise pyrepg.InterfaceError('a generic database error')
+    except pyrepg.InterfaceError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.DatabaseError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'DatabaseError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.Error,)
+    # verify it can be caught
+    try:
+        raise pyrepg.DatabaseError('a generic database error')
+    except pyrepg.DatabaseError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.DataError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'DataError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.DataError('a generic database error')
+    except pyrepg.DataError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.OperationalError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'OperationalError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.OperationalError('a generic database error')
+    except pyrepg.OperationalError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.IntegrityError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'IntegrityError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.IntegrityError('a generic database error')
+    except pyrepg.IntegrityError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.InternalError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'InternalError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.InternalError('a generic database error')
+    except pyrepg.InternalError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.ProgrammingError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'ProgrammingError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.ProgrammingError('a generic database error')
+    except pyrepg.ProgrammingError as error:
+        pass
+
+    # make sure the exception object is accessible
+    error = pyrepg.NotSupportedError
+    # make sure it is decorated correctly
+    assert error.__name__ == 'NotSupportedError'
+    assert error.__module__ == 'pyre.db.exceptions'
+    assert error.__bases__ == (pyrepg.DatabaseError,)
+    # verify it can be caught
+    try:
+        raise pyrepg.NotSupportedError('a generic database error')
+    except pyrepg.NotSupportedError as error:
         pass
 
     return 
