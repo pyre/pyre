ea73acdc2e8613fed986834b34ea9b28a4bfe739
Michael Aivazis <michael.aivazis@orthologue.com>
2011-02-14 09:05:24 -0800

    repaired the date line on the comment headers

--- doc/gauss/diagrams/Make.mm
+++ doc/gauss/diagrams/Make.mm
@@ -1,13 +1,9 @@
 # -*- Makefile -*-
 # -*- coding: utf-8 -*-
 #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-#                             michael a.g. aïvázis
-#                      california institute of technology
-#                      (c) 1998-2011  all rights reserved
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2011 all rights reserved
 #
 
 PROJECT = pyre
