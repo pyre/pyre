cf24bf9be91a0e9ea147f9350557fce47ad57d7c
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-02-26 10:19:22 +0200

    libpyre: added the initial version of the algebra package

---
+++ lib/algebra/Make.mm
@@ -0,0 +1,24 @@
+# -*- Makefile -*-
+# -*- coding: utf-8 -*-
+#
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2011 all rights reserved
+#
+
+PROJECT = pyre
+PACKAGE = algebra
+
+PROJ_TMPDIR = $(BLD_TMPDIR)/$(PROJECT)/$(PACKAGE)
+
+#
+all: export
+
+export:: export-package-headers
+
+EXPORT_HEADERS = \
+    operators.h operators.icc \
+    BCD.h BCD.icc
+
+# end of file 
