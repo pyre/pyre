16afb3618d50907c4ee4601d093315d26a863da7
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-03-04 13:55:51 -0800

    doc: added the skeleton of a presentation

---
+++ doc/overview/Make.mm
@@ -0,0 +1,68 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2011 all rights reserved
+#
+
+PROJECT = pyre
+PACKAGE = overview
+
+RECURSE_DIRS = \
+   figures \
+   listings \
+
+OTHERS = \
+
+#--------------------------------------------------------------------------
+#
+
+DOCUMENT = overview
+
+PACKAGES =
+
+INCLUDES = \
+    pyre.sty \
+    setup.tex \
+    references.bib
+
+SECTIONS = \
+    sec-components.tex
+
+FIGURES = \
+    figures/pyre-logo.pdf
+
+#--------------------------------------------------------------------------
+#
+
+all: $(DOCUMENT).pdf
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+#--------------------------------------------------------------------------
+#
+
+# preview types
+osx: $(DOCUMENT).pdf
+	open $(DOCUMENT).pdf
+
+xpdf: $(DOCUMENT).pdf
+	xpdf -remote $(DOCUMENT) $(DOCUMENT).pdf
+
+# make the document using the default document class
+$(DOCUMENT).pdf: $(DOCUMENT).tex $(PACKAGES) $(INCLUDES) $(SECTIONS) $(FIGURES)
+
+# housekeeping
+PROJ_CLEAN = $(CLEAN_LATEX) *.snm *.nav *.vrb 
+PROJ_DISTCLEAN = *.ps *.pdf $(PROJ_CLEAN)
+
+# end of file
