39f60f48098c69dfc7b826e7f059e711e3efeb77
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-05-04 17:15:13 -0700

    lib/journal: simplified the {diagnostic-injection} test

--- tests/libpyre/journal/diagnostic-injection.cc
+++ tests/libpyre/journal/diagnostic-injection.cc
@@ -26,64 +26,14 @@
 class Debug : public pyre::journal::Diagnostic<Debug> {
 public:
     bool isActive() const { return true; }
-
-    Debug & record() {
-        std::cout << "    endl" << std::endl;
-        return *this;
-    }
-
-    Debug & newline() {
-        std::cout << "    newline" << std::endl;
-        return *this;
-    }
-
-    Debug & print(const char * text) {
-        std::cout << "    " << text << std::endl;
-        return *this;
-    }
-
-public:
-    ~Debug() {
-        std::cout << " -- destroying a diagnostic" << std::endl;
-    }
-
-    Debug() :
-        pyre::journal::Diagnostic<Debug>::Diagnostic()
-    {
-        std::cout << " ++ creating a diagnostic" << std::endl;
-    }
-
-    Debug(const Debug & other) :
-        pyre::journal::Diagnostic<Debug>::Diagnostic(other)
-    {
-        std::cout << " ** copying a diagnostic" << std::endl;
-    }
-
-    Debug &
-    operator = (const Debug & other)
-    {
-        std::cout << " ** assigning a diagnostic" << std::endl;
-        // skip assignment to self
-        if (&other != this) {
-            // otherwise, copy the data
-            pyre::journal::Diagnostic<Debug>::operator=(other);
-        }
-        // and return me
-        return *this;
-    }
-
-
 };
-
-
-typedef pyre::journal::Diagnostic<Debug> diagnostic_t;
 
 
 // main program
 int main() {
 
     // instantiate
-    diagnostic_t d;
+    Debug d;
    
     // inject
     d 
