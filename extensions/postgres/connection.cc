982508ce0428470df3ec20084ac99084a77012ff
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-06-10 18:32:37 -0700

    postgres: added journaling to the connection start up and shut down

--- extensions/postgres/connection.cc
+++ extensions/postgres/connection.cc
@@ -10,6 +10,7 @@
 
 #include <Python.h>
 #include <libpq-fe.h>
+#include <pyre/journal.h>
 
 #include "constants.h"
 #include "connection.h"
@@ -42,6 +43,13 @@ connect(PyObject *, PyObject * args) {
     if (!PyArg_ParseTuple(args, "s:connect", &specification)) {
         return 0;
     }
+
+    // in case someone is listening...
+    pyre::journal::debug_t debug("postgres.connection");
+    debug 
+        << pyre::journal::at(__HERE__)
+        << "connecting with specification: '" << specification << "'"
+        << pyre::journal::endl;
 
     // establish a connection
     PGconn * connection = PQconnectdb(specification);
@@ -87,6 +95,13 @@ disconnect(PyObject *, PyObject * args) {
     // and remove the destructor
     PyCapsule_SetDestructor(connection, 0);
 
+    // in case someone is listening...
+    pyre::journal::debug_t debug("postgres.connection");
+    debug 
+        << pyre::journal::at(__HERE__)
+        << "disconnected" 
+        << pyre::journal::endl;
+
     // all done
     Py_INCREF(Py_None);
     return Py_None;
@@ -104,6 +119,14 @@ finish(PyObject * capsule) {
     // get pointer from the capsule and cast it to a pg connection
     PGconn * connection =
         static_cast<PGconn *>(PyCapsule_GetPointer(capsule, connectionCapsuleName));
+
+    // in case someone is listening...
+    pyre::journal::debug_t debug("postgres.connection");
+    debug 
+        << pyre::journal::at(__HERE__)
+        << "closing the connection to the server" 
+        << pyre::journal::endl;
+
     // shutdown 
     PQfinish(connection);
     // all done
