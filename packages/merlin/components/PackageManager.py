9a6a6b1e7c1cd5cffa1c864c9a100ce179e9108c
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-06-12 21:26:19 -0700

    merlin: the package manager now knows the local hostname; started fleshing out package retrieval

--- packages/merlin/components/PackageManager.py
+++ packages/merlin/components/PackageManager.py
@@ -17,4 +17,44 @@ class PackageManager(pyre.component, family="merlin.packages"):
     """
 
 
+    # properties
+    hostname = pyre.properties.str()
+    hostname.doc = "the name of this machine"
+
+
+    # interface
+    def find(self, uri):
+        """
+        Locate the package using {uri} as its address
+        """
+        executive = self.pyre_executive
+        # ask the executive to locate the spell factory
+        shelf = executive.retrieveComponentDescriptor(uri=uri, context=self)
+        print(shelf)
+
+
+
+    def shelves(self, name, folder):
+        """
+        Iterate over the contents of {folder} and return candidate shelves
+        """
+        # go through the entire contents
+        for shelf in folder.contents:
+            # everybody is a candidate, for now
+            yield folder.join(name, shelf)
+        # all done
+        return
+
+
+    # meta methods
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+
+        # figure out the hostname
+        if not self.hostname:
+            import socket
+            self.hostname = socket.gethostbyaddr(socket.gethostname())[0]
+
+        return
+
 # end of file 
