3bf12ac51ffd0edc16208e3284406ccec13a98a5
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-07-16 12:44:35 -0700

    setup.sh: took ownership of more of the build system setup

--- setup.sh
+++ setup.sh
@@ -8,29 +8,36 @@
 # set up a branch-aware build environment
 
 # get the branch nickname
-  nickname=$(bzr nick)
-  echo "setting up development for '${nickname}'"
+nickname=$(bzr nick)
+echo "setting up development for '${nickname}'"
 
-# the source directory
-  export DV_DIR=${HOME}/dv
-  export BLD_CONFIG=${DV_DIR}/config
-  export BLD_ROOT=${DV_DIR}/builds/${nickname}
-  echo "    building in '${BLD_ROOT}'"
-  # create the directory if it doesn't already exist
-  if [ ! -d ${BLD_ROOT} ]; then
-      echo "    creating '${BLD_ROOT}'"
-      mkdir -p ${BLD_ROOT}
-  fi
+# set up the build system
+export DV_DIR=${HOME}/dv # the source directory
+export BLD_CONFIG=${DV_DIR}/config # the home of config
+export GNU_MAKE=make # use gnu make
+export MM_STOP=1 # stop on all errors
+export LOGTAGS=on # turn mm logtags on
+export TARGET=shared,debug,mpi # the default build target
+
+# redirect the build temporary files 
+export BLD_ROOT=${DV_DIR}/builds/${nickname}
+echo "    building in '${BLD_ROOT}'"
+
+# create the directory if it doesn't already exist
+if [ ! -d ${BLD_ROOT} ]; then
+    echo "    creating '${BLD_ROOT}'"
+    mkdir -p ${BLD_ROOT}
+fi
 
 # the installation directory
-  export PYRE_DIR=${USER_TOOLS_DIR}/${nickname}
-  export EXPORT_ROOT=${PYRE_DIR}
-  echo "    deploying at '${EXPORT_ROOT}'"
-  export PYTHONPATH=${EXPORT_ROOT}
+export EXPORT_ROOT=${USER_TOOLS_DIR}/${nickname}
+echo "    deploying at '${EXPORT_ROOT}'"
 
+# adjust the python path
+export PYTHONPATH=${EXPORT_ROOT}
 # adjust the path
-  export PATH=${BLD_CONFIG}/make:${PYRE_DIR}/bin:${PATH}
+export PATH=${EXPORT_ROOT}/bin:${BLD_CONFIG}/make:${PATH}
 # and the location of the shared objects
-  export LD_LIBRARY_PATH=${EXPORT_ROOT}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${EXPORT_ROOT}/lib:${LD_LIBRARY_PATH}
 
 # end of file
