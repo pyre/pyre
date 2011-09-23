120d5d4849e2a4721bed34e387797e760cfbb13f
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-09-22 17:02:04 -0700

    pyre.calc: updated the node traversal test cases

--- tests/pyre/calc/hierarchical_group.py
+++ tests/pyre/calc/hierarchical_group.py
@@ -22,7 +22,7 @@ def test():
     model["pyre.user.name"] = "Michael Aïvázis"
     model["pyre.user.email"] = "aivazis@caltech.edu"
     model["pyre.user.affiliation"] = "California Institute of Technology"
-    model["pyre.user.signature"] = "{user.name}+' -- '+{user.email}"
+    model["pyre.user.signature"] = "{pyre.user.name}+' -- '+{pyre.user.email}"
     model["pyre.user.telephone"] = "+1 626.395.3424"
 
     # and some aliases
@@ -30,23 +30,25 @@ def test():
     model.alias(alias="χρήστης.όνομα", canonical="pyre.user.name")
 
     # here are the canonical names
-    names = { "name", "email", "affiliation", "signature", "telephone" }
+    names = { 
+        "pyre.user." + tag
+        for tag in ("name", "email", "affiliation", "signature", "telephone") }
 
     # get all the subnodes of "user"
     assert len(names) == len(tuple(model.children(root="pyre.user")))
-    for key, identifier, name, fqname, node in model.children(root="user"):
+    for key, slot in model.children(root="pyre.user"):
         # check that we got the canonical name
-        assert name in names
+        assert slot.name in names
         # and the correct node
-        assert model._resolve(name=fqname) == (node, identifier)
+        assert model._resolve(name=slot.name) == (slot.node, key)
 
     # repeat with the alias "χρήστης"
     assert len(names) == len(tuple(model.children(root="χρήστης")))
-    for key, identifier, name, fqname, node in model.children(root="χρήστης"):
+    for key, slot in model.children(root="χρήστης"):
         # check that we got the canonical name
-        assert name in names
+        assert slot.name in names
         # and the correct node
-        assert model._resolve(name=fqname) == (node, identifier)
+        assert model._resolve(name=slot.name) == (slot.node, key)
 
     return
 
