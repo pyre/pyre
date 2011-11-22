e15ef408301c15713f99b37175f5c62eba6d6b06
Michael Aivazis <michael.aivazis@orthologue.com>
2011-11-22 13:52:57 -0800

    extensions: turned mpi and postgres into optional packages

--- extensions/Make.mm
+++ extensions/Make.mm
@@ -10,9 +10,20 @@ PROJECT = pyre
 
 RECURSE_DIRS = \
     journal \
-    mpi \
-    postgres \
     timers
+
+# the optional packages
+# mpi
+MPI_DIR= # overriden by the the environment
+ifneq ($(strip $(MPI_DIR)),)
+  RECURSE_DIRS += mpi
+endif
+
+# postgres
+LIBPQ_DIR= # overriden by the the environment
+ifneq ($(strip $(LIBPQ_DIR)),)
+  RECURSE_DIRS += postgres
+endif
 
 #--------------------------------------------------------------------------
 #
