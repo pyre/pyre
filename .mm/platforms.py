1e99762a377fcd4464807e70f30d30ef26267eb5
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-12-16 07:32:24 -0800

    build: added {linux} platform support

--- .mm/platforms.py
+++ .mm/platforms.py
@@ -37,10 +37,50 @@ def platform(builder):
         # fix mpi
         mpiVersion = 'openmpi'
         builder.requirements['mpi'].environ = {
+            'MPI_VERSION': mpiVersion,
             'MPI_DIR': systemdir,
             'MPI_LIBDIR': os.path.join(systemdir, 'lib'),
             'MPI_INCDIR': os.path.join(systemdir, 'include', mpiVersion),
             'MPI_EXECUTIVE': 'openmpirun',
+            }
+        # fix python
+        pythonVersion = '3.2'
+        python = 'python' + pythonVersion
+        builder.requirements['python'].environ = {
+            'PYTHON': python,
+            'PYTHON_PYCFLAGS': '-b',
+            'PYTHON_DIR': systemdir,
+            'PYTHON_LIBDIR': os.path.join(systemdir, 'lib', python),
+            'PYTHON_INCDIR': os.path.join(systemdir, 'include', python),
+            }
+        # all done
+        return builder
+
+    # on linux
+    if platform == 'Linux':
+        # on normal distributions
+        systemdir = '/usr'
+        # fix {gsl}
+        builder.requirements['gsl'].environ = {
+            'GSL_DIR': systemdir,
+            'GSL_LIBDIR': os.path.join(systemdir, 'lib'),
+            'GSL_INCDIR': os.path.join(systemdir, 'include'),
+            }
+        # fix libpq
+        libpqVersion = '8.4'
+        builder.requirements['libpq'].environ = {
+            'LIBPQ_DIR': systemdir,
+            'LIBPQ_INCDIR': os.path.join(systemdir, 'include', 'postgresql'),
+            'LIBPQ_LIBDIR': os.path.join(systemdir, 'lib', 'postgresql', libpqVersion, 'lib'),
+            }
+        # fix mpi
+        mpiVersion = 'openmpi'
+        builder.requirements['mpi'].environ = {
+            'MPI_VERSION': mpiVersion,
+            'MPI_DIR': systemdir,
+            'MPI_LIBDIR': os.path.join(systemdir, 'lib'),
+            'MPI_INCDIR': os.path.join(systemdir, 'include', mpiVersion),
+            'MPI_EXECUTIVE': 'mpirun',
             }
         # fix python
         pythonVersion = '3.2'
