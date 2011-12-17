c7ed2b4f61ebd9f19ff512ce21d507b5cd71e59c
aivazis <aivazis@head.cluster>
2011-12-16 19:22:48 -0600

    build: added support for {asap.mit.edu}

--- .mm/hosts.py
+++ .mm/hosts.py
@@ -5,7 +5,11 @@
 # (c) 1998-2011 all rights reserved
 #
 
+# externals
+import os
 
+
+# the builder decorator
 def host(builder):
     """
     Decorate the {builder} with host specific options
@@ -13,6 +17,37 @@ def host(builder):
     # get the host name
     name = builder.host.name
     # print('host:', name)
+
+    # on asap.mit.edu
+    if name == 'head.cluster':
+        # no gsl
+        builder.requirements['gsl'].environ = {}
+        # set up mpi
+        mpiHome = '/act/openmpi/gnu'
+        mpiVersion = 'openmpi'
+        builder.requirements['mpi'].environ = {
+            'MPI_VERSION': mpiVersion,
+            'MPI_DIR': mpiHome,
+            'MPI_LIBDIR': os.path.join(mpiHome, 'lib'),
+            'MPI_INCDIR': os.path.join(mpiHome, 'include'),
+            'MPI_EXECUTIVE': 'mpirun',
+            }
+        # no postgres
+        builder.requirements['libpq'].environ = {}
+        # python 3.2 is in /usr/local
+        pythonHome = '/usr/local'
+        pythonVersion = '3.2'
+        python = 'python' + pythonVersion
+        builder.requirements['python'].environ = {
+            'PYTHON': python,
+            'PYTHON_PYCFLAGS': '-b',
+            'PYTHON_DIR': pythonHome,
+            'PYTHON_LIBDIR': os.path.join(pythonHome, 'lib', python),
+            'PYTHON_INCDIR': os.path.join(pythonHome, 'include', python+'m'),
+            }
+
+        # all done
+        return builder
 
     # for all other hosts
     return builder
