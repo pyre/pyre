f224cb1f06f50da6e3f17e9a9444838c43d21bd4
Michael Aïvázis <michael.aivazis@orthologue.com>
2011-12-20 10:08:12 -0800

    merlin.spells: started fleshing out {init}

--- packages/merlin/spells/Initializer.py
+++ packages/merlin/spells/Initializer.py
@@ -6,7 +6,8 @@
 #
 
 
-# packages
+# externals
+import os
 import pyre
 
 
@@ -17,18 +18,53 @@ from .Spell import Spell
 # declaration
 class Initializer(Spell):
     """
-    This is a sample documentation string for class Initializer
+    Create a new merlin project rooted at the given directory
     """
+
+
+    # public state
+    createPrefix = pyre.properties.bool(default=False)
+    createPrefix.aliases.add('create-prefix')
 
 
     # class interface
     # interface
     @pyre.export
-    def main(self, **kwds):
+    def main(self, *args, **kwds):
         """
-        This is the action of the spell
+        Make {folder} the root of a new merlin project. The target {folder} is given as an
+        optional command line argument, and defaults to the current directory. Issue an error
+        message if {folder} is already a merlin project.
         """
-        print(" ** new project here")
+        # NYI: non-local uris
+        print('name:', self.pyre_name)
+        # access my executive
+        merlin = self.merlin
+
+        # the first argument is supposed to be a subdirectory of the current directory
+        folder = args[0] if args else os.curdir
+        print("target folder: {!r}".format(folder))
+
+        # first check whether this directory is already part of a merlin project
+        root, metadir = merlin.locateProjectRoot(folder=folder)
+        # if it is
+        if root:
+            # complain
+            import journal
+            msg = "{!r} is already within an existing project".format(folder)
+            return journal.error("merlin.init").log(msg)
+
+        return
+
+        # otherwise, if the directory does not exist
+        if not os.path.isdir(folder):
+            print("  does not exist; creating")
+            # were we asked to build all parent directories?
+            if self.createPrefix:
+                # yes, do it
+                print("    including all intermediates")
+                # os.makedirs(os.path.abspath(folder))
+
         # all done
         return
 
