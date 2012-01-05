58112d79365665981f4ab9b9fd1a7c0f44f05d73
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-01-05 15:50:25 -0800

    mm: added a section for Alta to {developers.py}

--- .mm/developers.py
+++ .mm/developers.py
@@ -21,6 +21,17 @@ def developer(builder):
         # all done
         return builder
 
+    # for Alta Fang <altafang@caltech.edu>
+    if name == 'alta':
+        # set the default target
+        builder.build.user = ['debug', 'shared']
+        # turn the fancy stuff off
+        builder.requirements['gsl'].environ = {}
+        builder.requirements['mpi'].environ = {}
+        builder.requirements['libpq'].environ = {}
+        # all done
+        return builder
+
     # for all others
     return builder
 
