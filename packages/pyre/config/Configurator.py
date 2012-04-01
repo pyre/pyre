a045c62503caeffb990e28555b962b1f7fcd42dc
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-04-01 02:04:03 -0700

    pyre.config: reimplemented deferred configuration assignments

--- packages/pyre/config/Configurator.py
+++ packages/pyre/config/Configurator.py
@@ -62,6 +62,8 @@ class Configurator(Model):
         Initialize the component instance inventory by making the descriptors point to the
         evaluation nodes
         """
+        # print(" ** Configurator.configureComponentInstance:")
+        # print("      configuring {.pyre_name!r} as {!r}".format(component, name))
         # build the namespace
         name = name if name is not None else component.pyre_name
         # turn it into a key
@@ -73,6 +75,7 @@ class Configurator(Model):
             registrar=registrar, configurable=component, namespace=ns, errors=errors)
 
         # and return
+        # print(" --   done configuring {.pyre_name!r} as {!r}".format(component, name))
         return errors
 
 
@@ -109,7 +112,6 @@ class Configurator(Model):
         for key, slot in self.children(key=namespace):
             # print("        found {0.name!r}, with priority: {0.priority}".format(slot))
             # print("          from: {0.locator}".format(slot))
-
             # try to find the corresponding descriptor
             # build its name: its the last portion of its full name
             name = slot.name.split(self.separator)[-1]
@@ -153,77 +155,33 @@ class Configurator(Model):
         Apply whatever deferred configuration settings are available in the configuration store
         under {namespace}
         """
-        # print("Configurator._transferConditionalConfigurationSettings:")
-        # print("  configurable={.pyre_name!r}".format(configurable))
-        # print("  namespace={!r}".format(namespace))
+        # print(" ** Configurator._transferConditionalConfigurationSettings:")
+        # print("      configurable={.pyre_name!r}".format(configurable))
+        # print("      namespace={!r}".format(namespace))
         # initialize the error pile
         errors = errors if errors is not None else []
         # get the family name
         family = configurable.pyre_family
-        # print("  family={!r}".format(family))
+        # print("      family={!r}".format(family))
         # if there isn't one, we are all done
         if not family: 
-            # print("  no family, bailing out")
-            return errors
-        # get the inventory
-        inventory = configurable.pyre_inventory
-        # hash the two to build the deferral key
-        ckey = self._hash.hash(namespace)
-        # if there aren't any settings that match these criteria, we are all done
-        if ckey not in self.deferred: 
-            # print("  no deferred configuration")
+            # print("      no family, bailing out")
             return errors
 
-        # otherwise, loop over the assignments
-        for assignment, priority in self.deferred[ckey]:
-            # build the trait name
-            alias = self.separator.join(assignment.key)
-            # print("    found: {!r} <- {.value!r}".format(alias, assignment))
-            # print("      from", assignment.locator)
-            # print("      with priority", priority)
-            # make sure all the conditions apply
-            match = False
-            # print("    conditions:")
-            for name, family in assignment.conditions:
-                # print("      name={}, family={}".format(name, family))
-                # if the name matches the given {namespace},
-                if name == namespace:
-                    # print("      namespace match")
-                    target = configurable
-                # otherwise, look for a registered component by that name
-                else:
-                    # print("      looking up by name...")
-                    try:
-                        target = registrar.names[self.separator.join(name)]
-                        # print("        got it")
-                    # no match
-                    except KeyError:
-                        # print("        not there")
-                        # and search no more
-                        break
-                # bail out if the families don't match
-                if target.pyre_family != family: 
-                    # print("      families don't match")
-                    break
-                # otherwise
-                # print("      families match")
-            # if we didn't exit prematurely, it's a match
-            else:
-                match = True
-                
-            # if no match
-            if not match:
-                # move on to the next one
-                continue
+        # for each deferred assignment
+        # print("      making the deferred assignments")
+        for trait, assignment, priority in self._retrieveDeferredAssignments(
+            registrar=registrar, ns=namespace):
 
-            # print("    all conditions passed")
+            # print("        attempting: {.pyre_name}.{} <- {}".format(
+                    # configurable, trait, assignment.value))
             # look for the corresponding descriptor
             try:
-                descriptor = configurable.pyre_getTraitDescriptor(alias=alias)
-                # print("    matching descriptor: {.name!r}".format(descriptor))
+                descriptor = configurable.pyre_getTraitDescriptor(alias=trait)
+                # print("        matching descriptor: {.name!r}".format(descriptor))
             # found a typo?
             except configurable.TraitNotFoundError as error:
-                # print("    no matching descriptor")
+                # print("        no matching descriptor")
                 errors.append(error)
                 continue
 
@@ -231,15 +189,88 @@ class Configurator(Model):
             slot = configurable.pyre_inventory[descriptor]
 
             # assign the trait value
-            # print("    before: {!r} <- {!r}".format(alias, getattr(configurable, alias)))
+            # print("        before: {!r} <- {!r}".format(trait, getattr(configurable, trait)))
             self._assign(
                 existing=slot, value=assignment.value,
                 priority=priority, locator=assignment.locator) 
-            # print("    after: {!r} <- {!r}".format(alias, getattr(configurable, alias)))
-
-        # print("  done with {.pyre_name!r}".format(configurable))
-        # return the accumulated errors
+            # print("        after: {!r} <- {!r}".format(trait, getattr(configurable, trait)))
+        # all done
         return errors
+
+
+    def _retrieveDeferredAssignments(self, registrar, ns):
+        """
+        Retrieve and prioritize deferred assignments for the namespace {ns}
+        """
+        # print("      retrieving deferred assignments...")
+        # hash the namespace
+        key = self._hash.hash(ns)
+        # initialize the trait assignment table
+        table = {}
+        # hunt down all deferred assignments for this namespace
+        for assignment, priority in self.deferred[key]:
+            # build the trait name
+            traitName = self.separator.join(assignment.key)
+            # print(" **     found: {!r} <- {.value!r}".format(traitName, assignment))
+            # print("          from", assignment.locator)
+            # print("          with priority", priority)
+
+            # check the assignment conditions
+            # print("          checking conditions")
+            # if any of the specified conditions is not satisfied
+            if not self._verifyConditions(registrar=registrar, conditions=assignment.conditions):
+                # get the next deferred assignment
+                # print("            FAILED")
+                continue
+            # print("            PASSED")
+
+            # otherwise, attempt to
+            try:
+                # retrieve the priority of a previous assignment
+                _, previous = table[traitName]
+            # if there wasn't one
+            except KeyError:
+                # place this one in the table
+                table[traitName] = (assignment, priority)
+            # otherwise
+            else:
+                # replace what's there, if the new one is higher priority
+                if priority > previous: table[traitName] = (assignment, priority)
+        # at this point, {table} has one (assignment, priority) pair for each encountered trait
+        for trait, (assignment, priority) in table.items():
+            # hand each one to the caller
+            yield trait, assignment, priority
+        
+        # all done
+        return
+
+
+    def _verifyConditions(self, registrar, conditions):
+        """
+        Check whether the constraints specified in {conditions} are true
+        """
+        # for each constraint
+        for name, family in conditions:
+            # print("            name={}, family={}".format(name, family))
+            # attempt to 
+            try:
+                # lookup the specified component by name
+                # print("            looking up the component")
+                component = registrar.names[self.separator.join(name)]
+            # if not there
+            except KeyError:
+                # we are done
+                # print("              component {!r} not found".format(self.separator.join(name)))
+                return False
+            # print("              got it; comparing family names")
+            # if the families do not match
+            if component.pyre_family != family:
+                # we are done
+                # print("              family mismatch: {}".format(component.pyre_family))
+                return False
+            # print("            all good")
+        # if we got this far, all is good
+        return True
 
 
 # end of file 
