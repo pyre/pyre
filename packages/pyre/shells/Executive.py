cfdaed9f2475d8297c1d2e530ddbfbc30f4b5f1b
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-04-27 21:36:28 -0700

    pyre.shells: the {Executive} now sniffs around for platofrm information

--- packages/pyre/shells/Executive.py
+++ packages/pyre/shells/Executive.py
@@ -6,8 +6,11 @@
 #
 
 
-# external
+# externals
+import re
+import sys
 import pyre # the framework
+import platform
 # my interface
 from .Shell import Shell as shell
 
@@ -19,29 +22,26 @@ class Executive(pyre.component, implements=shell):
     """
 
 
-    # public data
+    # user configurable state
     home = pyre.properties.str(default=None)
     home.doc = "the process home directory"
 
+    hostmap = pyre.map(cast=pyre.properties.str)
+    hostmap.doc = """
+        a mapping of host nicknames to regular expressions that match against the formal
+        machine name as returned by platform.uname()
+        """
 
-    @property
-    def hostname(self):
-        """
-        Retrieve the name of the host on which this process is running
-        """
-        # use the hostname from {platform}
-        import platform
-        return platform.node()
+    # public data
+    system = None # the type of host on which this process is running
+    release = None # the OS release
+    version = None # the OS version
+    architecture = None # the CPU architecture
+    processor = None # the CPU type
 
-
-    @property
-    def platform(self):
-        """
-        Retrieve the type of host on which this process is running
-        """
-        # use the string from sys
-        import sys
-        return sys.platform
+    hostname = None # the name of the host on which this process is running
+    nickname = None # the nickname of the host on which this process is running
+    platform = None # the OS type on which this process is running
 
 
     # interface
@@ -52,6 +52,40 @@ class Executive(pyre.component, implements=shell):
         """
         # {Executive} is abstract
         raise NotImplementedError("class {.__name__} must implement 'launch'".format(type(self)))
+
+
+    def loadHostConfiguration(self, name=None):
+        """
+        Load the configuration files for the named host
+        """
+
+
+    # meta methods
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+
+        # host discovery
+        (system, name, release, version, architecture, processor) = platform.uname()
+        self.hostname = self.nickname = name
+        self.system = system
+        self.release = release
+        self.version = version
+        self.architecture = architecture
+        self.processor = processor
+        
+        self.platform = sys.platform
+
+        # check whether we know this host by a nickname by looking through my {hostmap}
+        for nickname, regex in self.hostmap.items():
+            # if the hostname matches
+            if re.match(regex, name):
+                # set the nickname
+                self.nickname = nickname
+                # all done
+                break
+
+        # all done
+        return
 
 
 # end of file 
