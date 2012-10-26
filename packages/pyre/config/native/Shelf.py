eb80e97ebd800e41f6f0d470ccddd705bfca0277
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-10-26 16:50:23 -0700

    pyre.config.native: shelves cannot rely on the {__file__} attribute of modules for their locator since it is not uniformly available

--- packages/pyre/config/native/Shelf.py
+++ packages/pyre/config/native/Shelf.py
@@ -6,15 +6,15 @@
 #
 
 
-# externals
-from ... import tracking
-
-
 # declaration
 class Shelf:
     """
     A symbol table built on top of a native python module
     """
+
+    # N.B: shelves used to build their locators using the {__file__} attribute of modules;
+    # unfortunately, this attribute is not set for all modules on all platforms, so it is not
+    # reliable. this version just records the caller's locator
 
 
     # exceptions
@@ -26,24 +26,20 @@ class Shelf:
     module = None
     locator = None
 
-    @property
-    def source(self):
-        """
-        Return the filename associated with this module
-        """
-        return self.module.__file__
-
 
     # interface
     def retrieveSymbol(self, symbol):
         """
         Retrieve {symbol} from this shelf
         """
+        # if the {symbol} is exported by my module
         try:
+            # grab and return it
             return getattr(self.module, symbol)
+        # otherwise
         except AttributeError as error:
+            # complain
             raise self.SymbolNotFoundError(shelf=self, symbol=symbol) from error
-
         # unreachable
         import journal
         raise journal.firewall('pyre.config.native').log("UNREACHABLE")
@@ -52,9 +48,13 @@ class Shelf:
     # meta methods
     def __init__(self, module, uri, locator,  **kwds):
         super().__init__(**kwds)
+        # record the module
         self.module = module
+        # the {uri} that caused it to get loaded
         self.uri = uri
-        self.locator = tracking.chain(this=tracking.file(source=module.__file__), next=locator)
+        # and the locator
+        self.locator = locator
+        # all done
         return
 
 
