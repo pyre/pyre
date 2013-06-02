4dc58a03c717bcd5a82fb4598256d2ef374bbd98
Michael Aïvázis <michael.aivazis@orthologue.com>
2013-06-02 13:51:36 -0700

    pyre.schemata: repaired the default values for {list} and {set}

--- packages/pyre/schemata/List.py
+++ packages/pyre/schemata/List.py
@@ -19,7 +19,6 @@ class List(Sequence):
 
     # constants
     typename = 'list' # the name of my type
-    default  = [] # my default value
 
 
     # interface
@@ -30,6 +29,19 @@ class List(Sequence):
         # easy enough; resist the temptation to optimize this by skipping the call to super: we
         # have to coerce every item in the container!
         return list(super().coerce(value, **kwds))
+
+
+    # meta-methods
+    def __init__(self, default=list, **kwds):
+        # adjust the default; carefully, so we don't all end up using the same global container
+        # checking for {None} is not appropriate here; the user may want {None} as the default
+        # value; we need a way to know that {default} was not supplied: use {list} as the
+        # marker
+        default = list() if default is list else default
+        # chain up with my default
+        super().__init__(default=default, **kwds)
+        # all done
+        return
 
 
 # end of file 
--- packages/pyre/schemata/Set.py
+++ packages/pyre/schemata/Set.py
@@ -19,7 +19,6 @@ class Set(Sequence):
 
     # constants
     typename = 'set' # the name of my type
-    default  = set() # my default value
 
 
     # interface
@@ -30,6 +29,19 @@ class Set(Sequence):
         # easy enough; resist the temptation to optimize this by skipping the call to super: we
         # have to coerce every item in the container!
         return set(super().coerce(value, **kwds))
+
+
+    # meta-methods
+    def __init__(self, default=set, **kwds):
+        # adjust the default; carefully, so we don't all end up using the same global container
+        # checking for {None} is not appropriate here; the user may want {None} as the default
+        # value; we need a way to know that {default} was not supplied: use {set} as the
+        # marker
+        default = set() if default is set else default
+        # chain up with my default
+        super().__init__(default=default, **kwds)
+        # all done
+        return
 
 
 # end of file 
