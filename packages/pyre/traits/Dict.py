d680565af8c5c492c595da97a42d1086adca6518
Michael Aïvázis <michael.aivazis@orthologue.com>
2013-06-17 22:35:00 -0700

    pyre.traits: first cut at the new {Dict}

--- packages/pyre/traits/Dict.py
+++ packages/pyre/traits/Dict.py
@@ -6,42 +6,219 @@
 #
 
 
-# externals
-import collections
-from .. import schemata
-from .. import tracking
-
+# access to the default trait
+from .Property import Property
 # superclass
+from .Slotted import Slotted
+
+
+# declaration
+class Dict(Slotted):
+    """
+    A property that maps strings to components
+    """
+
+
+    # public data
+    @property
+    def macro(self):
+        """
+        Access to the default strategy for handling macros in slot values
+        """
+        # build whatever my schema specifies
+        return self.schema.macro
+
+
+    # my value processors
+    def coerce(self, value, **kwds):
+        """
+        Walk {value} through the casting procedure appropriate for clients that are component
+        classes
+        """
+        # leave {None} alone
+        if value is None: return None
+        # make sure we are building class slots, and delegate
+        return self.catalog(factory=self.schema.classSlot, value=value, **kwds)
+
+
+    def instantiate(self, value, **kwds):
+        """
+        Walk {value} through the casting procedure appropriate for clients that are component
+        instances
+        """
+        # leave {None} alone
+        if value is None: return None
+        # make sure we are building instance slots, and delegate
+        return self.catalog(factory=self.schema.instanceSlot, value=value, **kwds)
+
+
+    # framework hooks triggered by client configuration
+    def classConfigured(self, component, **kwds):
+        """
+        Notification that the client class record has been configured
+        """
+        # chain up
+        super().classConfigured(component=component, **kwds)
+        # configure the class record
+        self.configureClient(client=component,
+                             myFactory=self.classSlot, traitFactory=self.schema.classSlot)
+        # all done
+        return self
+
+
+    def instanceConfigured(self, instance, **kwds):
+        """
+        Notification that the client class record has been configured
+        """
+        # chain up
+        super().instanceConfigured(instance=instance, **kwds)
+        # configure the instance
+        self.configureClient(client=instance,
+                             myFactory=self.instanceSlot, traitFactory=self.schema.instanceSlot)
+        # all done
+        return self
+
+
+    # meta-methods
+    def __init__(self, schema=Property.identity(), default=dict, **kwds):
+        # adjust the default value
+        default = dict() if default is dict else default
+        # chain up with a potentially adjusted default value
+        super().__init__(default=default, **kwds)
+        # record my schema
+        self.schema = schema
+        # build my slot factories
+        self.classSlot = self.factory(trait=self, processor=self.coerce)
+        self.instanceSlot = self.factory(trait=self, processor=self.instantiate)
+        # all done
+        return
+
+
+    # implementation details
+    # catalog initialization
+    def catalog(self, factory, value, node, **kwds):
+        """
+        Instantiate and initialize an appropriate map
+        """
+        # get the node key
+        key = node.key
+        # grab my schema
+        schema = self.schema
+
+        # decide which mapping strategy to use: if the {node} has no key
+        if key is None:
+            #  make a {NameMap}
+            catalog = NameMap(schema=schema, factory=factory)
+        # otherwise
+        else:
+            # make a key based map
+            catalog = KeyMap(schema=schema, factory=factory, key=key)
+
+        # use this priority
+        priority = self.pyre_nameserver.priority.user
+        # go through the entries in {value}
+        for key, setting in value.items():
+            # make a locator
+            locator = tracking.simple('while adding entry {!r} to {.name!r}'.format(key, self))
+            # and update my map
+            catalog.insert(name=key, value=setting, locator=locator, priority=priority())
+        # and return it
+        return catalog
+
+
+    # client configuration
+    def configureClient(self, client, myFactory, traitFactory):
+        """
+        A named client with public inventory requires further configuration
+        """
+        # access the nameserver
+        nameserver = self.pyre_nameserver
+        # and the configurator
+        configurator = self.pyre_configurator
+        # get my schema
+        schema = self.schema
+        # find my slot
+        slot = client.pyre_inventory[self]
+        # this gets called only for public inventory items, so I am guaranteed a key
+        key = slot.key
+        # get my name
+        tag = nameserver.getName(key)
+        # the priority of all these assignments
+        priority = nameserver.priority.user
+
+        # make a key based map
+        catalog = KeyMap(schema=schema, factory=traitFactory, key=key)
+
+        # grab all direct assignments to this key
+        for name, node in configurator.retrieveDirectAssignments(key):
+            # extract the item key
+            name = nameserver.split(name)[-1]
+            # and its value
+            value = node.value
+            # make a locator
+            locator = tracking.simple('while adding entry {!r} to {.name!r}'.format(name, self))
+            # and store them
+            catalog.insert(name=name, value=value, priority=priority(), locator=locator)
+
+        # grab all deferred assignments to this key
+        for assignment, prty in configurator.retrieveDeferredAssignments(key):
+            # store them
+            catalog.insert(
+                name=assignment.key[0], value=assignment.value, 
+                priority=prty, locator=assignment.locator)
+
+        # get the my current slot value
+        current = slot.value
+        # if non-trivial, use it to initialize my catalog; i expect it to be a dictionary
+        # this must happen after direct and indirect assignments to avoid changing the
+        # nameserver model while the update is taking place
+        if current: 
+            # raise NotImplementedError("NYI: priorities/locators?")
+            catalog.update(current)
+
+        # make a locator
+        here = tracking.simple('while configuring {.pyre_name!r}'.format(client))
+
+        # attach my new value
+        client.pyre_inventory.setTrait(
+            trait=self, factory=myFactory, 
+            value=catalog, priority=priority(), locator=here)
+        
+        # all done
+        return
+
+
+        # all done
+        return self
+
+# implementation details
+# externals
+# superclasses
+import collections
 from ..framework.Client import Client
-
-
-# TODO: priorities and locators
-
-# it's not clear that value overrides are working correctly for all possible sources of
-# configuration events; the latest pass avoids using {update} in favor of the new method
-# {insert} in the base class, but i don't think this goes far enough or solves all the problems
+from .. import tracking
 
 
 # the helper container classes
 class Map(collections.abc.MutableMapping, Client):
     """
-    The base class for the storage strategies
+    The base class for the storage helpers
     """
 
     # public data
     schema = None
-    strategy = None # information necessary to make slots 
+    factory = None # information necessary to make slots 
 
     # meta-methods
-    def __init__(self, schema, strategy, *args, **kwds):
+    def __init__(self, schema, factory, *args, **kwds):
         # chain  up
         super().__init__(**kwds)
         # my storage
         self.map = {}
         # set my schema
         self.schema = schema
-        # and my strategy
-        self.strategy = strategy
+        # and my slot factory
+        self.factory = factory
         # initialize my contents
         self.update(*args, **kwds)
         # all done
@@ -131,26 +308,16 @@ class KeyMap(Map):
         """
         # get the nameserver
         nameserver = self.pyre_nameserver
-        # build the key
-        key = self.key[name]
         # build the full name of the map entry
         fullname = nameserver.join(self.name, name)
-
-        # unpack the slot building strategy
-        macro, postprocessor = self.strategy(model=nameserver)
-        # build a slot to hold the {value}
-        new = macro(key=key, postprocessor=postprocessor, value=value,
-                    priority=priority, locator=locator)
-
+        # insert into the model
+        key = nameserver.insert(name=fullname, value=value,
+                                factory=self.factory, locator=locator, priority=priority)
         # adjust my map
         self.map[name] = key
-        # register this key as belonging to a trait
-        nameserver.registerTrait(key=key, strategy=self.strategy)
-        # adjust the model
-        nameserver.insert(name=fullname, key=key, node=new)
-
         # all done
         return
+
 
 
 class NameMap(Map):
@@ -169,220 +336,29 @@ class NameMap(Map):
 
 
     # implementation details
-    def insert(self, name, value, priority, locator):
+    def insert(self, name, value, **kwds):
         """
         Build a slot to hold {value} and place it in the map
         """
-        # get the nameserver
-        nameserver = self.pyre_nameserver
-        # unpack the slot part
-        macro, postprocessor = self.strategy(model=nameserver)
-        # build the slot
-        new = macro(key=None, postprocessor=postprocessor, value=value, 
-                    locator=locator, priority=priority)
-        # get the old slot 
-        old = self.map[name]
-        # pick the winner of the two
-        winner = type(new).select(model=ns, existing=old, replacement=new)
-        # if the new slot is the winner
-        if new is winner:
-            # update the map
-            self.map[name] = new
-        # all done
-        return
-
-
-# superclass
-from .Property import Property
-# declaration
-class Dict(Property):
-    """
-    A property that maps strings to components
-    """
-
-
-    # public data
-    default = {} # a meaningless place holder; it gets coerced into some kind of map
-    schema = schemata.identity
-
-
-    # interface
-    def identify(self, value, node, **kwds):
-        """
-        Walk {value} through the casting procedure
-        """
-        # leave {None} alone
-        if value is None: return None
-        # make sure we build class slots and delegate
-        return self.catalog(strategy=self.schema.classSlot, value=value, node=node)
-
-
-    def coerce(self, value, node, **kwds):
-        """
-        Walk {value} through the casting procedure
-        """
-        # leave {None} alone
-        if value is None: return None
-        # make sure we build instance slots and delegate
-        return self.catalog(strategy=self.schema.instanceSlot, value=value, node=node)
-
-
-    # configuration
-    def classConfigured(self, component, **kwds):
-        """
-        Notification that the component class I am being attached to is configured
-        """
-        # chain up
-        super().classConfigured(**kwds)
-        # configure the component class record
-        self.configureClient(
-            client=component, myStrategy=self.classSlot, traitStrategy=self.schema.classSlot)
-        # all done
-        return self
-
-
-    def instanceConfigured(self, instance, **kwds):
-        """
-        Notification that the component instance I am being attached to is configured
-        """
-        # chain up
-        super().instanceConfigured(**kwds)
-        # configure the component instance
-        self.configureClient(
-            client=instance, myStrategy=self.instanceSlot, traitStrategy=self.schema.instanceSlot)
-        # all done
-        return self
-
-
-    # support for constructing slots
-    def classSlot(self, model):
-        """
-        Hook registered with the nameserver that informs it of my macro preference and the
-        correct postprocessor to attach to new slots for component classes
-        """
-        # my schema knows
-        return self.macro(model=model), self.identify
-
-
-    def instanceSlot(self, model):
-        """
-        Hook registered with the nameserver that informs it of my macro preference and the
-        correct postprocessor to attach to new slots for component classes
-        """
-        # my schema knows
-        return self.macro(model=model), self.coerce
-
-
-    def macro(self, model):
-        """
-        Return my preferred macro processor
-        """
-        # ask my schema
-        return self.schema.macro(model=model)
-
-
-    # implementation details
-    # configuration
-    def configureClient(self, client, myStrategy, traitStrategy):
-        # access the nameserver
-        nameserver = self.pyre_nameserver
-        # and the configurator
-        configurator = self.pyre_configurator
-        # get my schema
-        schema = self.schema
-        # find my slot
-        slot = client.pyre_inventory[self]
-        # this gets called only for public inventory items, so I am guaranteed a key
-        key = slot.key
-        # get my name
-        tag = nameserver.getName(key)
-        # the priority of all these assignments
-        priority = nameserver.priority.user
-
-        # make a key based map
-        catalog = KeyMap(schema=schema, strategy=traitStrategy, key=key)
-
-        # grab all direct assignments to this key
-        for name, node in configurator.retrieveDirectAssignments(key):
-            # extract the item key
-            name = nameserver.split(name)[-1]
-            # and its value
-            value = node.value
-            # make a locator
-            locator = tracking.simple('while adding entry {!r} to {.name!r}'.format(name, self))
-            # and store them
-            catalog.insert(name=name, value=value, priority=priority(), locator=locator)
-
-        # grab all deferred assignments to this key
-        for assignment, prty in configurator.retrieveDeferredAssignments(key):
-            # store them
-            catalog.insert(
-                name=assignment.key[0], value=assignment.value, 
-                priority=prty, locator=assignment.locator)
-
-        # get the my current slot value
-        current = slot.value
-        # if non-trivial, use it to initialize my catalog; i expect it to be a dictionary
-        # this must happen after direct and indirect assignments to avoid changing the
-        # nameserver model while the update is taking place
-        if current: 
-            # raise NotImplementedError("NYI: priorities/locators?")
-            catalog.update(current)
-
-        # make a locator
-        here = tracking.simple('while configuring {.pyre_name!r}'.format(client))
-
-        # attach my new value
-        client.pyre_inventory.setTrait(
-            trait=self, strategy=myStrategy, 
-            value=catalog, priority=priority(), locator=here)
-        
-        # all done
-        return
-
-
-    # catalog initialization
-    def catalog(self, strategy, value, node):
-        """
-        Instantiate and initialize an appropriate map
-        """
-        # {None} is special; leave it alone
-        if value is None: return None
-
-        # get the node key
-        key = node.key
-        # grab my schema
-        schema = self.schema
-
-        # decide which mapping strategy to use: if the {node} has no key
-        if key is None:
-            #  make a {NameMap}
-            catalog = NameMap(schema=schema, strategy=strategy)
-        # otherwise
+        # make a slot
+        new = self.factory(value=value)
+        # look for an existing slot under the same name
+        try:
+            # get it
+            old = self.map[name]
+        # if there wasn't one
+        except KeyError:
+            # no worries
+            pass
+        # if there was
         else:
-            # make a key based map
-            catalog = KeyMap(schema=schema, strategy=strategy, key=key)
-
-        # use this priority
-        priority = self.pyre_nameserver.priority.user
-        # go through the entries in {value}
-        for key, setting in value.items():
-            # make a locator
-            locator = tracking.simple('while adding entry {!r} to {.name!r}'.format(key, self))
-            # and update my map
-            catalog.insert(name=key, value=setting, locator=locator, priority=priority())
-        # and return it
-        return catalog
-
-
-    # meta-methods
-    def __init__(self, schema=schema, default=default, **kwds):
-        # chain up with a potentially adjusted default value
-        super().__init__(default=default, **kwds)
-        # record my schema
-        self.schema = schema
+            # replace the old with the new in the evaluation graph
+            new.replace(old)
+        # update the map
+        self.map[name] = new
         # all done
         return
+
 
 
 # end of file 
