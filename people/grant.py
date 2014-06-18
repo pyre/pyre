3fd0feeea3814af9dce5dd8b0ca93eb6bc965ec5
Michael Aïvázis <michael.aivazis@orthologue.com>
2014-06-18 07:43:27 -0700

    people: {grant} is now a {pyre} app that can distinguish writers from readers

--- people/grant.py
+++ people/grant.py
@@ -11,60 +11,77 @@
 Build the {authorized_keys} file that grants write access to the repository
 """
 
+
 # externals
 import os
-# settings
-roll = '.'
-home = '/home/projects/pyre'
-root = os.path.join(home, 'repository')
+# framework access
+import pyre
 
 
-# build the {authorized_keys} file
-def grant():
+# the app declaration
+class Grant(pyre.application):
     """
-    Build the {authorized_keys} file
+    Build the {authorized_keys} file that grants write access to the repository
     """
-    # create the output file
-    authorized = open('authorized_keys', 'w')
-    # the command template
-    command = (
-            'command="bzr serve --inet --directory={} --allow-writes"'.format(root) +
+    
+
+    # public state
+    repository = pyre.properties.str()
+    readers = pyre.properties.list(schema=pyre.properties.str())
+    writers = pyre.properties.list(schema=pyre.properties.str())
+    
+
+    @pyre.export
+    def main(self, *args, **kwds):
+        """
+        Build the {authorized_keys} file
+        """
+        # the command template
+        command = (
+            'command="bzr serve --inet --directory={}{{}}"'.format(self.repository) +
             ',no-port-forwarding,no-X11-forwarding,no-agent-forwarding {}'
             )
-    # add the user keys to the file
-    authorized.writelines(command.format(key) for user,key in readKeys())
-    # close the file
-    authorized.close()
-    # all done
-    return
+
+        # create the output file
+        authorized = open('authorized_keys', 'w')
+
+        # first the writers
+        for user in self.writers:
+            # build the line and add write privileges
+            authorized.writelines(
+                command.format(' --allow-writes',key) for key in self.readKeys(user))
+
+        # now the readers
+        for user in self.readers:
+            # build the line without write privileges
+            authorized.writelines(
+                command.format('', key) for key in self.readKeys(user))
+                    
+        # all done
+        return 0
 
 
-# read the keys
-def readKeys():
-    """
-    Open all files with the extension {.pub} in the current directory and return all the keys
-    they contain
-    """
-    # for each file in the roll directory
-    for filename in os.listdir(roll):
-        # extract the name of the user and the extension of the file
-        user, ext = os.path.splitext(filename)
-        # skip files that do not contain public keys
-        if ext != '.pub': continue
+    # implementation details
+    def readKeys(self, user):
+        """
+        Open all the public key file for the given {user} and return all the keys it contains
+        """
         # assemble the path to the key file
-        keyfile = os.path.abspath(os.path.join(roll, filename))
+        keyfile = user + '.pub'
         # read the contents
         for key in open(keyfile, 'r'):
             # show me the key
-            yield user, key
-    # all done
-    return
+            yield key
+        # all done
+        return
 
 
 # main
 if __name__ == "__main__":
-    # do...
-    grant()
+    # create one
+    grant = Grant(name='grant')
+    # do
+    grant.run()
 
 
 # end of file 
