5cd06401a31f34fbcad6872e5501fa5d5376cc3e
Michael Aivazis <michael.aivazis@orthologue.com>
2014-09-11 18:14:16 +0300

    pyre.weaver: started suporting the hierarchical nature of the {XML} renderer

--- packages/pyre/weaver/XML.py
+++ packages/pyre/weaver/XML.py
@@ -19,7 +19,7 @@ class XML(BlockMill):
     """
 
 
-    # interface
+    # mill obligations
     @pyre.export
     def header(self):
         """
@@ -29,6 +29,51 @@ class XML(BlockMill):
         yield '<?xml version="1.0"?>'
         # and the rest
         yield from super().header()
+        # all done
+        return
+
+
+    # interface
+    def push(self, tag, attributes=None):
+        """
+        Open a {tag}
+        """
+        # if I have non-trivial attributes
+        if attributes is not None:
+            # render them
+            attr = ''.join(' {}="{}"'.format(key, value) for key,value in attributes.items())
+        # otherwise
+        else:
+            # boring...
+            attr = ''
+        # build the tag and send it
+        yield '{.leader}<{}{}>'.format(self, tag, attr)
+        # add the {tag} to the pile
+        self.tags.append(tag)
+        # indent
+        self.indent()
+        # all done
+        return
+
+
+    def pop(self):
+        """
+        Pop a tag from the stack
+        """
+        # outdent
+        self.outdent()
+        # close the tag
+        yield '{.leader}</{}>'.format(self, self.tags.pop())
+        # all done
+        return
+
+
+    # meta-methods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # initialize my tag stack
+        self.tags = []
         # all done
         return
 
