30b3d9abc497cf10f83758a594016966f6709f80
Michael Aivazis <michael.aivazis@orthologue.com>
2015-02-06 10:26:54 -0800

    pyre.weaver: added the dependency management steps to the {django} template

--- templates/django/web/Make.mm
+++ templates/django/web/Make.mm
@@ -9,10 +9,14 @@
 include {project.name}.def
 # the package
 PACKAGE=web/www
+
+# the target folders
+EXPORT_RESOURCES = resources
+EXPORT_TEMPLATES = templates
 # the package
-EXPORT_WEB = \
-    resources \
-    templates
+EXPORT_WEB = $(EXPORT_RESOURCES) $(EXPORT_TEMPLATES)
+# dependency management
+PROJ_LIVE_BOWER = /usr/local/bin/bower install
 
 # standard targets
 all: export
@@ -20,5 +24,7 @@ all: export
 export:: export-web
 
 live: live-web
+	$(SSH) $(PROJ_LIVE_USERURL) \
+           '$(CD) $(PROJ_LIVE_PCKGDIR)/$(PROJECT)/$(EXPORT_RESOURCES); $(PROJ_LIVE_BOWER)'
 
 # end of file
