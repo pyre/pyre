297117dab102f34ea882348142608a3cc64b105e
Michael Aivazis <michael.aivazis@orthologue.com>
2015-03-12 11:08:09 -0700

    etc: new folder; added emacs mode for the {pfg} files

---
+++ etc/Make.mm
@@ -0,0 +1,14 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2015 all rights reserved
+#
+
+# project defaults
+include pyre.def
+
+# standard targets
+all: tidy
+
+# end of file
