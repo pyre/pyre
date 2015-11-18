c9d544212e0f81875fd5ff7bb2efb90ffb745d95
Michael Aivazis <michael.aivazis@orthologue.com>
2015-11-18 14:07:35 -0800

    www: simplified and rationalized the default web page

--- web/www/scripts/boot.js
+++ web/www/scripts/boot.js
@@ -16,15 +16,9 @@ $(document).ready(
         // build a configuration object
         configuration = new pyre_Configuration("#pyre-configuration");
 
-        // action elements submit ajax requests to the server
-        $("[action]").click(submitAction);
-
-        // input elements mark the model as dirty when they change
-        $("td.field > input").change(updateModel);
-
         // issue a request for the current configuration
         $.ajax({
-            url: "/send"
+            url: "/boot"
         }).done(function(data) {
             configuration.receive(data);
         });
---
+++ web/www/scripts/component.js
@@ -0,0 +1,19 @@
+// -*- javascript -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2015 all rights reserved
+//
+
+// pyre configuration object
+// constructor
+function pyre_Component(name) {
+    // locate the configuration placeholder
+    this.element = $(name)
+
+    // all done
+    return
+}
+
+// end of file
