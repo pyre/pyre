b87c819d2cdcd195a36a57f76877e3b5eb61a166
Michael Aivazis <michael.aivazis@orthologue.com>
2016-01-11 17:31:55 +0200

    pyre/web: repaired the live installation targets

--- web/apache/Make.mm
+++ web/apache/Make.mm
@@ -14,11 +14,22 @@ PACKAGE = web/apache
 # standard targets
 all: tidy
 
-live: live-apache-conf live-apache-restart
+live: $(PROJECT).conf live-apache-conf live-apache-restart
 
 # there is another target that might be useful:
 #
 #    live-apache-conf: make a link to the configuration file in the apache {sites-available}
 #                      directory, followed by enabling the site
+
+# assemble the apache configuration file
+$(PROJECT).conf: $(PROJECT)_conf Make.mm
+	@sed \
+          -e "s:PYRE_MAJOR:$(PROJECT_MAJOR):g" \
+          -e "s:PYRE_MINOR:$(PROJECT_MINOR):g" \
+          -e "s:PROJ_LIVE_HOST:$(PROJ_LIVE_HOST):g" \
+          -e "s:PROJ_LIVE_HOME:$(PROJ_LIVE_HOME):g" \
+          -e "s:PROJ_LIVE_WEBDIR:$(PROJ_LIVE_WEBDIR):g" \
+          -e "s:PROJ_LIVE_DOCROOT:$(PROJ_LIVE_DOCROOT):g" \
+          $(PROJECT)_conf > $(PROJECT).conf
 
 # end of file
