c9a5b2c31cd139e2bc5f690add6846f6ca5f99d2
Michael Aivazis <michael.aivazis@orthologue.com>
2016-01-11 17:50:55 +0200

    mm: added {boot}, a target to build and install a minimal pyre useful for bootstrapping pyre applications

--- Make.mm
+++ Make.mm
@@ -23,9 +23,6 @@ RECURSE_DIRS = \
     bot \
     people
 
-# the pyre install archive
-PYRE_ZIP = $(EXPORT_ROOT)/pyre-1.0.zip
-
 # the standard targets
 all:
 	BLD_ACTION="all" $(MM) recurse
@@ -47,11 +44,24 @@ build: lib packages extensions defaults bin
 
 test: build tests examples
 
+# the pyre install archives
+PYTHON_TAG = ${shell $(PYTHON) bin/cache_tag.py}
+PYRE_VERSION = $(PROJECT_MAJOR).$(PROJECT_MINOR)
+PYRE_BOOTPKGS = pyre journal merlin
+PYRE_ZIP = $(EXPORT_ROOT)/pyre-$(PYRE_VERSION).$(PYTHON_TAG).zip
+PYRE_BOOTZIP = $(EXPORT_ROOT)/pyre-$(PYRE_VERSION)-boot.$(PYTHON_TAG).zip
+
 zip: build zipit
 
 zipit:
 	$(RM_F) $(PYRE_ZIP)
 	(cd $(EXPORT_ROOT); zip -r ${PYRE_ZIP} * )
+
+boot:
+	@$(RM_F) $(PYRE_BOOTZIP)
+	@(cd $(EXPORT_ROOT)/packages; zip -r ${PYRE_BOOTZIP} $(PYRE_BOOTPKGS)--include \*.pyc)
+	scp $(PYRE_BOOTZIP) $(PROJ_LIVE_USERURL):$(PROJ_LIVE_DOCROOT)
+	@$(RM_F) $(PYRE_BOOTZIP)
 
 # shortcuts for building specific subdirectories
 .PHONY: $(RECURSE_DIRS) doc
