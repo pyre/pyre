9527d8dc5a6ba63ac9378a9b29875077c3e5b6cd
Michael Aivazis <michael.aivazis@orthologue.com>
2016-09-11 20:01:21 +0300

    pyre::geometry: retrieved a lost test

---
+++ tests/libpyre/geometry/iterator-order.cc
@@ -0,0 +1,40 @@
+// -*- C++ -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2016 all rights reserved
+//
+
+// exercise grid order construction:
+//   verify that all the parts are accessible through the public headers
+//   verify constructor signatures
+//   assemble a order
+//   verify it can be iterated
+
+// portability
+#include <portinfo>
+// support
+#include <pyre/geometry.h>
+
+// entry point
+int main() {
+    // fix the rep
+    typedef std::array<int, 4> rep_t;
+    // alias index; exercise the compiler's ability to deduce the order
+    typedef pyre::geometry::index_t<rep_t> index_t;
+    // create a shortcut to my target iterator type
+    typedef pyre::geometry::iterator_t<index_t> iterator_t;
+
+    // make a order
+    iterator_t::order_type order {3, 2, 1, 0};
+    // build the iteration boundaries
+    index_t begin {0, 0, 0, 0};
+    index_t end {5, 4, 3, 2};
+    // make a iterator
+    iterator_t iterator {begin, end, order};
+
+    // all done
+    return 0;
+}
+
+// end of file
