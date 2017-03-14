7015263a0d1f012d0910f76778c74868f7d42796
Michael Aivazis <michael.aivazis@orthologue.com>
2017-03-14 13:55:19 -0700

    web: fleshed out the {about} page

---
+++ web/www/react/views/home/about/gauss.js
@@ -0,0 +1,63 @@
+// -*- jsx -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2017 all rights reserved
+//
+
+// externals
+import React from 'react'
+
+// support
+import Python from 'widgets/code/Python'
+
+// the code snippet
+const listing = `\
+# support
+
+# the application class
+class Gauss(pyre.application, family='gauss.applications.simple'):
+    """
+    A simple application that computes the volume of a shape
+    """
+
+    # public state
+    shape = Shape()
+    radius.doc = 'the shape whose volume will be computed'
+
+    # interface
+    @pyre.export
+    def main(self):
+        """
+        Compute the volume of my shape
+        """
+        # ask the shape to measure itself
+        measure = self.shape.measure()
+        # display the result
+        self.info.log("volume = {measure}".format(measure=measure))
+        # report succecss
+        return 0
+
+# main
+if __name__ == "__main__":
+    # instantiate an app
+    gauss = Gauss(name="simple")
+    # invoke it
+    status = gauss.run()
+    # share the status with the user's shell
+    raise SystemExit(status)
+
+# end of file
+`
+
+// dress up
+const content = ({...props}) => (
+    <Python {...props}>
+        {listing}
+    </Python>
+)
+
+// publish
+export default content
+
+// end of file
--- web/www/react/views/home/about/index.js
+++ web/www/react/views/home/about/index.js
@@ -8,36 +8,66 @@
 // externals
 import React from 'react'
 
+// support
+import { Section, SectionTitle, Paragraph } from 'widgets/doc'
 // locals
 import styles from './styles'
-import PanelExample from './panel'
+
+import Ball from './Ball'
+import Shape from './Shape'
+import Gauss from './Gauss'
 
 
 // declaration
 const About = () => (
-    <section style={styles.section}>
+    <Section>
 
-        <h1 style={styles.title}>about</h1>
+        <SectionTitle>about</SectionTitle>
 
-        <p style={styles.body}>
+        <Paragraph>
           <span className="pyre">pyre</span> is an open source application framework written
           in <a href="http://www.python.org">python</a>. It's an attempt to bring state of the
           art software design practices to scientific computing. The goal is to provide a
           strong skeleton on which to build scientific codes by steering the implementation
           towards usability and maintainability.
-        </p>
+        </Paragraph>
 
-        <p style={styles.body}>
+        <Paragraph>
           The basic conceptual building block in <span className="pyre">pyre</span> is
           the <em>component</em>. Components are classes that specifically grant access to some
           of their state to the application end user. Component authors provide default
           values to be used in case the user doesn't make a choice, but the user is explicitly
           given complete control and can override them during component configuration.
-        </p>
+        </Paragraph>
 
-        <PanelExample />
+        <Paragraph>
+            Here is a simple component that represents a multi-dimensional ball. It has two
+            user-configurable properties, and a single method that computes its measure:
+        </Paragraph>
 
-    </section>
+        <Ball/>
+
+        <Paragraph>
+            Packages that support more than one kind of shape should probably declare
+            a <em>protocol</em> to capture their basic properties and behaviors, much like
+            abstract base classes do for their sub-classes.
+        </Paragraph>
+
+        <Shape/>
+
+        <Paragraph>
+            Then, applications can use the protocol to express a <em>requirement</em> for some
+            kind of shape, and defer to the user for the actual choice. If the user does not
+            express an opinion, the default specified by Shape will be used.
+        </Paragraph>
+
+        <Gauss/>
+
+        <Paragraph>
+            For details, and more examples, please take a look at the tutorials.
+        </Paragraph>
+
+    </Section>
 )
 
 //   publish
