47d646e20a37a34d2694a18b75466744aba4bd0a
Michael Aivazis <michael.aivazis@orthologue.com>
2017-07-25 10:48:58 +0300

    pyre.shells: updated the support for {readline} in interactive shells

--- packages/pyre/shells/Interactive.py
+++ packages/pyre/shells/Interactive.py
@@ -33,67 +33,97 @@ class Interactive(Script, family="pyre.shells.interactive"):
 
 
     # implementation details
-    def pyre_interactiveSession(self, application, context=None):
+    def pyre_interactiveSession(self, application=None, banner=None, context=None):
         """
         Convert this session to an interactive one
         """
         # we need an application specific tag
-        name = application.pyre_namespace or application.pyre_name or 'pyre'
-
-        # try to
-        try:
-            # get readline
-            import readline
-        # if not there
-        except ImportError:
-            # no problem
-            pass
-        # if successful
-        else:
-            # turn on completion
-            import rlcompleter
-            # check which interface is available and do the right thing: on OSX, readline is
-            # provided by libedit
-            if 'libedit' in readline.__doc__:
-                # enable completion
-                readline.parse_and_bind('bind -v')
-                readline.parse_and_bind('bind ^I rl_complete')
-            # on other machines, or if the python readline extension is available on OSX
-            else:
-                # enable completion
-                readline.parse_and_bind('tab: complete')
-
-            # build the uri to the history file
-            history = pyre.primitives.path('~', '.{}-history'.format(name)).expanduser().resolve()
-            # stringify
-            history = str(history)
-            # attempt to
-            try:
-                # read it
-                readline.read_history_file(history)
-            # if not there
-            except IOError:
-                # no problem
-                pass
-            # make sure it gets saved
-            import atexit
-            # by registering a handler for when the session terminates
-            atexit.register(readline.write_history_file, history)
+        tag = (application.pyre_namespace or application.pyre_name) if application else 'pyre'
 
         # go live
         import code, sys
+        # configure {readline}
+        self.pyre_enrich(tag)
         # adjust the prompts
-        sys.ps1 = '{}: '.format(name)
+        sys.ps1 = '{}: '.format(tag)
         sys.ps2 = '  ... '
 
         # prime the local namespace
-        context = context or {}
-        # adjust it
-        context['app'] = application
-        # give the application an opportunity to add symbols as well
-        context = application.pyre_interactiveSessionContext(context=context)
+        symbols = {}
+        # if we have an application instance
+        if application:
+            # provide access to the application object itself
+            symbols['app'] = application
+            # and ask it to decorate further
+            symbols = application.pyre_interactiveSessionContext(context=symbols)
+        # if the caller gave us {context}
+        if context:
+            # include it
+            symbols.update(context)
+
+        # if we are serving a specific application
+        if banner is None and application:
+            # ask it for a sign on banner
+            banner = application.pyre_interactiveBanner()
+
         # enter interactive mode
-        return code.interact(banner=application.pyre_interactiveBanner(), local=context)
+        return code.interact(banner=banner, local=symbols)
+
+
+    def pyre_enrich(self, tag):
+        """
+        Attempt to provide a richer interactive experience
+        """
+        # attempt to
+        try:
+            # pull readline support
+            import readline, rlcompleter
+        # if unable
+        except ImportError:
+            # is there anything else we can do?
+            return
+
+        # get the package docstring, gingerly
+        doc = getattr(readline, '__doc__', None)
+        # on OSX, {readline} may be implemented using {libedit}
+        if 'libedit' in doc:
+            # bind the <tab> character to the correct behavior
+            readline.parse_and_bind('bind ^I rl_complete')
+        # otherwise
+        else:
+            # assume gnu {readline}
+            readline.parse_and_bind('tab: complete')
+
+        # again carefully, try to
+        try:
+            # initialize support
+            readline.read_init_file()
+        # if that fails
+        except OSError:
+            # there are many possible causes; most are unlikely on a well managed system
+            # in all likelihood, it's just that the configuration file doesn't exist
+            # can't do much about any of them, anyway
+            pass
+
+        # build the uri to the history file
+        history = pyre.primitives.path('~', '.{}-history'.format(tag)).expanduser().resolve()
+        # stringify
+        history = str(history)
+        # attempt to
+        try:
+            # read it
+            readline.read_history_file(history)
+        # if not there
+        except IOError:
+            # no problem
+            pass
+        # make sure it gets saved
+        import atexit
+        # by registering a handler for when the session terminates
+        atexit.register(readline.write_history_file, history)
+
+        # all done
+        return
 
 
 # end of file
