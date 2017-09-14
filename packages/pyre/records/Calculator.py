72fafb5c5f012066fa0a5da2d4a7e09cbecc1e24
Michael Aivazis <michael.aivazis@orthologue.com>
2017-09-14 11:48:53 +0300

    pyre.records: improved the readability of the {Calculator} implementation

--- packages/pyre/records/Calculator.py
+++ packages/pyre/records/Calculator.py
@@ -22,10 +22,12 @@ class Calculator:
         """
         Pull values from {source}, convert and yield them
         """
+        # make {pyre.calc} variables so record fields are mutable
+        variable = self.node.variable
         # zip together the data stream and the descriptors
         for field, value in zip(record.pyre_fields, source):
             # build a node to hold it
-            node = self.node.variable(value=value, postprocessor=field.process)
+            node = variable(value=value, postprocessor=field.process)
             # and make it available
             yield node
         # all done
