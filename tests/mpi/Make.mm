82d0cafed31835d81f1b9abff7ae4827dc9d300e
Michael Aivazis <michael.aivazis@orthologue.com>
2017-12-09 11:52:23 -0800

    mpi: added a machine file to the test suite to force the existence of the required resources

--- tests/mpi/Make.mm
+++ tests/mpi/Make.mm
@@ -8,8 +8,7 @@
 
 PROJECT = pyre
 
-#--------------------------------------------------------------------------
-#
+MPI_ARGS = --hostfile localhost
 
 all: test
 
@@ -18,24 +17,24 @@ test: sanity groups communications launch
 sanity:
 	${PYTHON} ./sanity.py
 	${PYTHON} ./extension.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./extension.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./extension.py
 	${PYTHON} ./world.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./world.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./world.py
 
 groups:
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./group.py
-	${MPI_EXECUTIVE} -np 7 ${PYTHON} ./group_include.py
-	${MPI_EXECUTIVE} -np 7 ${PYTHON} ./group_exclude.py
-	${MPI_EXECUTIVE} -np 7 ${PYTHON} ./group_setops.py
-	${MPI_EXECUTIVE} -np 7 ${PYTHON} ./restrict.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./group.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 7 ${PYTHON} ./group_include.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 7 ${PYTHON} ./group_exclude.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 7 ${PYTHON} ./group_setops.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 7 ${PYTHON} ./restrict.py
 
 communications:
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./bcast.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./sum.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./product.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./max.py
-	${MPI_EXECUTIVE} -np 8 ${PYTHON} ./min.py
-	${MPI_EXECUTIVE} -np 7 ${PYTHON} ./port.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./bcast.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./sum.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./product.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./max.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 8 ${PYTHON} ./min.py
+	${MPI_EXECUTIVE} ${MPI_ARGS} -np 7 ${PYTHON} ./port.py
 
 launch: mpirun slurm
 
