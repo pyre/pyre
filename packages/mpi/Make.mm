4f9f565eae4e2575ebd7aa985be6e9b133134971
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-04-22 18:34:28 -0700

    cm: switched from the {bxr} {revno} to the {git} {hash}

--- packages/mpi/Make.mm
+++ packages/mpi/Make.mm
@@ -25,7 +25,7 @@ EXPORT_PYTHON_MODULES = \
 # get today's date
 TODAY = ${strip ${shell date -u}}
 # grab the revision number
-REVISION = ${strip ${shell bzr revno}}
+REVISION = ${strip ${shell git log --format=format:"%h" -n 1}}
 # if not there
 ifeq ($(REVISION),)
 REVISION = 0
