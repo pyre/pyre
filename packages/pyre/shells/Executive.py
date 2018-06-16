44e2a3cabbff27dce654b097eeae68d5109149c7
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-06-16 16:12:41 +0300

    pyre.shells: added machine layout configuration options to the {shell} hierarchy

--- packages/pyre/shells/Executive.py
+++ packages/pyre/shells/Executive.py
@@ -23,6 +23,17 @@ class Executive(pyre.component, family='pyre.shells.executive', implements=shell
     # user configurable state
     home = pyre.properties.str(default=None)
     home.doc = "the process home directory"
+
+    # machine layout
+    hosts = pyre.properties.int(default=1)
+    hosts.doc = "the number of hosts in the parallel machine"
+
+    tasks = pyre.properties.int(default=1)
+    tasks.doc = "the number of tasks per host"
+
+    gpus = pyre.properties.int(default=0)
+    gpus.doc = "the number of GPU coprocessors per task"
+
     # a marker that enables applications to deduce the type of shell that is hosting them
     model = pyre.properties.str(default='unknown')
     model.doc = "the programming model"
