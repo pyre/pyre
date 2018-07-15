734b2a377633b97759cc5783edba3eac76d07fd6
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-07-15 11:43:50 +0300

    mm: made some progress in building the {mm-4.0} makefile

--- .mm/pyre.mm
+++ .mm/pyre.mm
@@ -10,18 +10,23 @@ pyre.major := 1
 pyre.minor := 0
 
 # pyre builds a python package
-pyre.packages := pyre.pkg
+pyre.packages := pyre.pkg journal.pkg
 # a library
 pyre.libraries := journal.lib pyre.lib
 # a python extension
-pyre.extensions := # pyre.ext
+pyre.extensions := journal.ext
 # and a test suite
 pyre.tests := # pyre.tst.pyre pyre.tst.libpyre
 
-# the package meta-data
+# the pyre meta-data
 pyre.pkg.root := packages/pyre/
 pyre.pkg.stem := pyre
 pyre.pkg.drivers := pyre
+
+# the pyre meta-data
+journal.pkg.root := packages/journal/
+journal.pkg.stem := journal
+journal.pkg.meta :=
 
 # the journal library meta-data
 journal.lib.root := lib/journal/
@@ -29,18 +34,23 @@ journal.lib.stem := journal
 journal.lib.incdir := $(builder.dest.inc)pyre/journal/
 journal.lib.master := journal.h
 journal.lib.extern :=
+journal.lib.c++.flags += $($(compiler.c++).std.c++17)
 
 # the pyre library meta-data
 pyre.lib.root := lib/pyre/
 pyre.lib.stem := pyre
 pyre.lib.extern := journal.lib
 pyre.lib.prerequisites := journal.lib
+pyre.lib.c++.flags += $($(compiler.c++).std.c++17)
 
 # the extension meta-data
-pyre.ext.stem := pyre
-pyre.ext.pkg := pyre.pkg
-pyre.ext.wraps := pyre.lib
-pyre.ext.extern := pyre.lib python
+journal.ext.root := extensions/journal/
+journal.ext.stem := journal
+journal.ext.pkg := journal.pkg
+journal.ext.wraps := journal.lib
+journal.ext.extern := journal.lib python
+journal.ext.prerequisites := journal.lib
+journal.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
 
 # the libpyre test suite
 pyre.tst.libpyre.stem := libpyre
@@ -50,14 +60,5 @@ pyre.tst.libpyre.prerequisites := pyre.lib
 # the pyre package test suite
 pyre.tst.pyre.stem := pyre
 pyre.tst.pyre.prerequisites := pyre.pkg pyre.ext
-
-
-# testsuite
-
-# the foo/sanity.py test cases
-pyre.tst.pyre.foo.sanity.cases := foo.sanity.help foo.sanity.friend
-# command lines
-foo.sanity.help := --help
-foo.sanity.friend := --friend=ally
 
 # end of file
