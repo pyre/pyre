0dc81c3861d318c7b64d4c72f733445724108eb2
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-07-18 13:05:49 +0300

    mm: added {host}, {postgres}, and {timers} to the list of extensions that now build successfully with {mm-4.0}

--- .mm/pyre.mm
+++ .mm/pyre.mm
@@ -14,7 +14,7 @@ pyre.packages := pyre.pkg journal.pkg
 # a library
 pyre.libraries := journal.lib pyre.lib
 # a python extension
-pyre.extensions := journal.ext
+pyre.extensions := journal.ext host.ext postgres.ext timers.ext
 # and a test suite
 pyre.tests := # pyre.tst.pyre pyre.tst.libpyre
 
@@ -22,7 +22,7 @@ pyre.tests := # pyre.tst.pyre pyre.tst.libpyre
 pyre.pkg.root := packages/pyre/
 pyre.pkg.stem := pyre
 pyre.pkg.drivers := pyre
-pyre.pkg.ext := extensions
+pyre.pkg.ext := extensions/
 
 # the pyre meta-data
 journal.pkg.root := packages/journal/
@@ -50,16 +50,42 @@ journal.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
 pyre.lib.root := lib/pyre/
 pyre.lib.stem := pyre
 pyre.lib.extern := journal.lib
-pyre.lib.prerequisites := journal.lib
+pyre.lib.prerequisites += journal.lib
 pyre.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+# the pyre extensions
+# host info
+host.ext.root := extensions/host/
+host.ext.stem := host
+host.ext.pkg := pyre.pkg
+host.ext.wraps := pyre.lib
+host.ext.extern := pyre.lib journal.lib python
+host.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
+host.ext.lib.prerequisites += journal.lib # pyre.lib is added automatically
+# postgres
+postgres.ext.root := extensions/postgres/
+postgres.ext.stem := postgres
+postgres.ext.pkg := pyre.pkg
+postgres.ext.wraps := pyre.lib
+postgres.ext.extern := pyre.lib journal.lib libpq python
+postgres.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
+postgres.ext.lib.prerequisites += journal.lib # pyre.lib is added automatically
+# timers
+timers.ext.root := extensions/timers/
+timers.ext.stem := timers
+timers.ext.pkg := pyre.pkg
+timers.ext.wraps := pyre.lib
+timers.ext.extern := pyre.lib journal.lib python
+timers.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
+timers.ext.lib.prerequisites += journal.lib # pyre.lib is added automatically
 
 # the libpyre test suite
 pyre.tst.libpyre.stem := libpyre
 pyre.tst.libpyre.extern := pyre.lib pyre
-pyre.tst.libpyre.prerequisites := pyre.lib
+pyre.tst.libpyre.prerequisites += pyre.lib
 
 # the pyre package test suite
 pyre.tst.pyre.stem := pyre
-pyre.tst.pyre.prerequisites := pyre.pkg pyre.ext
+pyre.tst.pyre.prerequisites += pyre.pkg pyre.ext
 
 # end of file
