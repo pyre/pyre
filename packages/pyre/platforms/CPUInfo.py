0e681c3dd5f1201983beae72ceb02ed37d664dfa
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-09-14 14:41:22 +0300

    pyre.platforms: narrative improvements

--- packages/pyre/platforms/CPUInfo.py
+++ packages/pyre/platforms/CPUInfo.py
@@ -21,9 +21,9 @@ class CPUInfo:
 
     sockets = 1    # the number of physical chips; a socket has cores
     cores = 1      # the number of cores per socket; a core has cpus
-    cpus = 1       # the number of cpus per core; see {hyperthreading}
+    cpus = 1       # the number of cpus per core; see {hyper-threading}
 
-    # there are layers aboe this that could be captured:
+    # there are layers above this that could be captured:
     #     books, drawers, nodes
     # will look into these as pyre and its applications get ported to the multi-layer architectures
 
