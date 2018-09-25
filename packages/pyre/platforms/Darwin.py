30e767f3b13d2c6eccdef7a9ed76d9f1e060d19c
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-09-25 14:42:46 +0300

    pyre.platforms: added support for the {mojave} release of {darwin}

--- packages/pyre/platforms/Darwin.py
+++ packages/pyre/platforms/Darwin.py
@@ -89,6 +89,7 @@ class Darwin(POSIX, family='pyre.platforms.darwin'):
     # private data
     # the known code names
     codenames = {
+        '10.14': 'mojave',
         '10.13': 'high sierra',
         '10.12': 'sierra',
         '10.11': 'el capitan',
