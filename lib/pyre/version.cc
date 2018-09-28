bc0cc15330046d0fc98fe352881097e3f5bff889
Lijun Zhu <ljzhu@gps.caltech.edu>
2018-09-27 22:19:37 -0700

    added symm to blas

---
+++ lib/pyre/version.cc
@@ -0,0 +1,17 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2018 all rights reserved
+
+// my declarations
+#include "version.h"
+
+pyre::version_t
+pyre::version() {
+    //
+    return { 1, 0, "150fbec0" };
+}
+
+// end of file
