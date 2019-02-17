e90cd878af99f635918bf58cae43805ef9ab9c04
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-02-16 16:20:18 -0800

    pyre.flow: {node} now has {pyre_clients} that prints out the registered observers of every trait; the original intent was to make a {node} aware of changes to its own traits, but that's not working correctly yet

--- packages/pyre/flow/Node.py
+++ packages/pyre/flow/Node.py
@@ -17,4 +17,34 @@ class Node(pyre.component):
     """
 
 
+    # introspection
+    def pyre_clients(self):
+        """
+        Print a list of the observers of each of my traits
+        """
+        # get my slot
+        myslot = self.pyre_slot()
+        # ask it to watch over all my traits
+        # myslot.observe(self.pyre_inventory[trait] for trait in self.pyre_configurables())
+
+        # show me
+        observers = tuple(myslot.observers)
+        # show me
+        print(f"{self.pyre_spec}:")
+        print(f"    observers = {observers}")
+        # go through my traits
+        for trait in self.pyre_configurables():
+            # show me
+            print(f"    trait: {trait.name}")
+            # get its slot
+            slot = self.pyre_inventory[trait]
+            # see who's watching it
+            observers = tuple(slot.observers)
+            # show me
+            print(f"        observers = {observers}")
+
+        # all done
+        return
+
+
 # end of file
