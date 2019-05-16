b6bb04060c1f91400df843d98b6408f92aec8580
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-05-15 20:05:44 -0700

    pyre.flow: removed the obsolete binding adjustments methods from the {factory} monitor

--- packages/pyre/flow/FactoryStatus.py
+++ packages/pyre/flow/FactoryStatus.py
@@ -34,34 +34,4 @@ class FactoryStatus(Status):
         return self.removeObserver(observer=product.pyre_status)
 
 
-    def replaceInput(self, new, old):
-        """
-        Replace the {old} input with a {new} one
-        """
-        # remove me from the list of {old} observers
-        old.pyre_status.removeObserver(observer=self)
-        # and add to the list of {new} observers
-        new.pyre_status.addObserver(observer=self)
-        # if the {new} product is stale
-        if new.pyre_stale:
-            # mark the downstream graph
-            self.flush(observable=new.pyre_status)
-        # all done
-        return
-
-
-    def replaceOutput(self, new, old):
-        """
-        Replace the {old} output with a {new} one
-        """
-        # remove the {old} product from the list of my observers
-        self.removeObserver(observer=old.pyre_status)
-        # add the new one
-        self.addObserver(observer=new.pyre_status)
-        # and mark it
-        new.pyre_stale = True
-        # all done
-        return
-
-
 # end of file
