1c909a5d9f8fc2e057a1ae93ce3551aad2a8c09c
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-06-24 15:41:11 +0300

    pyre.components: reimplemented the class and instance validation tests now that the illegal assignments get deetected much earlier

--- tests/pyre/components/component_instance_validation.py
+++ tests/pyre/components/component_instance_validation.py
@@ -27,22 +27,27 @@ def test():
 
     # instantiate
     b = base(name="b")
-    # make an assignment that violates the constraint
-    b.positive = -1
-    # read the value
+    # attempt to
     try:
-        b.positive
+        # make an assignment that violates the constraint
+        b.positive = -1
+        # should be unreachable
         assert False
+    # if it were detected correctly
     except b.ConstraintViolationError:
-        pass
+        # verify the state didn't change
+        assert b.positive == 0
 
-    # and another
-    b.interval = 1
+    # attempt to
     try:
-        b.interval
+        # make another assignment that violates the constraint
+        b.interval = 1
+        # should be unreachable
         assert False
+    # if it were detected correctly
     except b.ConstraintViolationError:
-        pass
+        # verify the state didn't change
+        assert b.positive == 0
 
     return
 
