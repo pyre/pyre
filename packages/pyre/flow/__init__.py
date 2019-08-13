1b76c51f80a4b36f1c815c03dedab9bf0a8d0d56
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-08-13 14:13:04 +0300

    pyre.flow: removed the {workflow} foundry and factory from the package because it is too primitive to be useful directly

--- packages/pyre/flow/__init__.py
+++ packages/pyre/flow/__init__.py
@@ -21,28 +21,10 @@ from .Flow import Flow as flow
 # the components
 from .Factory import Factory as factory
 from .Product import Product as product
+from .Workflow import Workflow as workflow
 
 # the decorators
 from .Binder import Binder as bind
-
-
-# the workflow foundry
-@pyre.foundry(implements=flow, tip="a container of flow products and factories")
-def workflow():
-    # get the implementation
-    from .Workflow import Workflow
-    # borrow its docstring
-    __doc__ = Workflow.__doc__
-    # and publish it
-    return Workflow
-
-
-# and workflow factory
-def newWorkflow(**kwds):
-    # get the class
-    from .Workflow import Workflow
-    # build an instance and return it
-    return Workflow(**kwds)
 
 
 # end of file
