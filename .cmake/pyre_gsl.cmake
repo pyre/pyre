c9d336494c1446dc496a5db2bc746dc2cea47662
Lijun Zhu <ljzhu@gps.caltech.edu>
2020-02-04 10:34:22 -0800

    cmake: add WITH_MPI macro to gsl module with target_compile_definitions

--- .cmake/pyre_gsl.cmake
+++ .cmake/pyre_gsl.cmake
@@ -64,6 +64,8 @@ function(pyre_gslModule)
       target_include_directories(gslmodule PRIVATE ${MPI_CXX_INCLUDE_PATH})
       # and the mpi libraries
       target_link_libraries(gslmodule PRIVATE ${MPI_CXX_LIBRARIES})
+      # add the preprocessor macro
+      target_compile_definitions(gslmodule PRIVATE WITH_MPI)
     endif()
 
     # install the extension
