69d4f993d0113f1bb1fe1f41002432661a513b17
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-03-20 13:35:32 -0700

    pyre: added a master header file

---
+++ lib/pyre/pyre.h
@@ -0,0 +1,20 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2020 all rights reserved
+
+// code guard
+#if !defined(pyre_h)
+#define pyre_h
+
+// support
+#include "algebra.h"
+#include "timers.h"
+#include "memory.h"
+#include "grid.h"
+
+#endif
+
+// end of file
