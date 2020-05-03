3a79f83df9e0ce5971d1f29a17b4916b27df5c1b
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-03 16:45:41 -0700

    cmake: added the python capability testsuite

---
+++ .cmake/pyre_tests_python.cmake
@@ -0,0 +1,41 @@
+# -*- cmake -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+#
+
+
+#
+# python capability tests
+#
+pyre_test_python_testcase(python/format.py)
+pyre_test_python_testcase(python/moditer.py)
+pyre_test_python_testcase(python/files.py)
+pyre_test_python_testcase(python/classattr.py)
+pyre_test_python_testcase(python/slots.py)
+pyre_test_python_testcase(python/decorators.py)
+pyre_test_python_testcase(python/descriptors.py)
+pyre_test_python_testcase(python/dict_in.py)
+pyre_test_python_testcase(python/dict_update.py)
+pyre_test_python_testcase(python/functions.py)
+pyre_test_python_testcase(python/initialization.py)
+pyre_test_python_testcase(python/locale_codec.py)
+pyre_test_python_testcase(python/inheritance_shadow.py)
+pyre_test_python_testcase(python/inheritance_multiple.py)
+pyre_test_python_testcase(python/inheritance_properties.py)
+pyre_test_python_testcase(python/metaclass.py)
+pyre_test_python_testcase(python/metaclass_interface.py)
+pyre_test_python_testcase(python/metaclass_callsequence.py)
+pyre_test_python_testcase(python/metaclass_dict.py)
+pyre_test_python_testcase(python/metaclass_prime.py)
+pyre_test_python_testcase(python/metaclass_kwds.py)
+pyre_test_python_testcase(python/metaclass_dynamic.py)
+pyre_test_python_testcase(python/metaclass_inheritance.py)
+pyre_test_python_testcase(python/metaclass_refcount.py)
+pyre_test_python_testcase(python/expressions.py)
+pyre_test_python_testcase(python/arithmetic.py)
+pyre_test_python_testcase(python/algebra.py)
+pyre_test_python_testcase(python/execv.py)
+
+
+# end of file
