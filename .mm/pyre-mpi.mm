d410cab720e35ed1cc6d348659732683626f3033
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-06 19:09:13 -0700

    mm: added the {mpi.lib} and {mpi.pkg} testsuites to the pile

---
+++ .mm/pyre-mpi.mm
@@ -0,0 +1,62 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+# check availability
+mpi.available := ${findstring mpi,$(extern.available)}
+
+# if {mpi} is available
+ifeq ($(mpi.available), mpi)
+
+
+# project meta-data
+pyre-mpi.major := $(repo.major)
+pyre-mpi.minor := $(repo.minor)
+pyre-mpi.micro := $(repo.micro)
+pyre-mpi.revision := $(repo.revision)
+
+
+# mpi builds a python package
+pyre-mpi.packages := pyre-mpi.pkg
+# a library
+pyre-mpi.libraries := pyre-mpi.lib
+# a python extension
+pyre-mpi.extensions := pyre-mpi.ext
+# and test suites
+pyre-mpi.tests := pyre-mpi.pkg.tests pyre-mpi.lib.tests
+
+
+# the mpi package meta-data
+pyre-mpi.pkg.root := packages/mpi/
+pyre-mpi.pkg.stem := mpi
+pyre-mpi.pkg.ext :=
+
+# the mpi library meta-data
+pyre-mpi.lib.root := lib/mpi/
+pyre-mpi.lib.stem := mpi
+pyre-mpi.lib.incdir := $(builder.dest.inc)pyre/mpi/
+pyre-mpi.lib.master := mpi.h
+pyre-mpi.lib.prerequisites := journal.lib
+pyre-mpi.lib.extern := journal.lib mpi
+pyre-mpi.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+# the mpi extension meta-data
+pyre-mpi.ext.root := extensions/mpi/
+pyre-mpi.ext.stem := mpi
+pyre-mpi.ext.pkg := pyre-mpi.pkg
+pyre-mpi.ext.wraps := pyre-mpi.lib
+pyre-mpi.ext.capsule :=
+pyre-mpi.ext.extern := pyre.lib journal.lib mpi python
+pyre-mpi.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
+
+
+# get the testsuites
+include pyre-mpi.pkg.tests pyre-mpi.lib.tests
+
+
+endif
+
+
+# end of file
