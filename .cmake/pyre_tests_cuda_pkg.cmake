10c9cab5560b3a2a604ae5fef9c07a664a46a569
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-07 21:05:17 -0700

    cmake: added the {cuda.pkg} testsuite to the pile

---
+++ .cmake/pyre_tests_cuda_pkg.cmake
@@ -0,0 +1,15 @@
+# -*- cmake -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+#
+
+
+# cuda
+# sanity
+pyre_test_python_testcase(cuda.pkg/sanity.py)
+pyre_test_python_testcase(cuda.pkg/extension.py)
+pyre_test_python_testcase(cuda.pkg/manager.py)
+
+
+# end of file
