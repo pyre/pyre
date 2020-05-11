be7a13015a633dafb2a2ae473cd38d79774387e3
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-11 13:07:47 -0700

    cmake: activated the {journal.ext} testsuite

---
+++ .cmake/pyre_tests_journal_ext.cmake
@@ -0,0 +1,149 @@
+# -*- cmake -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+#
+
+
+#
+# journal bindings
+#
+pyre_test_python_testcase(journal.ext/api_application.py)
+pyre_test_python_testcase(journal.ext/api_file.py)
+pyre_test_python_testcase(journal.ext/api_quiet.py)
+pyre_test_python_testcase(journal.ext/cerr.py)
+pyre_test_python_testcase(journal.ext/chronicler.py)
+pyre_test_python_testcase(journal.ext/chronicler_device.py)
+pyre_test_python_testcase(journal.ext/chronicler_meta.py)
+pyre_test_python_testcase(journal.ext/chronicler_quiet.py)
+pyre_test_python_testcase(journal.ext/chronicler_verbosity.py)
+pyre_test_python_testcase(journal.ext/cout.py)
+pyre_test_python_testcase(journal.ext/debug_cascade.py)
+pyre_test_python_testcase(journal.ext/debug_defaults.py)
+pyre_test_python_testcase(journal.ext/debug_empty.py)
+pyre_test_python_testcase(journal.ext/debug_example.py)
+pyre_test_python_testcase(journal.ext/debug_example_fatal.py)
+pyre_test_python_testcase(journal.ext/debug_file.py)
+pyre_test_python_testcase(journal.ext/debug_flush.py)
+pyre_test_python_testcase(journal.ext/debug_inject.py)
+pyre_test_python_testcase(journal.ext/debug_instance.py)
+pyre_test_python_testcase(journal.ext/debug_loop.py)
+pyre_test_python_testcase(journal.ext/debug_notes.py)
+pyre_test_python_testcase(journal.ext/debug_properties.py)
+pyre_test_python_testcase(journal.ext/debug_quiet.py)
+pyre_test_python_testcase(journal.ext/debug_sanity.py)
+pyre_test_python_testcase(journal.ext/debug_shared.py)
+pyre_test_python_testcase(journal.ext/device_init.py)
+pyre_test_python_testcase(journal.ext/error_cascade.py)
+pyre_test_python_testcase(journal.ext/error_defaults.py)
+pyre_test_python_testcase(journal.ext/error_empty.py)
+pyre_test_python_testcase(journal.ext/error_example.py)
+pyre_test_python_testcase(journal.ext/error_example_nonfatal.py)
+pyre_test_python_testcase(journal.ext/error_file.py)
+pyre_test_python_testcase(journal.ext/error_flush.py)
+pyre_test_python_testcase(journal.ext/error_inject.py)
+pyre_test_python_testcase(journal.ext/error_instance.py)
+pyre_test_python_testcase(journal.ext/error_loop.py)
+pyre_test_python_testcase(journal.ext/error_notes.py)
+pyre_test_python_testcase(journal.ext/error_properties.py)
+pyre_test_python_testcase(journal.ext/error_quiet.py)
+pyre_test_python_testcase(journal.ext/error_sanity.py)
+pyre_test_python_testcase(journal.ext/error_shared.py)
+pyre_test_python_testcase(journal.ext/firewall_cascade.py)
+pyre_test_python_testcase(journal.ext/firewall_defaults.py)
+pyre_test_python_testcase(journal.ext/firewall_empty.py)
+pyre_test_python_testcase(journal.ext/firewall_example.py)
+pyre_test_python_testcase(journal.ext/firewall_example_nonfatal.py)
+pyre_test_python_testcase(journal.ext/firewall_file.py)
+pyre_test_python_testcase(journal.ext/firewall_flush.py)
+pyre_test_python_testcase(journal.ext/firewall_inject.py)
+pyre_test_python_testcase(journal.ext/firewall_instance.py)
+pyre_test_python_testcase(journal.ext/firewall_loop.py)
+pyre_test_python_testcase(journal.ext/firewall_notes.py)
+pyre_test_python_testcase(journal.ext/firewall_properties.py)
+pyre_test_python_testcase(journal.ext/firewall_quiet.py)
+pyre_test_python_testcase(journal.ext/firewall_sanity.py)
+pyre_test_python_testcase(journal.ext/firewall_shared.py)
+pyre_test_python_testcase(journal.ext/info_cascade.py)
+pyre_test_python_testcase(journal.ext/info_defaults.py)
+pyre_test_python_testcase(journal.ext/info_empty.py)
+pyre_test_python_testcase(journal.ext/info_example.py)
+pyre_test_python_testcase(journal.ext/info_example_fatal.py)
+pyre_test_python_testcase(journal.ext/info_file.py)
+pyre_test_python_testcase(journal.ext/info_flush.py)
+pyre_test_python_testcase(journal.ext/info_inject.py)
+pyre_test_python_testcase(journal.ext/info_instance.py)
+pyre_test_python_testcase(journal.ext/info_loop.py)
+pyre_test_python_testcase(journal.ext/info_notes.py)
+pyre_test_python_testcase(journal.ext/info_properties.py)
+pyre_test_python_testcase(journal.ext/info_quiet.py)
+pyre_test_python_testcase(journal.ext/info_sanity.py)
+pyre_test_python_testcase(journal.ext/info_shared.py)
+pyre_test_python_testcase(journal.ext/sanity.py)
+pyre_test_python_testcase(journal.ext/trash.py)
+pyre_test_python_testcase(journal.ext/warning_cascade.py)
+pyre_test_python_testcase(journal.ext/warning_defaults.py)
+pyre_test_python_testcase(journal.ext/warning_empty.py)
+pyre_test_python_testcase(journal.ext/warning_example.py)
+pyre_test_python_testcase(journal.ext/warning_example_fatal.py)
+pyre_test_python_testcase(journal.ext/warning_file.py)
+pyre_test_python_testcase(journal.ext/warning_flush.py)
+pyre_test_python_testcase(journal.ext/warning_inject.py)
+pyre_test_python_testcase(journal.ext/warning_instance.py)
+pyre_test_python_testcase(journal.ext/warning_loop.py)
+pyre_test_python_testcase(journal.ext/warning_notes.py)
+pyre_test_python_testcase(journal.ext/warning_properties.py)
+pyre_test_python_testcase(journal.ext/warning_quiet.py)
+pyre_test_python_testcase(journal.ext/warning_sanity.py)
+pyre_test_python_testcase(journal.ext/warning_shared.py)
+
+# clean up
+add_test(NAME journal.ext.api_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "echo $(pwd); rm api_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.api_file.cleanup PROPERTY
+  DEPENDS journal.ext.api_file.py
+  )
+
+add_test(NAME journal.ext.debug_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "rm debug_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.debug_file.cleanup PROPERTY
+  DEPENDS journal.ext.debug_file.py
+  )
+
+add_test(NAME journal.ext.error_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "rm error_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.error_file.cleanup PROPERTY
+  DEPENDS journal.ext.error_file.py
+  )
+
+add_test(NAME journal.ext.firewall_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "rm firewall_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.firewall_file.cleanup PROPERTY
+  DEPENDS journal.ext.firewall_file.py
+  )
+
+add_test(NAME journal.ext.info_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "rm info_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.info_file.cleanup PROPERTY
+  DEPENDS journal.ext.info_file.py
+  )
+
+add_test(NAME journal.ext.warning_file.cleanup
+  COMMAND ${BASH_PROGRAM} -c "rm warning_file.log"
+  WORKING_DIRECTORY ${PYRE_TESTSUITE_DIR}/journal.ext
+  )
+set_property(TEST journal.ext.warning_file.cleanup PROPERTY
+  DEPENDS journal.ext.warning_file.py
+  )
+
+# end of file
