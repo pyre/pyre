f238a7b092c2ddbdef10d36a757a7605930a3e82
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-12 11:57:28 -0700

    journal: revived the legacy {mm} makefiles; they are still useful during development since they can operate without a functioning {pyre} installation

---
+++ tests/journal.lib/Make.mm
@@ -0,0 +1,154 @@
+# -*- Makefile -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2020 all rights reserved
+#
+
+include journal.def
+PACKAGE = journal
+
+PROJ_TMPDIR = $(BLD_TMPDIR)/$(PROJECT)/$(PACKAGE)
+PROJ_CLEAN += $(TESTS) $(SPECIAL_TESTS) *.log *.dSYM
+
+
+TESTS = \
+    alert_sanity \
+    ansi_ansi \
+    ansi_gray \
+    ansi_misc \
+    ansi_null \
+    ansi_x11 \
+    api_application \
+    api_file \
+    api_quiet \
+    cerr_sanity \
+    channel_default_device \
+    channel_device \
+    channel_flush \
+    channel_index \
+    channel_inject \
+    channel_instance \
+    channel_inventory \
+    channel_iomanip \
+    channel_manipulators \
+    channel_sanity \
+    channel_twice \
+    channel_xtalk \
+    chronicler_device \
+    chronicler_globals \
+    chronicler_nameset \
+    chronicler_sanity \
+    cout_sanity \
+    csi_sanity \
+    debug_cascade \
+    debug_empty \
+    debug_endl \
+    debug_example \
+    debug_example_fatal \
+    debug_file \
+    debug_flush \
+    debug_inject \
+    debug_loop \
+    debug_manipulators \
+    debug_nolocator \
+    debug_null \
+    debug_quiet \
+    debug_sanity \
+    debug_shared \
+    device_sanity \
+    error_cascade \
+    error_empty \
+    error_example \
+    error_example_nonfatal \
+    error_file \
+    error_inventory \
+    error_loop \
+    error_nolocator \
+    error_quiet \
+    error_sanity \
+    error_shared \
+    file_example \
+    file_sanity \
+    firewall_cascade \
+    firewall_empty \
+    firewall_error \
+    firewall_example \
+    firewall_example_nonfatal \
+    firewall_file \
+    firewall_inventory \
+    firewall_nolocator \
+    firewall_quiet \
+    firewall_sanity \
+    firewall_shared \
+    index_cascade \
+    index_contains \
+    index_iter \
+    index_lookup \
+    index_sanity \
+    info_cascade \
+    info_empty \
+    info_example \
+    info_example_fatal \
+    info_file \
+    info_loop \
+    info_nolocator \
+    info_quiet \
+    info_shared \
+    inventory_device \
+    inventory_proxy \
+    inventory_sanity \
+    journal_sanity \
+    memo_sanity \
+    null_inject \
+    null_manipulators \
+    null_sanity \
+    trash_sanity \
+    warning_cascade \
+    warning_empty \
+    warning_example \
+    warning_example_fatal \
+    warning_file \
+    warning_loop \
+    warning_nolocator \
+    warning_quiet \
+    warning_shared \
+    debuginfo \
+    firewalls \
+
+
+SPECIAL_TESTS = \
+    ansi_emulates \
+    chronicler_init \
+    chronicler_verbosity \
+
+PROJ_LCC_LIBPATH = $(PROJ_LIBDIR)
+PROJ_LCXX_LIBPATH = $(PROJ_LIBDIR)
+PROJ_LIBRARIES = -ljournal
+LIBRARIES = $(PROJ_LIBRARIES) $(EXTERNAL_LIBS)
+
+#--------------------------------------------------------------------------
+
+all: test clean
+
+test: $(TESTS) $(SPECIAL_TESTS)
+	@echo "testing:"
+	@for testcase in $(TESTS); do { echo "    $${testcase}" ; ./$${testcase} ; } done
+	@echo "    ansi_emulates foo"; TERM=foo ./ansi_emulates 0
+	@echo "    ansi_emulates xterm"; TERM=xterm ./ansi_emulates 1
+	@echo "    chronicler_init"; \
+            JOURNAL_VERBOSITY=5 JOURNAL_DEBUG=test.init.one,test.init.two \
+            ./chronicler_init \
+            --journal.verbosity=5 --journal.debug=test.init.one,test.init.two
+	@echo "    chronicler_verbosity 3"; JOURNAL_VERBOSITY=3 ./chronicler_verbosity 3
+	@echo "    chronicler_verbosity 5"; JOURNAL_VERBOSITY=5 ./chronicler_verbosity 5
+
+# build
+%: %.c
+	$(CC) $(CFLAGS) $^ -o $@ $(LCFLAGS) $(LIBRARIES)
+
+%: %.cc
+	$(CXX) $(CXXFLAGS) $^ -o $@ $(LCXXFLAGS) $(LIBRARIES)
+
+# end of file
---
+++ tests/journal.pkg/Make.mm
@@ -0,0 +1,122 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2020 all rights reserved
+#
+
+
+PROJECT = pyre
+
+PROJ_CLEAN += *.log
+
+#--------------------------------------------------------------------------
+#
+
+all: test clean
+
+test: sanity channels devices
+
+sanity:
+	${PYTHON} ./sanity.py
+	${PYTHON} ./csi_sanity.py
+	${PYTHON} ./chronicler_sanity.py
+	${PYTHON} ./alert_sanity.py
+	${PYTHON} ./memo_sanity.py
+	${PYTHON} ./null_sanity.py
+	${PYTHON} ./cout_sanity.py
+	${PYTHON} ./cerr_sanity.py
+	${PYTHON} ./trash_sanity.py
+	${PYTHON} ./file_sanity.py
+	${PYTHON} ./index_sanity.py
+	${PYTHON} ./channel_sanity.py
+	${PYTHON} ./debug_sanity.py
+	${PYTHON} ./error_sanity.py
+	${PYTHON} ./firewall_sanity.py
+	${PYTHON} ./info_sanity.py
+	${PYTHON} ./warning_sanity.py
+
+colors:
+	${PYTHON} ./ansi_ansi.py
+	${PYTHON} ./ansi_emulates.py
+	${PYTHON} ./ansi_gray.py
+	${PYTHON} ./ansi_misc.py
+	${PYTHON} ./ansi_null.py
+	${PYTHON} ./ansi_x11.py
+
+api:
+	${PYTHON} ./api_quiet.py
+	${PYTHON} ./api_file.py
+	${PYTHON} ./api_application.py
+	${PYTHON} ./chronicler_device.py
+
+channels:
+	${PYTHON} ./index_lookup.py
+	${PYTHON} ./channel_default_device.py
+	${PYTHON} ./channel_device.py
+	${PYTHON} ./channel_index.py
+	${PYTHON} ./channel_inject.py
+	${PYTHON} ./channel_inventory.py
+	${PYTHON} ./channel_state.py
+	${PYTHON} ./channel_twice.py
+	${PYTHON} ./channel_xtalk.py
+	${PYTHON} ./debug_cascade.py
+	${PYTHON} ./debug_empty.py
+	${PYTHON} ./debug_example.py
+	${PYTHON} ./debug_example_fatal.py
+	${PYTHON} ./debug_file.py
+	${PYTHON} ./debug_flush.py
+	${PYTHON} ./debug_inject.py
+	${PYTHON} ./debug_instance.py
+	${PYTHON} ./debug_loop.py
+	${PYTHON} ./debug_quiet.py
+	${PYTHON} ./error_cascade.py
+	${PYTHON} ./error_empty.py
+	${PYTHON} ./error_example.py
+	${PYTHON} ./error_example_nonfatal.py
+	${PYTHON} ./error_file.py
+	${PYTHON} ./error_flush.py
+	${PYTHON} ./error_inject.py
+	${PYTHON} ./error_instance.py
+	${PYTHON} ./error_loop.py
+	${PYTHON} ./error_quiet.py
+	${PYTHON} ./firewall_cascade.py
+	${PYTHON} ./firewall_empty.py
+	${PYTHON} ./firewall_example.py
+	${PYTHON} ./firewall_example_nonfatal.py
+	${PYTHON} ./firewall_file.py
+	${PYTHON} ./firewall_flush.py
+	${PYTHON} ./firewall_inject.py
+	${PYTHON} ./firewall_instance.py
+	${PYTHON} ./firewall_loop.py
+	${PYTHON} ./firewall_quiet.py
+	${PYTHON} ./index_cascade.py
+	${PYTHON} ./info_cascade.py
+	${PYTHON} ./info_empty.py
+	${PYTHON} ./info_example.py
+	${PYTHON} ./info_example_fatal.py
+	${PYTHON} ./info_file.py
+	${PYTHON} ./info_flush.py
+	${PYTHON} ./info_inject.py
+	${PYTHON} ./info_instance.py
+	${PYTHON} ./info_loop.py
+	${PYTHON} ./info_quiet.py
+	${PYTHON} ./warning_cascade.py
+	${PYTHON} ./warning_empty.py
+	${PYTHON} ./warning_example.py
+	${PYTHON} ./warning_example_fatal.py
+	${PYTHON} ./warning_file.py
+	${PYTHON} ./warning_flush.py
+	${PYTHON} ./warning_inject.py
+	${PYTHON} ./warning_instance.py
+	${PYTHON} ./warning_loop.py
+	${PYTHON} ./warning_quiet.py
+
+renderers:
+	${PYTHON} ./memo_long_filename.py
+
+devices:
+	${PYTHON} ./null_inject.py
+	${PYTHON} ./file_example.py
+
+# end of file
