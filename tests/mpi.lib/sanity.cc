f8966f2f248f671d33cf9634ee94713190ce9e6f
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-06-18 06:28:04 -0700

    pyre::mpi: the {sanity} test now can inject a message to a channel

--- tests/mpi.lib/sanity.cc
+++ tests/mpi.lib/sanity.cc
@@ -11,9 +11,38 @@
 
 // access to the raw mpi routines
 #include <pyre/mpi.h>
+// and the journal
+#include <pyre/journal.h>
+
 
 // main program
-int main() {
+int main(int argc, char *argv[]) {
+
+    // initialize {mpi}
+    MPI_Init(&argc, &argv);
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+
+    // build a handle to the world communicator
+    pyre::mpi::communicator_t world(MPI_COMM_WORLD, true);
+    // compute the size of the world group
+    int wsize = world.size();
+    // and my rank
+    int wrank = world.rank();
+
+    // make a channel
+    pyre::journal::info_t channel("mpi.sanity");
+    // but silence it
+    channel.deactivate();
+
+    // and say something
+    channel
+        << "[" << wrank << "/" << wsize << "]: hello world!"
+        << pyre::journal::endl(__HERE__);
+
+    // finalize mpi
+    MPI_Finalize();
+
     // all done
     return 0;
 }
