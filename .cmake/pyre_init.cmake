0fafa8022bc9572d657d8cd23643b04674899f84
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-10-21 14:55:18 -0700

    cmake: added robust and packager-friendly versioning
    New auto-versioning approach adds error checking with improved
    output messages and an escape hatch for packagers (e.g. me) to
    manually specify the version.

--- .cmake/pyre_init.cmake
+++ .cmake/pyre_init.cmake
@@ -96,38 +96,27 @@ function(pyre_getVersion)
   # get tag info
   execute_process(
     COMMAND ${GIT_EXECUTABLE} describe --tags --long --always
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     RESULT_VARIABLE GIT_DESCRIBE_STATUS
     OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-
-  # the parser of the "git describe" result
-  set(GIT_DESCRIBE "v([0-9]+)\\.([0-9]+)\\.([0-9]+)-([0-9]+)-g(.+)" )
-
-  if(GIT_DESCRIBE_VERSION MATCHES ${GIT_DESCRIBE})
-      # parse the bits
-      string(REGEX REPLACE ${GIT_DESCRIBE} "\\1" REPO_MAJOR ${GIT_DESCRIBE_VERSION} )
-      string(REGEX REPLACE ${GIT_DESCRIBE} "\\2" REPO_MINOR ${GIT_DESCRIBE_VERSION})
-      string(REGEX REPLACE ${GIT_DESCRIBE} "\\3" REPO_MICRO ${GIT_DESCRIBE_VERSION})
-      string(REGEX REPLACE ${GIT_DESCRIBE} "\\5" REPO_COMMIT ${GIT_DESCRIBE_VERSION})
-  else()
-      set(REPO_MAJOR 1)
-      set(REPO_MINOR 0)
-      set(REPO_MICRO 0)
-      set(REPO_COMMIT ${GIT_DESCRIBE_VERSION})
+  if(NOT "${GIT_DESCRIBE_STATUS}" STREQUAL "0")
+    message(FATAL_ERROR "git describe --tags --long --always failed")
   endif()
 
-  # export our local variables
-  set(REPO_MAJOR ${REPO_MAJOR} PARENT_SCOPE)
-  set(REPO_MINOR ${REPO_MINOR} PARENT_SCOPE)
-  set(REPO_MICRO ${REPO_MICRO} PARENT_SCOPE)
-  set(REPO_COMMIT ${REPO_COMMIT} PARENT_SCOPE)
+  set(GIT_DESCRIBE "v([0-9]+)\\.([0-9]+)\\.([0-9]+)-([0-9]+)-g(.+)" )
+  if(NOT GIT_DESCRIBE_VERSION MATCHES ${GIT_DESCRIBE})
+    message(FATAL_ERROR "Invalid version string: ${GIT_DESCRIBE_VERSION}")
+  endif()
 
-  # set the variables used in the package meta-data files
-  set(MAJOR ${REPO_MAJOR} PARENT_SCOPE)
-  set(MINOR ${REPO_MINOR} PARENT_SCOPE)
-  set(MICRO ${REPO_MICRO} PARENT_SCOPE)
+  # parse the bits
+  string(REGEX REPLACE ${GIT_DESCRIBE} "\\1" REPO_MAJOR ${GIT_DESCRIBE_VERSION} )
+  string(REGEX REPLACE ${GIT_DESCRIBE} "\\2" REPO_MINOR ${GIT_DESCRIBE_VERSION})
+  string(REGEX REPLACE ${GIT_DESCRIBE} "\\3" REPO_MICRO ${GIT_DESCRIBE_VERSION})
+  string(REGEX REPLACE ${GIT_DESCRIBE} "\\5" REPO_COMMIT ${GIT_DESCRIBE_VERSION})
+
+  set(PYRE_VERSION "${REPO_MAJOR}.${REPO_MINOR}.${REPO_MICRO}" PARENT_SCOPE)
   set(REVISION ${REPO_COMMIT} PARENT_SCOPE)
   string(TIMESTAMP TODAY PARENT_SCOPE)
 
