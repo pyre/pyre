fde8658ac43a2edfa60c0aa8f0e9d0603f1a7667
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-06 16:52:40 -0800

    journal: {alert} now treats all {page} lines as part of the message {body}

--- packages/journal/Alert.py
+++ packages/journal/Alert.py
@@ -30,16 +30,23 @@ class Alert(Renderer):
 
         # get the notes
         notes = entry.notes
-
-        # get the severity
+        # get the metadata
+        channel = notes["channel"]
         severity = notes["severity"]
+        application = notes["application"]
+
+        # get the colors
+        resetColor = palette["reset"]
+        severityColor = palette[severity]
+
         # generate
         buffer = [
-            palette[severity], notes["application"], palette["reset"],
-            "(",
-            palette[severity], severity, palette["reset"],
-            "): ",
-            palette["body"], page[0], palette["reset"]
+            severityColor, application, resetColor,
+            " ",
+            severityColor, severity, resetColor,
+            ": ",
+            severityColor, channel, resetColor,
+            ":",
             ]
         # assemble and push
         yield ''.join(buffer)
@@ -56,21 +63,75 @@ class Alert(Renderer):
         page = entry.page
 
         # if there's nothing to do
-        if len(page) < 2:
+        if not page:
             # bail
             return
 
+        # get the notes
+        notes = entry.notes
+        # get the metadata
+        severity = notes["severity"]
+
+        # get the colors
+        resetColor = palette["reset"]
+        bodyColor = palette["body"]
+        severityColor = palette[severity]
+
         # otherwise, go through the page contents
-        for line in page[1:]:
+        for line in page:
+            # assemble and push
+            yield f"{severityColor}{self.marker}{bodyColor}{line}{resetColor}"
+
+        # get the name of the file
+        filename = notes["filename"]
+        # get the line number
+        line = str(notes["line"])
+        # get the function name
+        function = notes["function"]
+
+        # consider it an indication that we have location information
+        if filename:
+            # lookup the longest filename we are going to print
+            maxlen = self.maxlen
+            # if the filename is too long
+            if len(filename) > maxlen:
+                # compute the leading part
+                filenameLeader = filename[:maxlen//4-3]
+                # the ellipsis
+                ellipsis = "..."
+                # and the trailing part of the filename
+                filenameTrailer = filename[-3*maxlen//4:]
+            # otherwise
+            else:
+                # the leading part is the whole thing
+                filenameLeader = filename
+                # no ellipsis
+                ellipsis = ""
+                # no trailing part
+                filenameTrailer = ""
+
             # make a buffer
             buffer = [
-                palette["body"], line, palette["reset"]
+                # a marker
+                severityColor, self.marker, bodyColor,
+                # the filename
+                f"{filenameLeader}{ellipsis}{filenameTrailer}:",
+                # the line number
+                f"{line}:" if line else "",
+                # the function name
+                f"{function}" if function else ""
                 ]
-            # assemble and push
+
+            # assemble the line and make it available
             yield ''.join(buffer)
 
         # all done
         return
+
+
+    # implementation details
+    maxlen = 60
+    marker = " >> "
 
 
 # end of file
