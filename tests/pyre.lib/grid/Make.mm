2c1d94c92778a5ad73035d22354298c884508231
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-19 18:05:07 -0800

    pyre::grid: migrated the test suite from {p2}

--- tests/pyre.lib/grid/Make.mm
+++ tests/pyre.lib/grid/Make.mm
@@ -11,8 +11,63 @@ include pyre.def
 
 # the pile of tests
 LAYOUT_TESTS = \
+    canonical_box.cc \
+    canonical_box_skip.cc \
+    canonical_cslice.cc \
+    canonical_map.cc \
+    canonical_map_origin.cc \
+    canonical_map_positive.cc \
+    canonical_nudge.cc \
+    canonical_sanity.cc \
+    canonical_slice.cc \
+    canonical_visit.cc \
+    canonical_visit_order.cc \
+    index_access.cc \
+    index_arithmetic.cc \
+    index_cartesian.cc \
+    index_enum.cc \
+    index_fill.cc \
+    index_from_tuple.cc \
+    index_iterator.cc \
+    index_sanity.cc \
+    index_scaling.cc \
+    index_structured_binding.cc \
+    index_zero.cc \
+    order_access.cc \
+    order_c.cc \
+    order_fortran.cc \
+    order_sanity.cc \
+    product_access.cc \
+    product_iteration.cc \
+    product_ordered_iteration.cc \
+    product_sanity.cc \
+    rep_at.cc \
+    rep_eq.cc \
+    rep_fill.cc \
+    rep_iteration.cc \
+    rep_op.cc \
+    rep_reverse_iteration.cc \
+    rep_sanity.cc \
+    rep_zero.cc \
+    sanity.cc \
+    shape_access.cc \
+    shape_arithmetic.cc \
+    shape_cartesian.cc \
+    shape_sanity.cc \
+    shape_scaling.cc \
+    shape_structured_binding.cc
 
 GRID_TESTS = \
+    grid_heap_box.cc \
+    grid_heap_box_skip.cc \
+    grid_heap_expand.cc \
+    grid_heap_iteration.cc \
+    grid_heap_sanity.cc \
+    grid_mmap_sanity.cc \
+    grid_mmap_set.cc \
+    grid_mmap_get.cc \
+    grid_sat.cc \
+    grid_sat_box.cc
 
 all: test clean
 
