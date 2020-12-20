2f732ce494294456321b615e21c8d0db6617070c
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-19 18:01:02 -0800

    pyre::memory: migrated the test suite from {p2}

--- tests/pyre.lib/memory/Make.mm
+++ tests/pyre.lib/memory/Make.mm
@@ -11,6 +11,23 @@ include pyre.def
 
 # the pile of tests
 TESTS = \
+    constmap_oob.cc \
+    constmap_read.cc \
+    constview_access.cc \
+    filemap_create.cc \
+    filemap_write.cc \
+    filemap_read.cc \
+    heap_access.cc \
+    heap_borrow.cc \
+    heap_copy.cc \
+    heap_oob.cc \
+    map_create.cc \
+    map_oob.cc \
+    map_read.cc \
+    map_write.cc \
+    memory_sanity.cc \
+    view_access.cc \
+    view_oob.cc \
 
 # tests that should fail because their access patterns are prohibited
 SHOULD_FAIL = \
