2e3f03bfea8753f384cd711e9c852f5e28fe7dc0
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-21 14:07:03 -0800

    pyre::timers: moved the {p2} implementation over

---
+++ lib/pyre/timers/api.h
@@ -0,0 +1,22 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+// code guard
+#if !defined(pyre_timers_api_h)
+#define pyre_timers_api_h
+
+
+// package api
+namespace pyre::timers {
+    // wall clock timer
+    using wall_timer_t = Timer<WallClock, Proxy>;
+    // process CPU time
+    using process_timer_t = Timer<ProcessClock, Proxy>;
+}
+
+
+#endif
+
+// end of file
