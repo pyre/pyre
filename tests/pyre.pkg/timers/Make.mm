87a13514827e7a4956574bfe2fdfb9af17575aa1
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-21 14:58:26 -0800

    pyre.timers: cleared out the test suite to make room for the new implementation

--- tests/pyre.pkg/timers/Make.mm
+++ tests/pyre.pkg/timers/Make.mm
@@ -8,24 +8,8 @@
 
 PROJECT = pyre
 
-#--------------------------------------------------------------------------
-#
-
 all: test
 
-test: sanity python native pyre
-
-sanity:
-	${PYTHON} ./sanity.py
-
-python:
-	${PYTHON} ./python_timer.py
-	${PYTHON} ./python_timer_errors.py
-
-native:
-	${PYTHON} ./native_timer.py
-
-pyre:
-	${PYTHON} ./pyre_timer.py
+test:
 
 # end of file
