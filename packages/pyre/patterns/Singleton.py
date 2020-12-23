bca66581cca577792221df31195be0b8c6ee45f3
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-23 10:59:26 -0800

    pyre.patterns: migrated the updated implementation of {singleton} from {p2}

--- packages/pyre/patterns/Singleton.py
+++ packages/pyre/patterns/Singleton.py
@@ -1,32 +1,58 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2020 all rights reserved
-#
 
 
-from .AbstractMetaclass import AbstractMetaclass
+# superclass
+from .Type import Type
 
 
-class Singleton(AbstractMetaclass):
+# declaration
+class Singleton(Type):
     """
     Provide support for classes that create and manage a single instance
 
     Adapted from Michele Simionato's implementation
+
+    N.B.: the singleton constructor is called only the first time the singleton is accessed, so
+    you have to make sure that the first access is the correct one if the class has a
+    non-trivial constructor. Subsequent calls do not have to worry about the constructor
+    signature since it will not be invoked ever again
     """
 
 
-    def __call__(cls, **kwds):
+    # constants
+    null = object() # a marker to indicate an uninitialized singleton
+
+
+    # metamethods
+    def __init__(self, name, bases, attributes, **kwds):
         """
-        Build and return the singleton instance every time the constructor is called
+        Initialize a new class record
         """
-        # attempt to access the class instance
-        try:
-            return cls._pyre_singletonInstance
-        except AttributeError:
-            cls._pyre_singletonInstance = it = super().__call__(**kwds)
-            return it
+        # chain up
+        super().__init__(name, bases, attributes, **kwds)
+        # reset the unique instance
+        self.pyre_singletonInstance = self.null
+        # all done
+        return
+
+
+    def __call__(self, **kwds):
+        """
+        Return the singleton instance every time the constructor is called
+        """
+        # get the singleton instance
+        it = self.pyre_singletonInstance
+        # if {it} doesn't exist yet
+        if it is self.null:
+            # build it
+            it = super().__call__(**kwds)
+            # and attach it
+            self.pyre_singletonInstance = it
+        # in either case, publish it
+        return it
 
 
 # end of file
