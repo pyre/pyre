137d3745f9033426b96f7400db43ec04b96517b2
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-23 13:30:28 -0800

    pyre.patterns: simplified the {extent} and {singleton} test cases

--- tests/pyre.pkg/patterns/singleton.py
+++ tests/pyre.pkg/patterns/singleton.py
@@ -1,41 +1,23 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2020 all rights reserved
-#
-
-
-"""
-Verify that the pyre implementation of the Singleton pattern works as advertised
-"""
 
 
 def test():
+    """
+    Sanity test: make sure the module is accessible
+    """
+    # access
     from pyre.patterns.Singleton import Singleton
-
-    class singleton(metaclass=Singleton):
-
-        def __init__(self):
-            self.value = 1
-            return
-
-    a = singleton()
-    a.value = 2
-    b = singleton()
-
-    assert a.value == 2
-    assert b.value == 2
-
-    return singleton
+    # all done
+    return
 
 
 # main
 if __name__ == "__main__":
-    # skip pyre initialization since we don't rely on the executive
-    pyre_noboot = True
-    # do...
+    # run the test
     test()
 
 
