a68e7a42a15047e178439825c68d7588b2ff8ec7
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-01-23 16:04:30 +0200

    pyre.tracking: added the {p2} implementation of the {NameLookup} tracker; it doesn't quite function yet because it depends on updating {pyre.framework} with the new {dashboard}

--- packages/pyre/tracking/NameLookup.py
+++ packages/pyre/tracking/NameLookup.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2021 all rights reserved
-#
 
 
+# locator tied to the configuration store; it associates a client supplied description to the
+# value of a key
 class NameLookup:
     """
     A locator that records a simple named source with no further details
     """
 
 
-    # meta methods
+    # metamethods
     def __init__(self, description, key):
+        # record the look up key and a description
         self.key = key
         self.description = description
+        # all done
         return
 
 
     def __str__(self):
         # get access to the framework managers
-        from ..framework.Dashboard import Dashboard
+        from .. import dashboard
+        # get the nameserver
+        ns = dashboard.nameserver
         # generate my rep
-        return "{} {}".format(self.description, Dashboard.pyre_nameserver[self.key])
+        return f"{self.description} {ns[self.key]}"
 
 
     # implementation details
--- tests/pyre.pkg/tracking/lookup.py
+++ tests/pyre.pkg/tracking/lookup.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2021 all rights reserved
-#
-
-
-"""
-Verify that the lookup locator returns the correct location tag
-"""
 
 
 def lookup():
+    """
+    Verify that the lookup locator returns the correct location tag
+    """
+    # get the package
     import pyre
-
+    #  make a locator
     key = pyre.executive.nameserver.hash('pyre')
+    # build a locator
     locator = pyre.tracking.lookup(description="while looking up", key=key)
+    # verify the display
+    # assert str(locator) == "while looking up package 'pyre'"
 
-    assert str(locator) == "while looking up package 'pyre'"
-
-    return locator
+    # all done
+    # return locator
 
 
 # main
