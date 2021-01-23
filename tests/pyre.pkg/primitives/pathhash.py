b12f0983b9ed29cb448f7248918efd06e25bbb0c
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-01-23 13:34:15 +0200

    pyre.primitives: {p2} upgrades to the {pathhash} test case

--- tests/pyre.pkg/primitives/pathhash.py
+++ tests/pyre.pkg/primitives/pathhash.py
@@ -1,46 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2021 all rights reserved
-#
-
-
-"""
-Check that path hashes work as advertised
-"""
 
 
 def test():
+    """
+    Check that path hashes work as advertised
+    """
     # access the class
     import pyre.primitives
     # build one
     pathhash = pyre.primitives.pathhash()
-    # here are a couple of multi-level addresses
+    # here is a couple of multi-level addresses
     separator = '.'
     moduleName = "pyre.primitives.PathHash".split(separator)
-    klassName = moduleName + ["PathHash"]
+    clsName = moduleName + ["PathHash"]
 
     # now hash the matching nodes
     module = pathhash.hash(moduleName)
-    klass = pathhash.hash(items=klassName)
-    # check that i get the same node the second time i retrieve it
-    assert module == pathhash.hash(items=moduleName)
-    assert klass == pathhash.hash(items=klassName)
-    # check that i can retrieve the class from within the module
-    assert klass == module.hash(items=["PathHash"])
+    cls = pathhash.hash(items=clsName)
 
-    # build an alias for the module
+    # check that i get the same node the second time i retrieve it
+    assert module is pathhash.hash(items=moduleName)
+    assert cls is pathhash.hash(items=clsName)
+    # check that i can retrieve the class from within the module
+    assert cls is module.hash(items=["PathHash"])
+
+    # set up an alias for the module
     base = pathhash.hash(items=['pyre'])
     alias = "pathhash"
     original = pathhash.hash(items="pyre.primitives.PathHash".split(separator))
-
+    # alias the two nodes
     base.alias(alias=alias, target=original)
+
     # check that the alias points where it should
-    assert module == pathhash.hash(items="pyre.pathhash".split(separator))
+    assert module is pathhash.hash(items="pyre.pathhash".split(separator))
     # and that both contain the same class
-    assert klass == pathhash.hash(items="pyre.pathhash.PathHash".split(separator))
+    assert cls is pathhash.hash(items="pyre.pathhash.PathHash".split(separator))
 
     # dump out the contents of the hash
     # pathhash.dump()
@@ -51,8 +49,6 @@ def test():
 
 # main
 if __name__ == "__main__":
-    # skip pyre initialization since we don't rely on the executive
-    pyre_noboot = True
     # do...
     test()
 
