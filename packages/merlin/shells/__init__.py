98b51b6748cd2c147f5cf0bc4489dc9a6716bf86
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-07 21:47:20 +0300

    merlin.shells: added the necessary support for building a modern {pyre} app

---
+++ packages/merlin/shells/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# the action protocol
+from .Action import Action as action
+# and the base command panel
+from .Command import Command as command
+
+# factories for the local objects
+from .Merlin import Merlin as merlin
+
+
+# end of file
