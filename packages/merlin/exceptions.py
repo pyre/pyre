644f4f57a4ae4cca52f2ac8380427fd7b75db37e
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-07 21:48:17 +0300

    merlin: added a base for merlin {exceptions}

---
+++ packages/merlin/exceptions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# get the framework
+import pyre
+
+
+# the base class for my exceptions
+class MerlinError(pyre.PyreError):
+    """
+    Base class for all merlin errors
+    """
+
+
+# end of file
