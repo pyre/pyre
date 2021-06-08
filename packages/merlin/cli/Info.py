68ac9e53bb19d7b5f47d2ded6e1994839301969b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-08 12:36:48 +0300

    merlin.cli: added {info} panel with the {user} details

--- packages/merlin/cli/Info.py
+++ packages/merlin/cli/Info.py
@@ -27,11 +27,34 @@ class Info(merlin.shells.command, family='merlin.cli.info'):
         # make a channel
         channel = journal.info("merlin.info.host")
         # report
-        channel.line(f"      name: {host.hostname}")
-        channel.line(f"  nickname: {host.nickname}")
-        channel.line(f"        os: {host.platform} {host.release} ({host.codename})")
-        channel.line(f"      arch: {host.cpus.architecture}")
-        channel.line(f"     cores: {host.cpus.cores}")
+        channel.line(f"         name: {host.hostname}")
+        channel.line(f"     nickname: {host.nickname}")
+        channel.line(f"           os: {host.platform} {host.release} ({host.codename})")
+        channel.line(f"         arch: {host.cpus.architecture}")
+        channel.line(f"        cores: {host.cpus.cores}")
+        # flush
+        channel.log()
+
+        # all done
+        return
+
+
+    @merlin.export(tip="display information about the user")
+    def user(self, plexus, **kwds):
+        """
+        Display information about the host
+        """
+        # get the host
+        user = plexus.pyre_user
+
+        # make a channel
+        channel = journal.info("merlin.info.user")
+        # report
+        channel.line(f"     username: {user.username} ({user.uid})")
+        channel.line(f"         home: {user.home}")
+        channel.line(f"         name: {user.name}")
+        channel.line(f"        email: {user.email}")
+        channel.line(f"  affiliation: {user.affiliation}")
         # flush
         channel.log()
 
