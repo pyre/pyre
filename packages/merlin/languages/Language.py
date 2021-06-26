a09e7906444d96377fab9db6a6e3cc0c6c7cf567
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-26 16:08:43 +0300

    merlin.languages: added a base class for the for implementing {language} support

---
+++ packages/merlin/languages/Language.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+
+
+# class declaration
+class Language(merlin.component, implements=merlin.protocols.language):
+    """
+    A high level container of artifacts
+    """
+
+
+# end of file
