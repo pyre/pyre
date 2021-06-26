3b3b186a32307cb993faf8a4f09df7f3ea9596e4
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-26 16:09:29 +0300

    merlin.languages: primed the {C} language implementation

---
+++ packages/merlin/languages/C.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# superclass
+from .Language import Language
+
+
+# class declaration
+class C(Language, family="merlin.projects.languages.c"):
+    """
+    A category of source artifacts encoded in the C programming language
+    """
+
+
+# end of file
