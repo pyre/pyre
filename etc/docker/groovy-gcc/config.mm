2eedc0b98b4ac8642f8f85cdcc441aa965fa5c37
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-26 10:10:40 +0300

    etc/docker: added {groovy-gcc}

---
+++ etc/docker/groovy-gcc/config.mm
@@ -0,0 +1,42 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# external dependencies
+# system tools
+sys.prefix := /usr
+sys.lib := ${sys.prefix}/lib
+sys.libx86 := ${sys.lib}/x86_64-linux-gnu
+
+# gsl
+gsl.version := 2.5
+gsl.dir := $(sys.prefix)
+# mpi
+mpi.version := 4.0.3
+mpi.flavor := openmpi
+mpi.dir := ${sys.libx86}/openmpi
+mpi.executive := mpiexec --allow-run-as-root
+# numpy
+numpy.version := 1.17.4
+numpy.dir := $(sys.prefix)/lib/python3/dist-packages/numpy/core
+# pybind11
+pybind11.version := 2.4.3
+pybind11.dir = $(sys.prefix)
+# python
+python.version := @PYTHON_VERSION@
+python.dir := $(sys.prefix)
+
+# install locations
+# this is necessary in order to override {mm} appending the build type to the install prefix
+builder.dest.prefix := $(project.prefix)/
+# install the pyton packages straight where they need to go
+builder.dest.pyc := $(sys.prefix)/lib/python3/dist-packages/
+
+# control over the build process
+# set the python compiler so we don't depend on the symbolic link, which may not even be there
+compiler.python := python$(python.version)
+
+
+# end of file
