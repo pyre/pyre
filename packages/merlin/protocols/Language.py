04e172249c417c259edba7ecb886a7baa8f2da00
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-26 15:37:26 +0300

    merlin.protocols: added {language} as a category of source artifacts that share common workflows

---
+++ packages/merlin/protocols/Language.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+
+
+# class declaration
+class Language(merlin.protocol, family="merlin.projects.languages"):
+    """
+    A category of source artifacts, usually associated with a family of processing workflows
+    """
+
+
+# end of file
