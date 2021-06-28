2a7e641d76044713ce170a250d843b5785097cae
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-06-28 08:59:23 +0300

    merlin.languages: added {cuda}

---
+++ packages/merlin/languages/CUDA.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# superclass
+from .Language import Language
+
+
+# class declaration
+class CUDA(Language, family="merlin.projects.languages.cuda"):
+    """
+    A category of source artifacts encoded in the C programming language
+    """
+
+    # constants
+    name= "CUDA"
+
+
+    # user configurable state
+    sources = merlin.properties.strings()
+    sources.default = [".cu"]
+    sources.doc = "the set of suffixes that identify an artifact as a source"
+
+    headers = merlin.properties.strings()
+    headers.default = [".h"]
+    headers.doc = "the set of suffixes that identify an artifact as a header"
+
+
+# end of file
