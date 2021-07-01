65917a812c30d9d36137a8ee5a15cfec0a5f9ef1
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-01 18:28:27 +0300

    merlin.compilers: added the {gnu} compilers

---
+++ packages/merlin/compilers/gnu/Suite.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import merlin
+
+
+# the GCC compiler suite
+class Suite(merlin.component,
+            family="merlin.compilers.gnu", implements=merlin.protocols.compiler):
+    """
+    The GCC compiler suite
+    """
+
+
+# end of file
