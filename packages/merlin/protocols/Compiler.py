80e10908b051ff47c9627c51b972142fc7dcceb3
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-02 12:08:53 +0300

    merlin.protocols: {compiler} now passes user supplied names through the table of known {aliases}

--- packages/merlin/protocols/Compiler.py
+++ packages/merlin/protocols/Compiler.py
@@ -15,4 +15,19 @@ class Compiler(merlin.protocol, family="merlin.compilers"):
     """
 
 
+    # framework hooks
+    @classmethod
+    def pyre_convert(cls, value, **kwds):
+        """
+        Translate the component specification in {value} into canonical form; invoked during value
+        processing
+        """
+        # pull the known aliases from {compilers}
+        from merlin.compilers import aliases
+        # attempt to convert
+        value = aliases.get(value, value)
+        # chain up
+        return super().pyre_convert(value=value, **kwds)
+
+
 # end of file
