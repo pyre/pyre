9d80695e36667e71f29dab1b4380d868c540022d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-15 08:32:00 +0300

    journal: added tests for the bindings of the {help_t} channel

---
+++ tests/journal.ext/help_example.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+def test():
+    """
+    Exercise the help channel with a realistic example
+    """
+    # get the trash can
+    from journal.ext.journal import Trash as trash
+    # and the channel
+    from journal.ext.journal import Help as help
+
+    # make a help channel
+    channel = help(name="tests.journal.help")
+    # send the output to trash
+    channel.device = trash()
+
+    # add some metadata
+    channel.notes["time"] = "now"
+    # inject
+    channel.line("help channel:")
+    channel.log("    hello world!")
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
