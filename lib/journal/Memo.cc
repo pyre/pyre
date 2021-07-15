8959d0dcc96758ede7615e0d7206e7ae85271b6b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-15 14:39:19 +0300

    journal: reformatted the {memo_t} output

--- lib/journal/Memo.cc
+++ lib/journal/Memo.cc
@@ -22,20 +22,17 @@
 
 
 // metamethods
-pyre::journal::Memo::
-~Memo()
-{}
+pyre::journal::Memo::~Memo() {}
 
 
 // implementation details
 void
-pyre::journal::Memo::
-header(palette_type & palette, linebuf_type & buffer, const entry_type & entry) const
+pyre::journal::Memo::header(
+    palette_type & palette, linebuf_type & buffer, const entry_type & entry) const
 {
     // get the page
     auto & page = entry.page();
-
-    // if there is no contents
+    // if there are no contents
     if (page.empty()) {
         // we are done
         return;
@@ -43,12 +40,10 @@ header(palette_type & palette, linebuf_type & buffer, const entry_type & entry) 
 
     // get the notes
     auto & notes = entry.notes();
-
     // get the channel severity
     auto severity = notes.at("severity");
-
-    // mark the beginning of a message
-    line_type marker { _marker };
+    // and its name
+    auto channel = notes.at("channel");
 
     // attempt to get location information
     // N.B.: we only print line number and function name if we know the filename
@@ -66,10 +61,8 @@ header(palette_type & palette, linebuf_type & buffer, const entry_type & entry) 
         // so that names that are longer than this maximum
         if (len > maxlen) {
             // get shortened
-            buffer
-                << filename.substr(0, maxlen/4 - 3)
-                << "..."
-                << filename.substr(len - 3*maxlen/4);
+            buffer << filename.substr(0, maxlen / 4 - 3) << "..."
+                   << filename.substr(len - 3 * maxlen / 4);
         } else {
             // otherwise, render the whole name
             buffer << filename;
@@ -95,35 +88,25 @@ header(palette_type & palette, linebuf_type & buffer, const entry_type & entry) 
         // wrap up the location info
         buffer << std::endl;
     }
-
-    // render the channel name and severity
-    buffer
-        // start things off with the marker
-        << palette[severity] << marker << palette["reset"]
-        // and the channel name
-        << palette[severity] << notes.at("channel") << palette["reset"]
-        // and the severity
-        << " (" << palette[severity] << severity << palette["reset"] << ")"
-        // done with the into line
-        << std::endl;
-
-    // now for the extra notes, if any
-    // build a set of the keys we have processed already
-    std::set<entry_type::key_type> known { "severity", "channel", "filename", "line", "function" };
-    // go through the notes
-    for (auto & [key, value] : notes) {
-        // if the key is in the known pile
-        if (known.find(key) != known.end()) {
-            // ignore it
-            continue;
-        }
-        // otherwise, render it
+    // if we don't have location information
+    else {
+        // we show some basic channel information instead
         buffer
-            << palette[severity] << marker << palette["reset"]
-            << palette[severity] << key << palette["reset"]
-            << ": "
-            << palette[severity] << value << palette["reset"]
-            << std::endl;
+            // print the severity
+            << palette[severity] << severity << palette["reset"] << ":" << std::endl;
+
+        // at the correct {decor} level
+        if (chronicler_t::decor() > 1) {
+            // print
+            buffer
+                // a marker
+                << palette[severity] << _headerMarker
+                << palette["reset"]
+                // and a warning that there is no location information
+                << "location information is not available"
+                // done
+                << std::endl;
+        }
     }
 
     // all done
@@ -132,12 +115,11 @@ header(palette_type & palette, linebuf_type & buffer, const entry_type & entry) 
 
 
 void
-pyre::journal::Memo::
-body(palette_type & palette, linebuf_type & buffer, const entry_type & entry) const
+pyre::journal::Memo::body(
+    palette_type & palette, linebuf_type & buffer, const entry_type & entry) const
 {
     // get the page
     auto & page = entry.page();
-
     // if the page is empty
     if (page.empty()) {
         // nothing to do
@@ -146,19 +128,89 @@ body(palette_type & palette, linebuf_type & buffer, const entry_type & entry) co
 
     // get the notes
     auto & notes = entry.notes();
-
-    // make a marker
-    line_type marker { " -- "  };
     // get the channel severity
     auto severity = notes.at("severity");
 
     // go through the lines in the page
     for (auto & line : page) {
         // and render them
+        buffer << palette[severity] << _bodyMarker << palette["reset"] << palette["body"] << line
+               << palette["reset"] << std::endl;
+    }
+
+    // all done
+    return;
+}
+
+
+void
+pyre::journal::Memo::footer(
+    palette_type & palette, linebuf_type & buffer, const entry_type & entry) const
+{
+    // get the page
+    auto & page = entry.page();
+    // if the page is empty
+    if (page.empty()) {
+        // nothing to do
+        return;
+    }
+
+    // get the notes
+    auto & notes = entry.notes();
+    // get the channel severity
+    auto severity = notes.at("severity");
+    // and its name
+    auto channel = notes.at("channel");
+
+    if (chronicler_t::decor() > 2) {
+        // look for the application name
+        auto loc = notes.find("application");
+        // if it's there
+        if (loc != notes.end()) {
+            // get the value
+            auto app = loc->second;
+            // render the app name
+            buffer
+                // first a marker
+                << palette[severity] << _footerMarker
+                << palette["reset"]
+                // inject the application name
+                << "from application " << palette[severity] << app
+                << palette["reset"]
+                // done
+                << std::endl;
+        }
+
+        // render the channel name and severity
         buffer
-            << palette[severity] << marker << palette["reset"]
-            << palette["body"] << line << palette["reset"]
+            // start things off with the marker
+            << palette[severity] << _footerMarker
+            << palette["reset"]
+            // the channel severity and name
+            << "because the " << palette[severity] << severity << palette["reset"] << " channel '"
+            << palette[severity] << channel << palette["reset"]
+            << "' is active"
+            // done with the intro line
             << std::endl;
+    }
+
+    // now for the extra notes, if any
+    if (chronicler_t::decor() > 1) {
+        // build a set of the keys we have processed already
+        std::set<entry_type::key_type> known { "severity", "channel",  "filename",
+                                               "line",     "function", "application" };
+        // go through the notes
+        for (auto & [key, value] : notes) {
+            // if the key is in the known pile
+            if (known.find(key) != known.end()) {
+                // ignore it
+                continue;
+            }
+            // otherwise, render it
+            buffer << palette[severity] << _footerMarker << palette["reset"] << palette[severity]
+                   << key << palette["reset"] << ": " << palette[severity] << value
+                   << palette["reset"] << std::endl;
+        }
     }
 
     // all done
