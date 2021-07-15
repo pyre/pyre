c321729dfa772577faaa49df715f160ee03e6c82
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-15 09:02:27 +0300

    journal: added the {help} severity to the two console palettes

--- lib/journal/ErrorConsole.cc
+++ lib/journal/ErrorConsole.cc
@@ -37,13 +37,10 @@
 
 // metamethods
 // constructor
-pyre::journal::ErrorConsole::
-ErrorConsole() :
-    Stream("cerr", std::cerr),
-    _tty(isatty(2) == 1)
+pyre::journal::ErrorConsole::ErrorConsole() : Stream("cerr", std::cerr), _tty(isatty(2) == 1)
 {
     // if i am connected to a compatible terminal
-    if (_tty && ansi_t::compatible())  {
+    if (_tty && ansi_t::compatible()) {
         // populate my palette with some colors
         // put things back to normal
         _palette["reset"] = ansi_t::x11("normal");
@@ -53,6 +50,7 @@ ErrorConsole() :
         _palette["info"] = ansi_t::x11("forest green");
         _palette["warning"] = ansi_t::x11("orange");
         _palette["error"] = ansi_t::x11("red");
+        _palette["help"] = ansi_t::x11("cyan");
         _palette["debug"] = ansi_t::x11("cornflower blue");
         _palette["firewall"] = ansi_t::x11("fuchsia");
         // the page body
@@ -62,9 +60,7 @@ ErrorConsole() :
 
 
 // destructor
-pyre::journal::ErrorConsole::
-~ErrorConsole()
-{}
+pyre::journal::ErrorConsole::~ErrorConsole() {}
 
 
 // end of file
