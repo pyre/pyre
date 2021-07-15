bcf753e3b9c0510d912af0cbd1bbf358cde3769f
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-15 15:02:17 +0300

    journal: the bindings now provide convenient access to the {decor} and {detail} controls of {chronicler_t}

--- extensions/journal/api.cc
+++ extensions/journal/api.cc
@@ -12,38 +12,56 @@
 
 // add bindings to the inventory
 void
-pyre::journal::py::
-api(py::module & m) {
+pyre::journal::py::api(py::module & m)
+{
     // easy access to the manager of the global state
     m.attr("chronicler") = m.attr("Chronicler");
 
     // registration of the application name
-    m.def("application",
-          // the implementation
-          &pyre::journal::application, "name"_a,
-          // the docstring
-          "register the application {name}"
-          );
+    m.def(
+        "application",
+        // the implementation
+        &pyre::journal::application, "name"_a,
+        // the docstring
+        "register the application {name}");
 
     // suppress all output
-    m.def("quiet",
-          // the implementation
-          &pyre::journal::quiet,
-          // the docstring
-          "suppress all channel output"
-          );
+    m.def(
+        "quiet",
+        // the implementation
+        &pyre::journal::quiet,
+        // the docstring
+        "suppress all channel output");
 
     // send output to a log file
-    m.def("logfile",
-          // the implementation
-          [](const debug_t::string_type & path) {
-              pyre::journal::logfile(path);
-          },
-          // the docstring
-          "send all output to a file",
-          // the arguments
-          "name"_a
-          );
+    m.def(
+        "logfile",
+        // the implementation
+        [](const debug_t::string_type & path) { pyre::journal::logfile(path); },
+        // the docstring
+        "send all output to a file",
+        // the arguments
+        "name"_a);
+
+    // set the maximum message decoration level
+    m.def(
+        "decor",
+        // the implementation
+        [](chronicler_t::detail_type level) { chronicler_t::decor(level); },
+        // the docstring
+        "set the maximum message decoration level",
+        // the arguments
+        "level"_a);
+
+    // set the maximum message detail level
+    m.def(
+        "detail",
+        // the implementation
+        [](chronicler_t::detail_type level) { chronicler_t::detail(level); },
+        // the docstring
+        "set the maximum message detail level",
+        // the arguments
+        "level"_a);
 
     // all done
     return;
