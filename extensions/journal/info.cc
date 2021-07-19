939599c810429b26b0d63614effbbf0c80387cc9
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-19 16:11:50 +0300

    journal: cleaned up the {info_t} bindings

--- extensions/journal/info.cc
+++ extensions/journal/info.cc
@@ -32,7 +32,11 @@ pyre::journal::py::info(py::module & m)
     // the info channel interface
     py::class_<info_t>(m, "Informational")
         // the constructor
-        .def(py::init<const string_t &>(), "name"_a)
+        .def(
+            // the implementation
+            py::init<const string_t &>(),
+            // the signature
+            "name"_a)
 
         // accessors
         // the name; read-only property
@@ -78,13 +82,16 @@ pyre::journal::py::info(py::module & m)
             // the docstring
             "the output device")
 
-        // the content of the current entry
+        // the contents of the current entry
         .def_property_readonly(
             "page",
             // the getter
             // N.B. the explicit declaration of the λ return value is
             // critical in making the page read/write in python
-            [](info_t & channel) -> pyre::journal::page_t & { return channel.entry().page(); },
+            [](info_t & channel) -> pyre::journal::page_t & {
+                // get the entry page
+                return channel.entry().page();
+            },
             // the docstring
             "the contents of the current entry")
 
@@ -94,7 +101,10 @@ pyre::journal::py::info(py::module & m)
             // the getter
             // N.B. the explicit declaration of the λ return value is
             // critical in making the notes read/write in python
-            [](info_t & channel) -> pyre::journal::notes_t & { return channel.entry().notes(); },
+            [](info_t & channel) -> pyre::journal::notes_t & {
+                // get the entry notes
+                return channel.entry().notes();
+            },
             // the docstring
             "the notes associated with the current entry")
 
@@ -102,7 +112,10 @@ pyre::journal::py::info(py::module & m)
         .def_property_readonly_static(
             "ApplicationError",
             // the getter
-            [m](py::object) -> py::object { return m.attr("ApplicationError"); },
+            [m](py::object) -> py::object {
+                // this is a module level attribute
+                return m.attr("ApplicationError");
+            },
             // the docstring
             "the keeper of the global state")
 
@@ -110,7 +123,10 @@ pyre::journal::py::info(py::module & m)
         .def_property_readonly_static(
             "severity",
             // the getter
-            [](py::object) -> info_t::string_type { return "info"; },
+            [](py::object) -> info_t::string_type {
+                // hardwired, as it is unlikely to change
+                return "info";
+            },
             // the docstring
             "get the channel severity name")
 
@@ -118,7 +134,10 @@ pyre::journal::py::info(py::module & m)
         .def_property_readonly_static(
             "chronicler",
             // the getter
-            [m](py::object) -> py::object { return m.attr("Chronicler"); },
+            [m](py::object) -> py::object {
+                // access the class record registered with the module
+                return m.attr("Chronicler");
+            },
             // the docstring
             "the keeper of the global state")
 
@@ -126,7 +145,10 @@ pyre::journal::py::info(py::module & m)
         .def_property_readonly_static(
             "defaultActive",
             // the getter
-            [](py::object) -> info_t::active_type { return info_t::index().active(); },
+            [](py::object) -> info_t::active_type {
+                // ask my index
+                return info_t::index().active();
+            },
             // the docstring
             "the default state of info channels")
 
@@ -134,7 +156,10 @@ pyre::journal::py::info(py::module & m)
         .def_property_readonly_static(
             "defaultFatal",
             // the getter
-            [](py::object) -> info_t::fatal_type { return info_t::index().fatal(); },
+            [](py::object) -> info_t::fatal_type {
+                // ask my index
+                return info_t::index().fatal();
+            },
             // the docstring
             "the default fatality of info channels")
 
@@ -142,9 +167,15 @@ pyre::journal::py::info(py::module & m)
         .def_property_static(
             "defaultDevice",
             // the getter
-            [](py::object) -> info_t::device_type { return info_t::index().device(); },
+            [](py::object) -> info_t::device_type {
+                // my index knows
+                return info_t::index().device();
+            },
             // the setter
-            [](py::object, info_t::device_type device) { info_t::index().device(device); },
+            [](py::object, info_t::device_type device) -> void {
+                // ask my index to set the new device
+                info_t::index().device(device);
+            },
             // the docstring
             "the default device for all info channels")
 
@@ -169,37 +200,40 @@ pyre::journal::py::info(py::module & m)
         .def(
             "line",
             // the handler
-            [](info_t & channel, py::object msg) {
-                // cast to string
-                info_t::string_type message = py::str(msg);
+            [](info_t & channel, info_t::string_type message) -> info_t & {
                 // inject
                 channel << message << pyre::journal::newline;
+                // enable chaining
+                return channel;
             },
+            // the signature
+            "message"_a = "",
             // the docstring
-            "add another line to the message page",
-            // the arguments
-            "message"_a = "")
+            "add another line to the message page")
 
         // add a message to the channel and flush
         .def(
             "log",
             // the handler
-            [](info_t & channel, py::object msg) {
-                // cast to string
-                info_t::string_type message = py::str(msg);
+            [](info_t & channel, info_t::string_type message) -> info_t & {
                 // inject and flush
                 channel << locator() << message << pyre::journal::endl;
+                // enable chaining
+                return channel;
             },
+            // the signature
+            "message"_a = "",
             // the docstring
-            "add the optional {message} to the channel contents and then record the entry",
-            // the arguments
-            "message"_a = "")
+            "add the optional {message} to the channel contents and then record the entry")
 
         // operator bool
         .def(
             "__bool__",
             // the implementation
-            [](const info_t & channel) { return channel.active(); },
+            [](const info_t & channel) -> bool {
+                // interpret as a request for the activation state
+                return channel.active();
+            },
             // the docstring
             "syntactic sugar for checking the state of a channel")
 
@@ -215,11 +249,14 @@ pyre::journal::py::info(py::module & m)
         .def_static(
             "logfile",
             // the implementation
-            [](const info_t::string_type & path) { info_t::logfile(path); },
+            [](const info_t::string_type & path) -> void {
+                // set up the device
+                info_t::logfile(path);
+            },
+            // the signature
+            "name"_a,
             // the docstring
-            "send all output to a file",
-            // the arguments
-            "name"_a)
+            "send all output to a file")
 
         // done
         ;
