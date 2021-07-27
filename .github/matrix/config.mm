a270d641c7711c634b4dba3a561faf90773643cf
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-07-27 08:45:53 +0300

    github: added {matrix}, the folder with the {mm} configuration files

---
+++ .github/matrix/config.mm
@@ -0,0 +1,36 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# external dependencies
+# system tools
+sys.prefix := /usr
+sys.lib := ${sys.prefix}/lib
+sys.libx86 := ${sys.lib}/x86_64-linux-gnu
+
+# gsl
+gsl.version := 2.5
+gsl.dir := $(sys.prefix)
+# mpi
+mpi.version := 4.0.3
+mpi.flavor := openmpi
+mpi.dir := ${sys.libx86}/openmpi
+
+# python
+python.version := $(pythonVersion)
+python.dir := $(pythonLocation)
+# numpy
+numpy.version := 1.21.1
+numpy.dir := $(python.dir))/lib/python$(python.version)/site-packages/numpy/core
+# pybind11
+pybind11.version := 2.7.0
+pybind11.dir := $(python.dir))/lib/python$(python.version)/site-packages/pybind11/include
+
+# control over the build process
+# set the python compiler so we don't depend on the symbolic link, which may not even be there
+compiler.python := python$(python.version)
+
+
+# end of file
