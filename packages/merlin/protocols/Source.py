2c5468e7806bdf8de371e75757d37b01dce6eb88
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-01 12:21:57 +0300

    merlin.protocols: added a {source} specification

---
+++ packages/merlin/protocols/Source.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# schema
+from .Language import Language as language
+
+
+# class declaration
+class Source(merlin.protocol, family="merlin.projects.sources"):
+    """
+    Encapsulation of a file with source code
+    """
+
+
+    # user configurable state
+    language = merlin.properties.tuple(schema=language())
+    language.doc = "the language of the source code"
+
+
+    # framework hooks
+    @classmethod
+    def pyre_default(cls, **kwds):
+        """
+        Specify the default implementation
+        """
+        # grab the library foundry and return it
+        return merlin.projects.source
+
+
+# end of file
