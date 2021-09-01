c721b93cd36a372a3219c644b2f42e0460e8201e
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-01 12:22:58 +0300

    merlin.projects: primed the {source} implementer

---
+++ packages/merlin/projects/Source.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+
+
+# class declaration
+class Source(merlin.component,
+             family="merlin.projects.sources.source", implements=merlin.protocols.source):
+    """
+    Encapsulation of a source file
+    """
+
+
+
+# end of file
