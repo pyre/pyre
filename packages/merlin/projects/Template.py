4aaef86006f7ad5105db280d383ca1a60fa1fa7d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-02 08:55:55 +0300

    merlin.projects: added the {template} source type to mark {autogen} assets

---
+++ packages/merlin/projects/Template.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# superclass
+from .Source import Source
+
+
+# class declaration
+class Template(Source, family="merlin.projects.sources.template"):
+    """
+    Encapsulation of a template file that generates other sources
+    """
+
+
+# end of file
