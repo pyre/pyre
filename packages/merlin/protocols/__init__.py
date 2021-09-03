03cf088ab15fdf8420648c1ff3b48e00db1673bb
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-03 09:40:12 +0300

    merlin.protocols: added {foreign}, a new category that represents assets that are outside the scope of merlin

--- packages/merlin/protocols/__init__.py
+++ packages/merlin/protocols/__init__.py
@@ -9,6 +9,7 @@ from .Asset import Asset as asset
 from .Auxiliary import Auxiliary as auxiliary
 from .Compiler import Compiler as compiler
 from .Directory import Directory as directory
+from .Foreign import Foreign as foreign
 from .Language import Language as language
 from .Library import Library as library
 from .Project import Project as project
