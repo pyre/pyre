88bb455fbce13b6fb40c485bab13beb17e38e573
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-06 17:38:59 +0300

    merlin.projects: {file} is now the type of all file based project assets; further differentiation is accomplished by setting {category} to an appropriate value

---
+++ packages/merlin/projects/File.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# superclass
+from .Asset import Asset
+
+
+# class declaration
+class File(Asset,
+           family="merlin.projects.files.file",
+           implements=merlin.protocols.file):
+    """
+    Encapsulation of a file based project asset
+    """
+
+
+    # required configurable state
+    language = merlin.properties.str()
+    language.doc = "a clue about the toolchain that processes this asset"
+
+
+# end of file
