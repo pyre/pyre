8aacfff74f67a44cff53d51a9779b0805771b955
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-06 17:13:46 +0300

    merlin.protocols: introduced {assetCategory} as the protocol for specifying asset types

---
+++ packages/merlin/protocols/AssetCategory.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+
+
+# class declaration
+class AssetCategory(merlin.protocol, family="merlin.projects.categories"):
+    """
+    Protocol for all project asset categories
+    """
+
+
+# end of file
