2bcfa381d2a67a12a8d093e98413eff0d7243462
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-07 13:31:10 +0300

    pyre.protocols: {language} now uses {pyre_convert} to map common names to canonical ones

--- packages/merlin/protocols/Language.py
+++ packages/merlin/protocols/Language.py
@@ -24,9 +24,29 @@ class Language(merlin.protocol, family="merlin.languages"):
 
 
     # implementation details
-    # a map from the common name of a language to its supporting foundry; this index is
+    # a map from the common to the canonical name of a language; this index is
     # maintained automatically by the {merlin.components.language} foundry
-    supported = {}
+    aliases = {}
+
+
+    # framework hooks
+    @classmethod
+    def pyre_convert(cls, value, **kwds):
+        """
+        Translate the component specification in {value} into canonical form; invoked during value
+        processing
+        """
+        # turn the value into a {uri}
+        uri = cls.uri().coerce(value)
+        # extract the address bit
+        family = uri.address
+        # run it through the compiler aliases
+        family = cls.aliases.get(family, family)
+        # and reattach it
+        uri.address = family
+
+        # chain up
+        return super().pyre_convert(value=str(uri), **kwds)
 
 
 # end of file
