d5e62b70d65edc68dacf2d27d6af1776d9a0a3b2
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-12 15:06:52 +0300

    merlin.components: the {builder} now primes the virtual filesystem with its {prefix} and {stage}

--- packages/merlin/components/Builder.py
+++ packages/merlin/components/Builder.py
@@ -18,15 +18,45 @@ class Builder(merlin.component,
 
     # required state
     prefix = merlin.properties.path()
-    prefix.default = "./products"
+    prefix.default = "/tmp/{pyre.user.username}/products"
     prefix.doc = "the installation location of the final build products"
 
     stage = merlin.properties.path()
-    stage.default = "./builds"
+    stage.default = "/tmp/{pyre.user.username}/builds"
     stage.doc = "the location of the intermediate, disposable build products"
 
     prefixLayout = merlin.protocols.prefix()
     prefixLayout.doc = "the layout of the installation area"
+
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+
+        # grab the root of the virtual filesystem
+        vfs = self.pyre_fileserver
+
+        # get my prefix
+        prefix = self.prefix
+        # create it, if it doesn't already exist
+        prefix.mkdir(parents=True, exist_ok=True)
+        # anchor a filesystem
+        prefix = vfs.retrieveFilesystem(root=prefix)
+        # and mount it
+        vfs["prefix"] = prefix
+
+        # repeat for my staging area
+        stage = self.stage
+        # create it, if it doesn't already exist
+        stage.mkdir(parents=True, exist_ok=True)
+        # anchor a filesystem
+        stage = vfs.retrieveFilesystem(root=stage)
+        # and mount it
+        vfs["stage"] = stage
+
+        # all done
+        return
 
 
 # end of file
