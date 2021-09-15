b2e094012132005d67c420ee377d1f90625dbcda
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-15 09:52:42 +0300

    merlin.products: primed the base merlin {product}

---
+++ packages/merlin/products/Product.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# framework
+import merlin
+
+
+# base class for merlin products
+class Product(merlin.flow.product, implements=merlin.protocols.specification, internal=True):
+    """
+    The base class for {merlin} products
+    """
+
+
+# end of file
--- packages/merlin/products/__init__.py
+++ packages/merlin/products/__init__.py
@@ -4,4 +4,8 @@
 # (c) 1998-2021 all rights reserved
 
 
+# publish the base protocol so users can extend
+from .Product import Product as product
+
+
 # end of file
