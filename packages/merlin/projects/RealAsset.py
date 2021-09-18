41dff12fb730a6e52379cba04051ab491bfa1d76
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-18 08:47:37 +0300

    merlin.projects: introduced an {asset} class that captures the ones that have a physical footprint on the filesystem

---
+++ packages/merlin/projects/RealAsset.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import merlin
+# superclass
+from .Asset import Asset
+
+
+# class declaration
+class RealAsset(Asset):
+    """
+    Encapsulation of an asset with a physical presence on the filesystem
+    """
+
+
+    # meta methods
+    def __init__(self, node, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # store my node
+        self.node = node
+        # all done
+        return
+
+
+# end of file
