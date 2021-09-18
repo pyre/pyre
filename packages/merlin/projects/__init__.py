344e5f283b467af6b0d116a0ef2ea2ec4b2f95af
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-18 08:46:37 +0300

    merlin.projects: {asset} is no longer publicly visible

--- packages/merlin/projects/__init__.py
+++ packages/merlin/projects/__init__.py
@@ -8,7 +8,6 @@
 from .Product import Product as product
 
 # export the publicly visible assets
-from .Asset import Asset as asset
 from .Directory import Directory as directory
 from .File import File as file
 from .Project import Project as project
