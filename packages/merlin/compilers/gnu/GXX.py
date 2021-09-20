4e33f21b3700001aade04918023b9a8970f3c828
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-20 19:47:25 +0300

    merlin.compilers: fleshed out the {gnu} compilers a bit more: they now have a {driver} trait and can sniff their version

--- packages/merlin/compilers/gnu/GXX.py
+++ packages/merlin/compilers/gnu/GXX.py
@@ -4,6 +4,9 @@
 # (c) 1998-2021 all rights reserved
 
 
+# support
+import re
+import subprocess
 # framework
 import merlin
 
@@ -14,6 +17,61 @@ class GXX(merlin.component,
     """
     The C++ compiler from the GNU compiler suite
     """
+
+
+    # constants
+    suite = "gcc"
+    language = "c++"
+
+
+    # configurable state
+    driver = merlin.properties.path()
+    driver.default = "g++"
+
+
+    # interface
+    def version(self):
+        """
+        Retrieve the compiler version
+        """
+        # get the driver
+        driver = str(self.driver)
+
+        # set up the shell command
+        settings = {
+            'executable': driver,
+            'args': (driver, '--version'),
+            'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE,
+            'universal_newlines': True,
+            'shell': False
+        }
+        # make a pipe
+        with subprocess.Popen(**settings) as pipe:
+            # get the text source
+            stream = pipe.stdout
+
+            # the first line is the version
+            line = next(stream).strip()
+            # extract the fields
+            match = self._regex.match(line)
+            # if it didn't match
+            if not match:
+                # oh well...
+                return 'unknown', 'unknown', 'unknown'
+            # otherwise, extract the version fields
+            major = match.group("major")
+            minor = match.group("minor")
+            micro = match.group("micro")
+            # and return it
+            return major, minor, micro
+
+        # all done
+        return 'unknown', 'unknown', 'unknown'
+
+
+    # implementation details
+    _version = None
+    _regex = re.compile(r"g\+\+\s+\(.*\)\s+(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)")
 
 
 # end of file
