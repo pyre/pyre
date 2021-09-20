7a9bc5d59c2714732a4577b8f46bb3d31fbe8799
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-20 19:52:07 +0300

    merlin.components: added {abi} tagging to the {prefix} and {stage} of the {builder}

--- packages/merlin/components/Builder.py
+++ packages/merlin/components/Builder.py
@@ -34,6 +34,10 @@ class Builder(merlin.component,
     stage.default = "/tmp/{pyre.user.username}/builds"
     stage.doc = "the location of the intermediate, disposable build products"
 
+    prefixABI = merlin.properties.bool()
+    prefixABI.default = True
+    prefixABI.doc = "control whether to fold the compiler ABI into the installation prefix"
+
     prefixLayout = merlin.protocols.prefix()
     prefixLayout.doc = "the layout of the installation area"
 
@@ -82,11 +86,48 @@ class Builder(merlin.component,
         """
         Hook invoked after the {plexus} is fully initialized
         """
+        # we are looking to tag the build using information about the compilers in use
+        # this is really a poorly stated problem with many possible variables that could
+        # affect the binary compatibility of the build products; we punt and use the c++ or c
+        # compiler as the dominant factor
+        # so
+        determinant = None
+        # get the set of compilers and go through them
+        for compiler in plexus.compilers:
+            # if this is the c++ compiler
+            if compiler.language == "c++":
+                # mark it as the determining compiler
+                determinant = compiler
+                # we are done
+                break
+            # the c compiler
+            if compiler.language == "c":
+                # is a fallback
+                determinant = compiler
+        # if we didn't manage to find anything useful
+        if determinant is None:
+            # we won't add abi information to the products
+            abi = ""
+        # otherwise
+        else:
+            # get the compiler family
+            suite = compiler.suite
+            # and its version
+            major, _, _ = compiler.version()
+            # and fold them into the ABI
+            abi = f"{suite}{major}"
+
         # grab the root of the virtual filesystem
         vfs = self.pyre_fileserver
 
-        # assemble the path to my prefix
-        prefix = self.prefix
+        # if the user want the ABI folded into the prefix
+        if self.prefixABI:
+            # assemble the path to my prefix
+            prefix = self.prefix / abi / self.tag
+        # otherwise
+        else:
+            # assemble the path to my prefix
+            prefix = self.prefix
         # create it, if it doesn't already exist
         prefix.mkdir(parents=True, exist_ok=True)
         # anchor a filesystem
@@ -95,7 +136,7 @@ class Builder(merlin.component,
         vfs["prefix"] = prefix
 
         # assemble the path to my staging area
-        stage = self.stage
+        stage = self.stage / abi /self.tag
         # create it, if it doesn't already exist
         stage.mkdir(parents=True, exist_ok=True)
         # anchor a filesystem
