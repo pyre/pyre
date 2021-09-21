c8a1f0e0921730a5308078cf8ca54af1891e3e72
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-21 09:16:11 +0300

    merlin.compilers: refactored all the behavior common to the {gnu} compilers to a new base class

--- packages/merlin/compilers/gnu/GCC.py
+++ packages/merlin/compilers/gnu/GCC.py
@@ -9,69 +9,23 @@ import re
 import subprocess
 # framework
 import merlin
+# superclass
+from .GNU import GNU
 
 
 # the C compiler from the GNU compiler suite
-class GCC(merlin.component,
-          family="merlin.compilers.gnu.gcc", implements=merlin.protocols.compiler):
+class GCC(GNU, family="merlin.compilers.gnu.gcc"):
     """
     The C compiler from the GNU compiler suite
     """
 
 
     # constants
-    suite = "gcc"
     language = "c"
-
 
     # configurable state
     driver = merlin.properties.path()
     driver.default = "gcc"
-
-
-    # interface
-    def version(self):
-        """
-        Retrieve the compiler version
-        """
-        # get the driver
-        driver = str(self.driver)
-
-        # set up the shell command
-        settings = {
-            'executable': driver,
-            'args': (driver, '--version'),
-            'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE,
-            'universal_newlines': True,
-            'shell': False
-        }
-        # make a pipe
-        with subprocess.Popen(**settings) as pipe:
-            # get the text source
-            stream = pipe.stdout
-
-            # the first line is the version
-            line = next(stream).strip()
-            # extract the fields
-            match = self._regex.match(line)
-            # if it didn't match
-            if not match:
-                # oh well...
-                return 'unknown', 'unknown', 'unknown'
-            # otherwise, extract the version fields
-            major = match.group("major")
-            minor = match.group("minor")
-            micro = match.group("micro")
-            # and return it
-            return major, minor, micro
-
-        # all done
-        return 'unknown', 'unknown', 'unknown'
-
-
-    # implementation details
-    _version = None
-    _regex = re.compile(r"gcc\s+\(.*\)\s+(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)")
 
 
 # end of file
--- packages/merlin/compilers/gnu/GFortran.py
+++ packages/merlin/compilers/gnu/GFortran.py
@@ -9,69 +9,23 @@ import re
 import subprocess
 # framework
 import merlin
+# superclass
+from .GNU import GNU
 
 
 # the FORTRAN compiler from the GNU compiler suite
-class GFortran(merlin.component,
-          family="merlin.compilers.gnu.gfortran", implements=merlin.protocols.compiler):
+class GFortran(GNU, family="merlin.compilers.gnu.gfortran"):
     """
     The FORTRAN compiler from the GNU compiler suite
     """
 
 
     # constants
-    suite = "gcc"
     language = "fortran"
-
 
     # configurable state
     driver = merlin.properties.path()
     driver.default = "gfortran"
-
-
-    # interface
-    def version(self):
-        """
-        Retrieve the compiler version
-        """
-        # get the driver
-        driver = str(self.driver)
-
-        # set up the shell command
-        settings = {
-            'executable': driver,
-            'args': (driver, '--version'),
-            'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE,
-            'universal_newlines': True,
-            'shell': False
-        }
-        # make a pipe
-        with subprocess.Popen(**settings) as pipe:
-            # get the text source
-            stream = pipe.stdout
-
-            # the first line is the version
-            line = next(stream).strip()
-            # extract the fields
-            match = self._regex.match(line)
-            # if it didn't match
-            if not match:
-                # oh well...
-                return 'unknown', 'unknown', 'unknown'
-            # otherwise, extract the version fields
-            major = match.group("major")
-            minor = match.group("minor")
-            micro = match.group("micro")
-            # and return it
-            return major, minor, micro
-
-        # all done
-        return 'unknown', 'unknown', 'unknown'
-
-
-    # implementation details
-    _version = None
-    _regex = re.compile(r"GNU Fortran\s+\(.*\)\s+(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)")
 
 
 # end of file
