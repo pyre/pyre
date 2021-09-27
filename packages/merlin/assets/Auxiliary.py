0111bbd9728cbb6784909f2d074cc493e218f3dc
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-27 13:35:05 +0300

    merlin.asset: all {assetCategory} implementers now derive from {category} and participate in identifying themselves to visitors

--- packages/merlin/assets/Auxiliary.py
+++ packages/merlin/assets/Auxiliary.py
@@ -6,12 +6,12 @@
 
 # support
 import merlin
+# superclass
+from .Category import Category
 
 
 # class declaration
-class Auxiliary(merlin.component,
-                family="merlin.assets.categories.auxiliary",
-                implements=merlin.protocols.assetCategory):
+class Auxiliary(Category, family="merlin.assets.categories.auxiliary"):
     """
     The category of auxiliary assets
     """
@@ -19,6 +19,23 @@ class Auxiliary(merlin.component,
 
     # constants
     category = "auxiliary"
+
+
+    # interface
+    def identify(self, authority, **kwds):
+        """
+        Ask {authority} to process a header file
+        """
+        # attempt to
+        try:
+            # ask authority for a handler for my type
+            handler = authority.auxiliary
+        # if it doesn't exist
+        except AttributeError:
+            # chain up
+            return super().identify(authority=authority)
+        # if it does, invoke it
+        return handler(**kwds)
 
 
 # end of file
--- packages/merlin/assets/Unrecognizable.py
+++ packages/merlin/assets/Unrecognizable.py
@@ -6,12 +6,12 @@
 
 # support
 import merlin
+# superclass
+from .Category import Category
 
 
 # class declaration
-class Unrecognizable(merlin.component,
-                     family="merlin.assets.categories.unrecognizable",
-                     implements=merlin.protocols.assetCategory):
+class Unrecognizable(Category, family="merlin.assets.categories.unrecognizable"):
     """
     Encapsulation of a file whose purpose is not known
     """
@@ -19,6 +19,23 @@ class Unrecognizable(merlin.component,
 
     # constants
     category = "unrecognizable"
+
+
+    # interface
+    def identify(self, authority, **kwds):
+        """
+        Ask {authority} to process a header file
+        """
+        # attempt to
+        try:
+            # ask authority for a handler for my type
+            handler = authority.unrecognizable
+        # if it doesn't exist
+        except AttributeError:
+            # chain up
+            return super().identify(authority=authority)
+        # if it does, invoke it
+        return handler(**kwds)
 
 
 # end of file
