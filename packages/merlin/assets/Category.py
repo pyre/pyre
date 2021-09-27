6336ff0457be31f21f0d46123f794b1a4ec23c4e
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-27 13:33:53 +0300

    merlin.assets: added a base asset {category} with the terminal case for the category visitor

---
+++ packages/merlin/assets/Category.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import journal
+# framework
+import merlin
+
+
+# class declaration
+class Category(merlin.component,
+                family="merlin.assets.categories.category",
+                implements=merlin.protocols.assetCategory):
+    """
+    The base category for all file based assets
+    """
+
+
+    # constants
+    category = "unknown"
+
+
+    # interface
+    def identify(self, authority, **kwds):
+        """
+        Ask {authority} to process a header file
+        """
+        # attempt to
+        try:
+            # ask authority for a handler for my type
+            handler = authority.category
+        # if it doesn't exist
+        except AttributeError:
+            # this is almost certainly a bug; make a channel
+            channel = journal.firewall("merlin.assets.identify")
+            # complain
+            channel.line(f"unable to find a handler for '{self.pyre_name}'")
+            channel.line(f"for the asset category '{self.__class__.__name__}'")
+            channel.line(f"while looking through the interface of '{authority.pyre_name}'")
+            # flush
+            channel.log()
+            # and fail, just in case firewalls aren't fatal
+            return None
+        # if it does, invoke it
+        return handler(**kwds)
+
+
+# end of file
