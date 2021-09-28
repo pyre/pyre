a0ff5cf77b6db336477297a2567483c42e75e12b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-09-28 08:50:01 +0300

    meriln.factories: primed {cp}

---
+++ packages/merlin/factories/Copy.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import journal
+# framework
+import merlin
+# superclass
+from .Factory import Factory
+
+
+# creates a directory an a filesystem
+class Copy(Factory, family="merlin.factories.cp"):
+    """
+    Copy a file from one location to another
+    """
+
+
+    # inputs
+    source = merlin.protocols.file.input()
+    source.doc = "the source file"
+
+    within = merlin.protocols.directory.input()
+    within.doc = "the containing directory at the destination"
+
+    # output
+    destination = merlin.protocols.file.output()
+    destination.doc = "the destination file"
+
+
+    # protocol obligations
+    @merlin.export
+    def pyre_make(self, **kwds):
+        """
+        Construct my products
+        """
+        # the trivial implementation here is just a place for a breakpoint while debugging
+        # it will be removed at some point...
+        # chain up
+        return super().pyre_make(**kwds)
+
+
+    # framework hooks
+    def pyre_run(self, **kwds):
+        """
+        Make the subdirectory
+        """
+        # marker
+        indent = " " * 2
+        # sign on
+        print(f"{indent*1}[cp] {self.destination.path}")
+
+        # all done
+        return
+
+
+# end of file
