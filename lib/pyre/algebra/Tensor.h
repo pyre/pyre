df36c14a4484cecb41b49a40751df49559648053
Bianca Giovanardi <biancagi@mit.edu>
2021-10-18 19:04:08 +0200

    lib/pyre/algebra: removed constructors/destructors count for {Tensor}

--- lib/pyre/algebra/Tensor.h
+++ lib/pyre/algebra/Tensor.h
@@ -7,8 +7,6 @@
 #if !defined(pyre_algebra_Tensor_h)
 #define pyre_algebra_Tensor_h
 
-//#define DEVELOP_MODE
-
 // TOFIX
 #include "../grid.h"
 
@@ -136,20 +134,7 @@ private:
 
     // data
     data_t _data;
-
-#ifdef DEVELOP_MODE
-public:
-    static int _constructor_calls;
-    static int _destructor_calls;
-#endif    // DEVELOP_MODE
 };
-
-#ifdef DEVELOP_MODE
-template <typename T, int... I> 
-int pyre::algebra::Tensor<T, I...>::_constructor_calls = 0;
-template <typename T, int... I>
-int pyre::algebra::Tensor<T, I...>::_destructor_calls = 0;
-#endif    // DEVELOP_MODE
 
 // typedef for real values
 using real = double;
