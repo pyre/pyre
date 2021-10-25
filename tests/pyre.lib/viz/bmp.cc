204f0c087c07e4c69450acbc82fd0a31a6bf3468
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-10-25 10:35:29 -0700

    pyre::viz: added a test case that creates a sample  {bmp_t}

---
+++ tests/pyre.lib/viz/bmp.cc
@@ -0,0 +1,60 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// STL
+#include <vector>
+#include <tuple>
+#include <fstream>
+// support
+#include <pyre/viz.h>
+
+
+// type aliases
+using bmp_t = pyre::viz::bmp_t;
+
+// driver
+int
+main(int argc, char * argv[])
+{
+    // pick a height
+    int height = 255;
+    // and a width
+    int width = 255;
+
+    // make a vector of {rgb} triplets
+    std::vector<bmp_t::rgb_type> data;
+    // give it enough room
+    data.reserve(width * height);
+    // go through it
+    for (auto idx = 0; idx < width * height; ++idx) {
+        // and place color values
+        data.emplace(data.end(), idx % 256, idx % 128, idx % 64);
+    }
+
+    // make a bitmap
+    bmp_t bmp(height, width);
+
+    // point to the beginning of the data
+    auto start = data.begin();
+    // encode
+    auto img = bmp.encode(start);
+
+    // open a file
+    std::ofstream str("chip.bmp", std::ios::out | std::ios::binary);
+    // if we succeeded
+    if (str.is_open()) {
+        // ask for the stream size
+        auto bytes = bmp.bytes();
+        // write
+        str.write(img, bytes);
+    }
+
+    // all done
+    return 0;
+}
+
+
+// end of file
