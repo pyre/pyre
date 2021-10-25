257900422346ec5a2d067a1384498b63188d5311
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-10-25 10:15:38 -0700

    pyre::viz: primed the new {viz} package

---
+++ lib/pyre/viz/api.h
@@ -0,0 +1,18 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+// code guard
+#if !defined(pyre_viz_api_h)
+#define pyre_viz_api_h
+
+
+// publicly visible types
+namespace pyre::viz {
+
+} // namespace pyre::viz
+
+#endif
+
+// end of file
