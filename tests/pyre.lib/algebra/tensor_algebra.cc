7c5d5be4e7746be60ccaf343e92125bc6b34dae2
Bianca Giovanardi <biancagi@mit.edu>
2021-10-28 18:30:52 +0200

    tests/pyre.lib/algebra: tests now use namespace pyre::algebra

--- tests/pyre.lib/algebra/tensor_algebra.cc
+++ tests/pyre.lib/algebra/tensor_algebra.cc
@@ -12,59 +12,60 @@
 #include <pyre/algebra/Tensor.h>
 #include <pyre/algebra/tensor_algebra.h>
 
+using namespace pyre::algebra;
+
 // main program
 int main(int argc, char* argv[]) {
 
-    pyre::algebra::vector_t<2> vector1 = { 0.0, 0.0 };
+    vector_t<2> vector1 = { 0.0, 0.0 };
     assert(vector1 == vector1);
 
-    constexpr pyre::algebra::vector_t<2> vector2 = { 1.0, 2.0 };
+    constexpr vector_t<2> vector2 = { 1.0, 2.0 };
     vector1 += vector2;
     assert(vector1 + vector2 == 2.0 * vector2);
     static_assert(vector2 + vector2 == 2.0 * vector2);
-    static_assert(vector2 - vector2 == pyre::algebra::vector_t<2>::zero);
-    static_assert(pyre::algebra::vector_t<2>::zero
-        == pyre::algebra::vector_t<2>::one - pyre::algebra::vector_t<2>::one);
+    static_assert(vector2 - vector2 == vector_t<2>::zero);
+    static_assert(vector_t<2>::zero == vector_t<2>::one - vector_t<2>::one);
 
-    constexpr pyre::algebra::scalar_t a = 1.0;
+    constexpr scalar_t a = 1.0;
     static_assert(vector2 * a == vector2);
     static_assert(vector2 * (-a) == -vector2);
     static_assert((-a) * vector2 == -vector2);
     static_assert(vector2 / a == vector2);
     static_assert(vector2 / (-a) == -vector2);
 
-    constexpr pyre::algebra::vector_t<3> vector3 = { 1, 0, 0 };
-    constexpr pyre::algebra::vector_t<3> vector4 = { 0, 1, 0 };
+    constexpr vector_t<3> vector3 = { 1, 0, 0 };
+    constexpr vector_t<3> vector4 = { 0, 1, 0 };
     static_assert(vector3 * vector4 == 0.0);
 
-    constexpr pyre::algebra::scalar_t b(1.0);
+    constexpr scalar_t b(1.0);
     static_assert(2 * b == b + 1);
 
     // TODO: Add tests for all algebraic operators
 
     // Matrix-vector product // TODO: { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} }
-    constexpr pyre::algebra::matrix_t<3, 3> A = { 1, -2, 0, 0, 1, 2, 0, 1, 1 };
-    constexpr pyre::algebra::vector_t<3> x = { 1, 1, 1 };
-    constexpr pyre::algebra::vector_t<3> y = a * A * x;
+    constexpr matrix_t<3, 3> A = { 1, -2, 0, 0, 1, 2, 0, 1, 1 };
+    constexpr vector_t<3> x = { 1, 1, 1 };
+    constexpr vector_t<3> y = a * A * x;
     static_assert(inv(A)*y == a * x);
 
     // transpose tensor
-    constexpr pyre::algebra::matrix_t<3, 3> B = transpose(A);
+    constexpr matrix_t<3, 3> B = transpose(A);
     // transpose of transpose is the identity operator
     static_assert(transpose(B) == A);
 
     // transpose preserves trace
-    constexpr pyre::algebra::scalar_t traceA = tr(A);
-    constexpr pyre::algebra::scalar_t traceB = tr(B);
+    constexpr scalar_t traceA = tr(A);
+    constexpr scalar_t traceB = tr(B);
     static_assert(traceA == traceB);
 
     // transpose preserves determinant
-    constexpr pyre::algebra::scalar_t detA = det(A);
-    constexpr pyre::algebra::scalar_t detB = det(B);
+    constexpr scalar_t detA = det(A);
+    constexpr scalar_t detB = det(B);
     static_assert(det(B) == det(A));
 
     // product of eigenvalues is equal to determinant
-    constexpr pyre::algebra::symmetric_matrix_t<2> C = { 1, 2, 2, 2 };
+    constexpr symmetric_matrix_t<2> C = { 1, 2, 2, 2 };
     constexpr auto lambda = eigenvalues(C); 
     static_assert(lambda[0] * lambda[1] == det(C));
 
@@ -80,34 +81,34 @@ int main(int argc, char* argv[]) {
     // Jacobi's theorem (all odd dimension skew symmetric matrices are singular)
     static_assert(det(skew(A)) == 0.0);
 
-    constexpr pyre::algebra::matrix_t<2, 2> M = {0, 1, 2, 3};
+    constexpr matrix_t<2, 2> M = {0, 1, 2, 3};
     // symmetric tensors have zero skew part
-    static_assert(skew(symmetric(M)) == pyre::algebra::matrix_t<2>::zero);
+    static_assert(skew(symmetric(M)) == matrix_t<2>::zero);
     // Cayley-Hamilton's theorem (a matrix is a solution of its characteristic polynomial) (2D)
-    constexpr pyre::algebra::matrix_t<2, 2> I = { 1, 0, 0, 1 };
-    static_assert(M * M - tr(M) * M + det(M) * I == pyre::algebra::matrix_t<2>::zero);
+    constexpr matrix_t<2, 2> I = { 1, 0, 0, 1 };
+    static_assert(M * M - tr(M) * M + det(M) * I == matrix_t<2>::zero);
 
-    static_assert(pyre::algebra::matrix_t<2>::zero == pyre::algebra::matrix_t<2>::zero);
+    static_assert(matrix_t<2>::zero == matrix_t<2>::zero);
     static_assert(M == M);
 
-    constexpr pyre::algebra::matrix_t<2> unit00 = pyre::algebra::matrix_t<2>::unit<{0, 0}>;
-    constexpr pyre::algebra::matrix_t<2> unit01 = pyre::algebra::matrix_t<2>::unit<{0, 1}>;
-    constexpr pyre::algebra::matrix_t<2> unit10 = pyre::algebra::matrix_t<2>::unit<{1, 0}>;
-    constexpr pyre::algebra::matrix_t<2> unit11 = pyre::algebra::matrix_t<2>::unit<{1, 1}>;
-    static_assert(unit00 == pyre::algebra::matrix_t<2>{1, 0, 0, 0});
-    static_assert(unit01 == pyre::algebra::matrix_t<2>{0, 1, 0, 0});
-    static_assert(unit10 == pyre::algebra::matrix_t<2>{0, 0, 1, 0});
-    static_assert(unit11 == pyre::algebra::matrix_t<2>{0, 0, 0, 1});
-    static_assert(unit00 + unit01 + unit10 + unit11 == pyre::algebra::matrix_t<2>::one);
+    constexpr matrix_t<2> unit00 = matrix_t<2>::unit<{0, 0}>;
+    constexpr matrix_t<2> unit01 = matrix_t<2>::unit<{0, 1}>;
+    constexpr matrix_t<2> unit10 = matrix_t<2>::unit<{1, 0}>;
+    constexpr matrix_t<2> unit11 = matrix_t<2>::unit<{1, 1}>;
+    static_assert(unit00 == matrix_t<2>{1, 0, 0, 0});
+    static_assert(unit01 == matrix_t<2>{0, 1, 0, 0});
+    static_assert(unit10 == matrix_t<2>{0, 0, 1, 0});
+    static_assert(unit11 == matrix_t<2>{0, 0, 0, 1});
+    static_assert(unit00 + unit01 + unit10 + unit11 == matrix_t<2>::one);
 
     // QUESTION: the following line triggers an explicit constructor compiler error
-    // constexpr pyre::algebra::vector_t<3> unit0 = pyre::algebra::vector_t<3>::unit<{0}>;
-    constexpr pyre::algebra::vector_t<3> unit0 = pyre::algebra::vector_t<3>::unit<0>;
-    constexpr pyre::algebra::vector_t<3> unit1 = pyre::algebra::vector_t<3>::unit<1>;
-    constexpr pyre::algebra::vector_t<3> unit2 = pyre::algebra::vector_t<3>::unit<2>;
-    static_assert(unit0 == pyre::algebra::vector_t<3>{1, 0, 0});
-    static_assert(unit1 == pyre::algebra::vector_t<3>{0, 1, 0});
-    static_assert(unit2 == pyre::algebra::vector_t<3>{0, 0, 1});
+    // constexpr vector_t<3> unit0 = vector_t<3>::unit<{0}>;
+    constexpr vector_t<3> unit0 = vector_t<3>::unit<0>;
+    constexpr vector_t<3> unit1 = vector_t<3>::unit<1>;
+    constexpr vector_t<3> unit2 = vector_t<3>::unit<2>;
+    static_assert(unit0 == vector_t<3>{1, 0, 0});
+    static_assert(unit1 == vector_t<3>{0, 1, 0});
+    static_assert(unit2 == vector_t<3>{0, 0, 1});
 
     // all done
     return 0;
--- tests/pyre.lib/algebra/tensor_print.cc
+++ tests/pyre.lib/algebra/tensor_print.cc
@@ -12,31 +12,33 @@
 #include <pyre/algebra/Tensor.h>
 #include <pyre/algebra/tensor_algebra.h>
 
+using namespace pyre::algebra;
+
 // main program
 int main(int argc, char* argv[]) {
 
-    pyre::algebra::matrix_t<1, 1> A = { 1 };
+    matrix_t<1, 1> A = { 1 };
     std::cout << A << std::endl;
 
-    pyre::algebra::matrix_t<1, 2> B = { 0, 1 };
+    matrix_t<1, 2> B = { 0, 1 };
     std::cout << B << std::endl;
 
-    pyre::algebra::matrix_t<2, 1> C = { 0, 1 };
+    matrix_t<2, 1> C = { 0, 1 };
     std::cout << C << std::endl;
 
-    pyre::algebra::matrix_t<2, 2> D = { 0, 1, 2, 3 };
+    matrix_t<2, 2> D = { 0, 1, 2, 3 };
     std::cout << D << std::endl;
 
-    pyre::algebra::matrix_t<3, 3> E = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    matrix_t<3, 3> E = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
     std::cout << E << std::endl;
 
-    pyre::algebra::vector_t<1> a = { 1 };
+    vector_t<1> a = { 1 };
     std::cout << a << std::endl;
 
-    pyre::algebra::vector_t<2> b = { 1, 1 };
+    vector_t<2> b = { 1, 1 };
     std::cout << b << std::endl;
 
-    pyre::algebra::vector_t<3> c = { 1, 1, 1 };
+    vector_t<3> c = { 1, 1, 1 };
     std::cout << c << std::endl;
 
     // all done
