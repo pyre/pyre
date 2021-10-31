05395f8acb550d375bcb3528787f5a354c6d19e1
Bianca Giovanardi <biancagi@mit.edu>
2021-10-31 12:32:59 +0100

    tests/pyre.lib/algebra: tensor algebra checks have now their own test

--- tests/pyre.lib/algebra/tensor_algebra.cc
+++ tests/pyre.lib/algebra/tensor_algebra.cc
@@ -4,29 +4,27 @@
 // (c) 2021 all rights reserved
 //
 
-// system includes
-#include <iostream>
+// support
 #include <cassert>
 
-// dependencies
-#include <pyre/algebra/Tensor.h>
+// get the tensor algebra
 #include <pyre/algebra/tensor_algebra.h>
 
+// use namespace for readability
 using namespace pyre::algebra;
 
 // main program
 int main(int argc, char* argv[]) {
 
-    // Matrix-vector product // TODO: { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} }
+    // matrix-vector product
     constexpr matrix_t<3, 3> A = { 1, -2, 0, 0, 1, 2, 0, 1, 1 };
     constexpr vector_t<3> x = { 1, 1, 1 };
-    constexpr vector_t<3> y = A * x;
-    static_assert(inv(A)*y == x);
-
+    constexpr scalar_t a = -2.0;
+    constexpr vector_t<3> y = a * A * x;
+    static_assert(inv(A) * y / a == x);
 
     // all done
     return 0;
 }
-
 
 // end of file
