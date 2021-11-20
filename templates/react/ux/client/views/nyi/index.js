9157544f9c668cfec92bce1407e6680b8176f92b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-11-20 09:23:59 -0800

    templates/react: the {nyi} view now displays the path of the unimplemented page

--- templates/react/ux/client/views/nyi/index.js
+++ templates/react/ux/client/views/nyi/index.js
@@ -6,6 +6,7 @@
 
 // externals
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 // locals
 // shapes
 import {{ Hammer }} from '~/shapes'
@@ -15,19 +16,25 @@ import styles from './styles'
 
 // the area
 const loading = () => (
-    <section style={{styles.loading}}>
-        <div style={{styles.placeholder}}>
-            <svg style={{styles.icon}} version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <g transform="scale(0.3)" fill="#f37f19" stroke="none">
-                    <Hammer style={{styles.shape}} />
-                </g>
-            </svg>
-            <p style={{styles.message}}>
-                this page is not implemented yet
-            </p>
-        </div>
-    </section>
-)
+    // grab the location
+    const location = useLocation()
+
+    // render
+    return (
+        <section style={{styles.loading}}>
+            <div style={{styles.placeholder}}>
+                <svg style={{styles.icon}} version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="scale(0.3)" fill="#f37f19" stroke="none">
+                        <Hammer style={{styles.shape}} />
+                    </g>
+                </svg>
+                <p style={{styles.message}}>
+                    the page at "{location.pathname}" is not implemented yet
+                </p>
+            </div>
+        </section>
+    )
+}
 
 
 // publish
