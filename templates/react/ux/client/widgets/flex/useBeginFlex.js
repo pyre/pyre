cb86304327d8d9fec2a0e708da8a1b83e8a67455
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-14 21:45:44 -0800

    templates/react: reimplemented the calculation of the {downstream} panels in the {flex} widget so that it does not rely on the panel registration order

--- templates/react/ux/client/widgets/flex/useBeginFlex.js
+++ templates/react/ux/client/widgets/flex/useBeginFlex.js
@@ -15,9 +15,9 @@ import {{ Context }} from './context'
 export default ({{ panel }}) => {{
     // grab the state mutator
     const {{
-        mainExtent,
         panels,
-        isManaged, setIsManaged,
+        mainExtent, mainPos,
+        hasFlexed, setHasFlexed,
         setSeparatorLocation,
         setFlexingPanel,
         setDownstreamPanels,
@@ -30,38 +30,15 @@ export default ({{ panel }}) => {{
         // quash any side effects
         evt.preventDefault()
 
-        // unpack the cursor location
-        const location = {{ x: evt.clientX, y: evt.clientY }}
-
         // get the panel refs
         const refs = Array.from(panels.keys())
-        // find the index of the active panel
-        const idx = refs.indexOf(panel)
-        // use it to extract all the downstream panels
-        const downstream = refs.slice(idx + 1)
-
-        // currently, disallow activity on the last separator; so skip the state update
-        // if there are no downstream panels
-        if (downstream !== []) {{
-            // record the panel that is flexing
-            setFlexingPanel(panel)
-            // and the downstream ones
-            setDownstreamPanels(downstream)
-            // save the current mouse coordinates
-            setSeparatorLocation(location)
-        }}
+        // collect placement and sizing information for all panels
+        const extents = new Map(refs.map(ref => [ref, ref.current.getBoundingClientRect()]))
 
         // if this is the first time we are flexing
-        if (!isManaged) {{
-            // collect the current sizes
-            const extents = refs.map(
-                panelRef => Math.round(panelRef.current.getBoundingClientRect()[mainExtent])
-            )
-
+        if (!hasFlexed) {{
             // go through all the panels
-            refs.forEach((panelRef, idx) => {{
-                // grab the extent of the current panel
-                const extent = extents[idx]
+            extents.forEach((extent, panelRef) => {{
                 // check whether the user has marked this panel as able to absorb viewport changes
                 const {{ auto }} = panels.get(panelRef)
                 // deduce the correct flex: every panel is now frozen to its styled extent,
@@ -73,14 +50,38 @@ export default ({{ panel }}) => {{
                 // apply the flex
                 style.flex = flx
                 // transfer its current extent to the its style
-                style[mainExtent] = `${{extent}}px`
+                style[mainExtent] = `${{extent[mainExtent]}}px`
                 // all done
                 return
             }})
-
             // mark
-            setIsManaged(true)
+            setHasFlexed(true)
         }}
+
+        //  find the position of the flexing panel
+        const pos = extents.get(panel)[mainPos]
+
+        // the downstream panels are those panels
+        const downstream = refs.filter(
+            // whose position is greater than the flexing panel
+            ref => extents.get(ref)[mainPos] > pos
+        ).sort(
+            // sorted in increasing order by their position
+            (op1, op2) => extents.get(op1)[mainPos] - extents.get(op2)[mainPos]
+        )
+
+        // currently, disallow activity on the last separator
+        if (downstream.length === 0) {{
+            // so skip the state update if there are no downstream panels
+            return
+        }}
+
+        // record the panel that is flexing
+        setFlexingPanel(panel)
+        // and the downstream ones
+        setDownstreamPanels(downstream)
+        // unpack the mouse coordinates and use them as the current separator location
+        setSeparatorLocation({{ x: evt.clientX, y: evt.clientY }})
 
         // all done
         return
