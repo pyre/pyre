ce01765db82687a7a2cd1a8275a2af6d01fe2fff
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-23 12:25:43 +0200

    pyre::memory: added const and non-const maps over {char}, {int16_t}, {int32_t}, and {int64_t}

--- extensions/pyre/memory/maps.cc
+++ extensions/pyre/memory/maps.cc
@@ -21,6 +21,54 @@ void
 pyre::py::memory::maps(py::module & m)
 {
     // const maps
+    // c
+    constmap<char>(
+        // in scope
+        m,
+        // class name
+        "ByteConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {char}");
+    // i2
+    constmap<int16_t>(
+        // in scope
+        m,
+        // class name
+        "Int16ConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {int16_t}");
+    // i4
+    constmap<int32_t>(
+        // in scope
+        m,
+        // class name
+        "Int32ConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {int32_t}");
+    // i8
+    constmap<int64_t>(
+        // in scope
+        m,
+        // class name
+        "Int64ConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {int64_t}");
+    // d4
+    constmap<float>(
+        // in scope
+        m,
+        // class name
+        "FloatConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {float}");
+    // d8
+    constmap<double>(
+        // in scope
+        m,
+        // class name
+        "DoubleConstMap",
+        // docstring
+        "a read-only file backed memory buffer of {double}");
     // c4
     constmap<std::complex<float>>(
         // in scope
@@ -39,6 +87,54 @@ pyre::py::memory::maps(py::module & m)
         "a read-only file backed memory buffer of {std::complex<double>}");
 
     // maps
+    // c
+    map<char>(
+        // in scope
+        m,
+        // class name
+        "ByteMap",
+        // docstring
+        "a read/write file backed memory buffer of {char}");
+    // i2
+    map<int16_t>(
+        // in scope
+        m,
+        // class name
+        "Int16Map",
+        // docstring
+        "a read/write file backed memory buffer of {int16_t}");
+    // i4
+    map<int32_t>(
+        // in scope
+        m,
+        // class name
+        "Int32Map",
+        // docstring
+        "a read/write file backed memory buffer of {int32_t}");
+    // i8
+    map<int64_t>(
+        // in scope
+        m,
+        // class name
+        "Int64Map",
+        // docstring
+        "a read/write file backed memory buffer of {int64_t}");
+    // d4
+    map<float>(
+        // in scope
+        m,
+        // class name
+        "FloatMap",
+        // docstring
+        "a read/write file backed memory buffer of {float}");
+    // d8
+    map<double>(
+        // in scope
+        m,
+        // class name
+        "DoubleMap",
+        // docstring
+        "a read/write file backed memory buffer of {double}");
     // c4
     map<std::complex<float>>(
         // in scope
