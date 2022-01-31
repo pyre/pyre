1166e4f61dc6fa5d12b6020d17bc9aac4aa6eb4a
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-31 16:21:50 +0200

    pyre::timers: cleaned up the {process_timer_reset} test case

--- tests/pyre.lib/timers/process_timer_reset.cc
+++ tests/pyre.lib/timers/process_timer_reset.cc
@@ -21,27 +21,22 @@ using proctimer_t = pyre::timers::process_timer_t;
 
 
 // verify that we can manipulate the timer state
-int main() {
+int
+main()
+{
     // make a timer
     proctimer_t timer("tests.timer");
     // and start it
     timer.start();
-
-    // do something
-    double sum=0;
-    for (int i=0; i < 1000*1000; ++i) {
-        sum += i;
-    }
-
     // stop it
     timer.stop();
 
-    // reset the timer
+    // reset it
     timer.reset();
     // verify it is inactive
-    assert (timer.active() == false);
+    assert(timer.active() == false);
     // and that the accumulated time is reset to zero
-    assert (timer.read() == proctimer_t::duration_type::zero());
+    assert(timer.read() == proctimer_t::duration_type::zero());
 
     // all done
     return 0;
