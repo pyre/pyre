5c72d77278f2186eafa0270de6c3d1ae01b7d90d
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-14 08:20:39 -0800

    h5: primed the bindings

---
+++ extensions/h5/api.cc
@@ -0,0 +1,24 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+
+// add global bindings to the module
+void
+h5::py::api(py::module & m)
+{
+    // module attributes
+
+    // all done
+    return;
+}
+
+
+// end of file
