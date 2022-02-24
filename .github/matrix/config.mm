84abd6499e36f2177eaa11ce766a54775331ab6e
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-23 19:06:55 -0800

    github: enabled building and testing the {hdf5} support

--- .github/matrix/config.mm
+++ .github/matrix/config.mm
@@ -6,24 +6,33 @@
 # external dependencies
 # system tools
 sys.prefix := /usr
-sys.lib := ${sys.prefix}/lib
-sys.libx86 := ${sys.lib}/x86_64-linux-gnu
+sys.lib := ${sys.prefix}/lib/x86_64-linux-gnu
 
 # gsl
 gsl.version := 2.5
 gsl.dir := $(sys.prefix)
+
+# hdf5
+hdf5.version := 1.10.6
+hdf5.dir := ${sys.prefix}
+hdf5.parallel := openmpi
+hdf5.incpath := $(hdf5.dir)/include/hdf5/$(hdf5.parallel)
+hdf5.libpath := $(sys.lib)/hdf5/${hdf5.parallel}
+
 # mpi
 mpi.version := 4.0.3
 mpi.flavor := openmpi
-mpi.dir := ${sys.libx86}/openmpi
+mpi.dir := ${sys.lib}/$(mpi.flavor)
 mpi.executive := mpiexec
 
 # python
 python.version := $(pythonVersion)
 python.dir := $(pythonLocation)
+
 # numpy
 numpy.version := 1.21.1
 numpy.dir := $(python.dir)/lib/python$(python.version)/site-packages/numpy/core
+
 # pybind11
 pybind11.version := 2.7.0
 pybind11.dir := $(python.dir)/lib/python$(python.version)/site-packages/pybind11
