56b9cc620e8e3223a451adcdf94ab4321f9986c1
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-17 18:29:32 -0700

    pyre::h5: cosmetics in the {sanity} test case

--- tests/pyre.lib/h5/sanity.cc
+++ tests/pyre.lib/h5/sanity.cc
@@ -4,12 +4,14 @@
 // (c) 1998-2022 all rights reserved
 
 
-// get the h5
+// get the h5 support
 #include <pyre/h5.h>
 
 
 // compile time sanity check: make sure the header file is accessible
-int main() {
+int
+main()
+{
     // nothing to do
     return 0;
 }
