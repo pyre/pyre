a40bf1f766d16722151e06934011339f4cfd7877
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-17 17:06:18 -0700

    pyre::h5: primed the namespace

---
+++ lib/pyre/h5/external.h
@@ -0,0 +1,21 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(pyre_h5_external_h)
+#define pyre_h5_external_h
+
+
+// externals
+#include <complex>
+// support
+#include <pyre/journal.h>
+// the hdf5 api
+#include <H5Cpp.h>
+
+
+#endif
+
+// end of file
---
+++ lib/pyre/h5/forward.h
@@ -0,0 +1,18 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(pyre_h5_forward_h)
+#define pyre_h5_forward_h
+
+
+// datatypes
+namespace pyre::h5 {
+} // namespace pyre::h5
+
+
+#endif
+
+// end of file
