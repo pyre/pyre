55c144125f402081d7bf67067ecf492be9b12bf6
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-17 17:41:00 -0700

    pyre::h5: primed the {h5} test suite

---
+++ tests/pyre.lib/h5/sanity.cc
@@ -0,0 +1,18 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// get the h5
+#include <pyre/h5.h>
+
+
+// compile time sanity check: make sure the header file is accessible
+int main() {
+    // nothing to do
+    return 0;
+}
+
+
+// end of file
