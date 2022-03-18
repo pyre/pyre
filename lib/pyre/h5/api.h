15d73fe2ef1ee71c376e6681ccbad0dca1829396
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-17 17:06:18 -0700

    pyre::h5: primed the namespace

---
+++ lib/pyre/h5/api.h
@@ -0,0 +1,19 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(pyre_h5_api_h)
+#define pyre_h5_api_h
+
+
+// publicly visible types
+namespace pyre::h5 {
+
+} // namespace pyre::h5
+
+
+#endif
+
+// end of file
---
+++ lib/pyre/h5/public.h
@@ -0,0 +1,23 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(pyre_h5_public_h)
+#define pyre_h5_public_h
+
+
+// external packages
+#include "external.h"
+// set up the namespace
+#include "forward.h"
+
+// published type aliases and declarations that constitute the public API of this package
+// this is the file you are looking for
+#include "api.h"
+
+
+#endif
+
+// end of file
