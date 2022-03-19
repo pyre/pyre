15c82624598457aa1fbce7abe3a6a7730a343219
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-19 11:57:09 -0700

    pyre::h5: removed the attempt to support {std::complex} from the {datatypes} test case, and added checks that the type deduction generates references to the HDF5 {PredType} singletons

--- tests/pyre.lib/h5/datatypes.cc
+++ tests/pyre.lib/h5/datatypes.cc
@@ -4,6 +4,8 @@
 // (c) 1998-2022 all rights reserved
 
 
+// externals
+#include <cassert>
 // get the h5 support
 #include <pyre/h5.h>
 
@@ -13,16 +15,20 @@ int
 main()
 {
     // instantiate the native types that are supported by {hdf5} out of the box
-    [[maybe_unused]] auto charTp = pyre::h5::datatype<char>();
-    [[maybe_unused]] auto shortTp = pyre::h5::datatype<short>();
-    [[maybe_unused]] auto intTp = pyre::h5::datatype<int>();
-    [[maybe_unused]] auto longTp = pyre::h5::datatype<long>();
-    [[maybe_unused]] auto floatTp = pyre::h5::datatype<float>();
-    [[maybe_unused]] auto doubleTp = pyre::h5::datatype<double>();
+    auto charTp = pyre::h5::datatype<char>();
+    auto shortTp = pyre::h5::datatype<short>();
+    auto intTp = pyre::h5::datatype<int>();
+    auto longTp = pyre::h5::datatype<long>();
+    auto floatTp = pyre::h5::datatype<float>();
+    auto doubleTp = pyre::h5::datatype<double>();
 
-    // instantiate our extensions to include the common {std::complex} instantiations
-    [[maybe_unused]] auto complexFloatTp = pyre::h5::datatype<std::complex<float>>();
-    [[maybe_unused]] auto complexDoubleTp = pyre::h5::datatype<std::complex<double>>();
+    // check them against the {H5} singletons
+    assert((charTp == H5::PredType::NATIVE_CHAR));
+    assert((shortTp == H5::PredType::NATIVE_SHORT));
+    assert((intTp == H5::PredType::NATIVE_INT));
+    assert((longTp == H5::PredType::NATIVE_LONG));
+    assert((floatTp == H5::PredType::NATIVE_FLOAT));
+    assert((doubleTp == H5::PredType::NATIVE_DOUBLE));
 
     // all done
     return 0;
