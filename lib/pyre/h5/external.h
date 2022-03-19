a8cacce31773c928eb4a65bc48c28faf310b282c
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-19 11:39:54 -0700

    pyre::h5: added local type aliases for some {H5} classes

--- lib/pyre/h5/external.h
+++ lib/pyre/h5/external.h
@@ -9,11 +9,21 @@
 
 
 // externals
+#include <array>
 #include <complex>
 // support
 #include <pyre/journal.h>
 // the hdf5 api
 #include <H5Cpp.h>
+
+
+// aliases
+namespace pyre::h5 {
+    // from {hdf5}
+    using dataset_t = H5::DataSet;
+    using dataspace_t = H5::DataSpace;
+    using datatype_t = H5::DataType;
+} // namespace pyre::h5
 
 
 #endif
