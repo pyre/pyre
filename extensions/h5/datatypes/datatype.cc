d8aa7d8b76cea7d511f1062f17ddf9f10963ce1b
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-20 20:37:00 -0700

    pyre::h5: moved the {datatype} bindings to their own directory

---
+++ extensions/h5/datatypes/datatype.cc
@@ -0,0 +1,31 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+
+// file objects
+void
+h5::py::datatypes::datatype(py::module & m)
+{
+    // add bindings for the base hdf5 datatype
+    auto dataType = py::class_<DataType>(
+        // in scope
+        m,
+        // class name
+        "DataType",
+        // docstring
+        "the base HDF5 datatype");
+
+    // all done
+    return;
+}
+
+
+// end of file
---
+++ extensions/h5/datatypes/datatypes.cc
@@ -0,0 +1,37 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+
+// file objects
+void
+h5::py::datatypes::datatypes(py::module & m)
+{
+    // make a new namespace to hold datatype descriptions
+    auto datatypes = m.def_submodule(
+        // the name of the module
+        "datatypes",
+        // its docstring
+        "HDF5 datatypes");
+
+    // add the class definitions
+    // the base data types
+    datatype(datatypes);
+    // predefined datatypes
+    predefined(datatypes);
+    // add the constants that correspond to the native types
+    native(datatypes);
+
+    // all done
+    return;
+}
+
+
+// end of file
