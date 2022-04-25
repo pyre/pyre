8dd0809f4824c870683844f050c206d1e24ad2ac
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-25 08:52:18 -0700

    pyre::timers: removed unused variable warning from the {process_timer_t} test cases

--- tests/pyre.lib/timers/process_timer_shared.cc
+++ tests/pyre.lib/timers/process_timer_shared.cc
@@ -18,32 +18,34 @@ using proctimer_t = pyre::timers::process_timer_t;
 
 
 // verify that two timers that have the same name share the same movement
-int main() {
+int
+main()
+{
     // make a timer
     proctimer_t t1("tests.timer");
     // start it
     t1.start();
     // it should now be active
-    assert (t1.active() == true);
+    assert(t1.active() == true);
 
     // do something
-    double sum=0;
-    for (int i=0; i < 1000*1000; ++i) {
+    [[maybe_unused]] double sum = 0;
+    for (int i = 0; i < 1000 * 1000; ++i) {
         sum += i;
     }
 
     // make another timer with the same name
     proctimer_t t2("tests.timer");
     // verify it is running
-    assert (t2.active() == true);
+    assert(t2.active() == true);
     // stop it
     t2.stop();
 
     // verify we stopped both timers
-    assert (t1.active() == false);
-    assert (t2.active() == false);
+    assert(t1.active() == false);
+    assert(t2.active() == false);
     // and that they both show the same accumulated time
-    assert (t1.read() == t2.read());
+    assert(t1.read() == t2.read());
 
     // all done
     return 0;
