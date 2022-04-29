f57f132916ed3b305685e3d426d9aa5891b4fc89
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-29 11:07:16 -0700

    pyre::grid: added bindings for {3d} and {4d} memory mapped grids

--- extensions/pyre/grid/grids.h
+++ extensions/pyre/grid/grids.h
@@ -11,6 +11,7 @@
 // the {pyre} extension namespace
 namespace pyre::py::grid {
     // the grid instantiations
+    // 2d
     void byteConstMapGrid2D(py::module &);
     void int16ConstMapGrid2D(py::module &);
     void int32ConstMapGrid2D(py::module &);
@@ -19,6 +20,24 @@ namespace pyre::py::grid {
     void doubleConstMapGrid2D(py::module &);
     void complexFloatConstMapGrid2D(py::module &);
     void complexDoubleConstMapGrid2D(py::module &);
+    // 3d
+    void byteConstMapGrid3D(py::module &);
+    void int16ConstMapGrid3D(py::module &);
+    void int32ConstMapGrid3D(py::module &);
+    void int64ConstMapGrid3D(py::module &);
+    void floatConstMapGrid3D(py::module &);
+    void doubleConstMapGrid3D(py::module &);
+    void complexFloatConstMapGrid3D(py::module &);
+    void complexDoubleConstMapGrid3D(py::module &);
+    // 4d
+    void byteConstMapGrid4D(py::module &);
+    void int16ConstMapGrid4D(py::module &);
+    void int32ConstMapGrid4D(py::module &);
+    void int64ConstMapGrid4D(py::module &);
+    void floatConstMapGrid4D(py::module &);
+    void doubleConstMapGrid4D(py::module &);
+    void complexFloatConstMapGrid4D(py::module &);
+    void complexDoubleConstMapGrid4D(py::module &);
 
     // the interface decorators
     template <class gridT>
