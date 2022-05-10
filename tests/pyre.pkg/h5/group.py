59576e84b297140367359e9c8da8ed0a3f760c0c
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-10 10:55:39 -0700

    pyre.h5: primed the test suite

---
+++ tests/pyre.pkg/h5/group.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2022 all rights reserved
+#
+
+
+"""
+Check that we can harvest datasets from groups
+"""
+
+# framework
+import pyre
+
+
+# make a group
+class Group(pyre.h5.group):
+    """
+    A group of datasets in some HDF5 file
+    """
+
+
+
+def test():
+    # instantiate
+    group = Group()
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # drive
+    test()
+
+
+# end of file
