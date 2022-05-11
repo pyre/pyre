590a51e6e2356ba003326f5a763293acf9303eb6
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-10 19:12:49 -0700

    pyre.h5: primed access to {h5} files

---
+++ packages/pyre/h5/H5.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2022 all rights reserved
+#
+
+
+# superclass
+from .Group import Group
+
+
+# a dataset container
+class H5(Group):
+    """
+    An h5 file
+    """
+
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # all done
+        return
+
+
+# end of file
