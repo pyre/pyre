7aed129c7d8fce9964eb9014b0d1738fcdd1b9f5
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-17 12:35:12 -0700

    pyre.h5: added some depth to the {group_class} test case

--- tests/pyre.pkg/h5/group_class.py
+++ tests/pyre.pkg/h5/group_class.py
@@ -21,8 +21,13 @@ def test():
         """
         A group of datasets in some HDF5 file
         """
+        # something simple
+        id = pyre.h5.int()
+        id.doc = "a simple dataset"
 
-        id = pyre.h5.int(default=0)
+        # something a bit more complicated
+        pols = pyre.h5.list()
+        pols.doc = "a dataset that's a container"
 
 
     # configure the journal
@@ -30,13 +35,19 @@ def test():
     # make a channel
     channel = journal.debug("pyre.h5.group")
 
-    # show me
-    channel.line(f"{Group.__name__}")
+    # sign on
+    channel.line(f"group: {Group.__name__}")
 
-    # go through the contents of the identifier map
+    # identifier section
     channel.line(f"  identifiers:")
+    # go through the contents of the identifier map
     for name, identifier in Group.pyre_idmap.items():
+        # represent
         channel.line(f"    {name}: {identifier}")
+        # the doc string
+        channel.line(f"      doc: {identifier.doc}")
+        # show me the default value
+        channel.line(f"      default: {identifier.default}")
 
     # flush
     channel.log()
