691d45ae6f6b40b14d8fd29f751057c19adb90ce
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-17 22:23:47 -0700

    pyre.h5: primed {inventory}, a {dict} subclass that will hold the descriptor values

---
+++ packages/pyre/h5/Inventory.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# value storage
+class Inventory(dict):
+    """
+    Storage for the values of identifiers
+    """
+
+
+# end of file
