1392d8c84b151d50594d502cfce6f2f8481c1f63
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-17 22:19:58 -0700

    pyre.h5: primed the class hierarchy to loosely mirror the {c++} layer of {hdf5}: at the root of the hierarchy, {identifier} corresponds to {IdComponent}

---
+++ packages/pyre/h5/Identifier.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# base class of all h5 objects
+class Identifier:
+    """
+    A placeholder for h5 identifiers, a very very low level concept
+    """
+
+
+    # metamethods
+    def __init__(self, name=None, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # my name
+        self.pyre_name = name
+        # all done
+        return
+
+
+    # descriptor support
+    def __set_name__(self, cls, name):
+        """
+        Attach my name
+        """
+        # attach my name
+        self.pyre_name = name
+        # all done
+        return
+
+
+    def __get__(self, instance, cls):
+        """
+        Read access to my value
+        """
+        print(f"{self.pyre_name}.__get__: {cls=}, {instance=}")
+
+
+    def __set__(self, instance, value):
+        """
+        Write access to my value
+        """
+        print(f"{self.pyre_name}.__set__: {instance=}, {value=}")
+
+
+    # rep
+    def __str__(self):
+        """
+        Human readable representation
+        """
+        # easy enough
+        return "an identifier"
+
+
+# end of file
