7dab513ee3e5fa182a257a1d89aa9cc77d938e70
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-17 22:24:54 -0700

    pyre.h5: added {location}, the base class that manages the address of all {h5} objects that have one

---
+++ packages/pyre/h5/Location.py
@@ -0,0 +1,48 @@
+#-*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# superclass
+from .Identifier import Identifier
+# metaclass
+from .Schema import Schema
+# my parts
+from .Inventory import Inventory
+
+
+# base class of all h5 objects that have an address
+class Location(Identifier, metaclass=Schema):
+    """
+    The base class for all h5 objects that have an address
+    """
+
+
+    # metamethods
+    def __init__(self, name=None, at=None, **kwds):
+        # chain up
+        super().__init__(name=name, **kwds)
+        # record the location
+        self.pyre_location = at if at is not None else name
+        # initialize my inventory
+        self.pyre_inventory = Inventory()
+        # all done
+        return
+
+
+    def __set_name__(self, cls, name):
+        """
+        Attach my name
+        """
+        # chain up
+        super().__set_name__(cls, name)
+        # if i don't have a location
+        if self.pyre_location is None:
+            # use my name as my location
+            self.pyre_location = name
+        # all done
+        return
+
+
+# end of file
