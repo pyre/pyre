b3670fd2989ab0642ac2cf548f9e2675772d4cd4
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-17 22:29:55 -0700

    pyre.h5: {dataset} now derives from {object} and it is {typed}

--- packages/pyre/h5/Dataset.py
+++ packages/pyre/h5/Dataset.py
@@ -4,11 +4,27 @@
 # (c) 1998-2022 all rights reserved
 
 
+# superclass
+from .Object import Object
+# type information
+from .. import schemata
+
+
 # the dataset descriptor base class
-class Dataset:
+@schemata.typed
+class Dataset(Object):
     """
     The base class of all dataset descriptors
     """
+
+
+    # metamethods
+    def __str__(self):
+        """
+        Human readable representation
+        """
+        # easy enough
+        return f"a dataset of type '{self.typename}'"
 
 
 # end of file
