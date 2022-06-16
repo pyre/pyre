e6e56ca3819172cc1f1d9409d9429ce0f14882f3
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-06-16 16:56:08 +0300

    pyre::h5: primed the support for {mode} in the {file} constructor

--- extensions/h5/file.cc
+++ extensions/h5/file.cc
@@ -26,12 +26,20 @@ h5::py::file(py::module & m)
     // constructor
     cls.def(
         // the implementation
-        py::init([](std::string path) {
-            // make a read-only entity and return it
+        py::init([](std::string path, std::string mode) {
+            // valid modes, per {h5py}
+            //    r: read-only, file must exist
+            //   r+: read/write, file must exist
+            //    w: create file, truncate if it exists
+            //   w-: create file, fail if it exists
+            //    x: alias for avove
+            //    a: create if it doesn't exist, read/write regardless
+
+            // for now, make a read-only entity and return it
             return new File(path, H5F_ACC_RDONLY);
         }),
         // the signature
-        "path"_a,
+        "path"_a, "mode"_a = "r",
         // the docstring
         "open an HDF5 file given its {path}");
 
