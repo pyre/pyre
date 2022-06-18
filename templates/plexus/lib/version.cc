255378c6f67525538ab4d989f1c7a81092a57c66
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-06-18 12:51:58 +0300

    templates/plexus: updated the package {lib}

--- templates/plexus/lib/version.cc
+++ templates/plexus/lib/version.cc
@@ -4,20 +4,16 @@
 // {project.authors}
 // {project.affiliations}
 // (c) {project.span} all rights reserved
-//
-
 
 // my declarations
 #include "version.h"
 
-
-// my definitions
-const char *
-{project.name}::
-version() {{
-    // my version number
-    return "1.0";
-}}
-
+// build and return the version tuple
+auto
+{project.name}::version::version() -> version_t
+{
+    // easy enough
+    return version_t { major, minor, micro, revision };
+}
 
 // end of file
