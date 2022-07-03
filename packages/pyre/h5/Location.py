61d4e9ad78fe402e82ce24f4aa634e44954e4c81
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-03 13:44:54 +0300

    pyre.h5: {location} is no longer a {schema} instance; {pyre_location} is now {pathlike}

--- packages/pyre/h5/Location.py
+++ packages/pyre/h5/Location.py
@@ -4,56 +4,38 @@
 # (c) 1998-2022 all rights reserved
 
 
+# external
+import pyre
 # superclass
 from .Identifier import Identifier
-# metaclass
-from .Schema import Schema
+# typing
+import typing
 
 
 # base class of all h5 objects that have an address
-class Location(Identifier, metaclass=Schema):
+class Location(Identifier):
     """
     The base class for all h5 objects that have an address
     """
 
 
-    # interface
-    def pyre_datasets(self):
-        """
-        Generate a sequence of my datasets
-        """
-        # i don't have any
-        return []
-
-
-    def pyre_groups(self):
-        """
-        Generate a sequence of my subgroups
-        """
-        # i don't have any
-        return []
-
-
-    def pyre_locations(self):
-        """
-        Generate a sequence of locations among my contents
-        """
-        # i don't have any
-        return []
-
-
     # metamethods
-    def __init__(self, name=None, at=None, **kwds):
+    def __init__(self,
+                 name: typing.Optional[str] = None,
+                 at: typing.Optional[str | pyre.primitives.pathlike] = None,
+                 **kwds):
         # chain up
         super().__init__(name=name, **kwds)
+        # prefer {at} as the location, and fall back to {name} if it's trivial
+        location = at or name
         # record the location
-        self.pyre_location = at if at is not None else name
+        self.pyre_location = pyre.primitives.path(location) if location else None
         # all done
         return
 
 
     # framework hooks
-    def pyre_bind(self, name):
+    def pyre_bind(self, name: str):
         """
         Attach my name
         """
@@ -62,7 +44,7 @@ class Location(Identifier, metaclass=Schema):
         # if i don't have a location
         if self.pyre_location is None:
             # use my name as my location
-            self.pyre_location = name
+            self.pyre_location = pyre.primitives.path(name)
         # all done
         return
 
