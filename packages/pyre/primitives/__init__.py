66afbe105acf41f1af4175f23fdd7b608ab582f3
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-03 13:45:17 +0300

    pyre.primitives: added a {pathlike} typing shortcut

--- packages/pyre/primitives/__init__.py
+++ packages/pyre/primitives/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2022 all rights reserved
-#
-
 
 # the extensions to the primitive datatypes
 from .Path import Path as path
 from .PathHash import PathHash as pathhash
 from .URI import URI as uri
 
+# typing
+import os
+# my shorthand
+pathlike = os.PathLike | str
 
 # end of file
