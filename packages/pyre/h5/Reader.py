52e01a139ece45fdb411cf20649f08f4ccf59b68
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-05 13:42:37 +0300

    pyre.h5: the {query} traversal by the {reader} is now significantly simpler; it also realizes the {pyre_id} of all nodes in its resulting structure

--- packages/pyre/h5/Reader.py
+++ packages/pyre/h5/Reader.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
+# support
+import journal
+
 # superclass
 from .File import File
+
+# typing
+import os
+from .Dataset import Dataset
+from .Group import Group
+from .Location import Location
 
 
 # the base reader
@@ -11,17 +20,15 @@ class Reader(File):
     The base reader for h5 products
     """
 
-
     # interface
-    def open(self, path):
+    def open(self, path: os.PathLike) -> File:
         """
         Access the h5 file at {path}
         """
         # set the mode and delegate
         return super().open(path=path, mode="r")
 
-
-    def read(self, query=None):
+    def read(self, query: Location = None) -> Location:
         """
         Open the h5 file at {path} and read the information in {query}
         """
@@ -29,37 +36,36 @@ class Reader(File):
         if query is None:
             # grab my schema and use it for guidance as to what to read from the file
             query = self.schema()
-
-        # visit the {query} structure and return the result
-        return query.pyre_identify(authority=self)
-
+        # visit the {query} structure and return the result; the initial {parent} is the {file}
+        # object, therefore {query} must be anchored by an element whose {pyre_location} is a
+        # valid absolute path
+        return query.pyre_identify(authority=self, parent=self)
 
     # implementation details
-    def pyre_onDataset(self, dataset, parent=None):
+    def pyre_onDataset(self, dataset: Dataset, parent: Group) -> Dataset:
         """
         Process a {group} at {prefix}
         """
-        # clone
-        clone = dataset.pyre_clone()
-        # if i have a known parent
-        if parent:
-            # attach me
-            parent.pyre_set(descriptor=clone, value=clone)
+        # realize the h5 object that gives me access to my contents
+        hid = parent.pyre_id.dataset(path=str(dataset.pyre_location))
+        # clone the {dataset}
+        clone = dataset.pyre_clone(id=hid)
+        # attach the clone to its parent
+        parent.pyre_set(descriptor=clone, identifier=clone)
         # all done
         return clone
 
-
-    def pyre_onGroup(self, group, parent=None):
+    def pyre_onGroup(self, group: Group, parent: Group) -> Group:
         """
         Process a {group}
         """
-        # clone
-        clone = group.pyre_clone()
-        # if i have a known parent
-        if parent:
-            # attach me
-            parent.pyre_set(descriptor=clone, value=clone)
-        # go through my children
+        # realize the h5 object that gives me access to the group contents
+        hid = parent.pyre_id.group(path=str(group.pyre_location))
+        # clone it
+        clone = group.pyre_clone(id=hid)
+        # attach it to its parent
+        parent.pyre_set(descriptor=clone, identifier=clone)
+        # now, go through its children
         for child in group.pyre_locations():
             # and visit each one
             child.pyre_identify(authority=self, parent=clone)
