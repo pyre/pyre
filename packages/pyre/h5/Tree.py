a6e4c3a220cd705b65b0183111a5f330f62866c3
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-05 13:35:21 +0300

    pyre.h5: {tree} cosmetics by {black}

--- packages/pyre/h5/Tree.py
+++ packages/pyre/h5/Tree.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2022 all rights reserved
@@ -8,7 +8,6 @@ class Tree:
     """
     A visitor that draws the structure of {location} instances
     """
-
 
     # interface
     def explore(self, location, margin="", graphic=""):
@@ -23,22 +22,25 @@ class Tree:
             return
 
         # set up the decorations for all but the last child
-        bodyMargin = margin + '|  '
-        bodyGraphic = margin + '+- '
+        bodyMargin = margin + "|  "
+        bodyGraphic = margin + "+- "
         # go through all but the last child
         for child in children[:-1]:
             # and explore each one
-            yield from self.explore(location=child, margin=bodyMargin, graphic=bodyGraphic)
+            yield from self.explore(
+                location=child, margin=bodyMargin, graphic=bodyGraphic
+            )
 
         # the last child gets decorated slightly differently
-        lastMargin = margin + '   '
-        lastGraphic = margin + '`- '
+        lastMargin = margin + "   "
+        lastGraphic = margin + "`- "
         # repeat for the last child
-        yield from self.explore(location=children[-1], margin=lastMargin, graphic=lastGraphic)
+        yield from self.explore(
+            location=children[-1], margin=lastMargin, graphic=lastGraphic
+        )
 
         # all done
         return
-
 
     # metamethods
     def __init__(self, indent=0, **kwds):
@@ -49,7 +51,6 @@ class Tree:
         # all done
         return
 
-
     # implementation details
     def render(self, location, graphic=""):
         # unpack my state
@@ -59,7 +60,6 @@ class Tree:
         marker = location.pyre_marker
         # build a string and return it
         return f"{indent}{graphic}{label} ({marker})"
-
 
     # constant
     INDENT = " " * 2
