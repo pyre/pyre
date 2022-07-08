a73c305af8990abca64a148aeaa78341aecf6cce
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-08 13:54:08 +0300

    pyre.h5: fleshed out the interface of {datset} value synchronization

--- packages/pyre/h5/Dataset.py
+++ packages/pyre/h5/Dataset.py
@@ -5,6 +5,7 @@
 
 
 # support
+import journal
 import pyre
 
 # superclass
@@ -18,6 +19,9 @@ class Dataset(Object):
     The base class of all dataset descriptors
     """
 
+    # my mixins
+    from .typed import bool, int, str, timestamp
+
     # public data
     @property
     def value(self):
@@ -26,8 +30,12 @@ class Dataset(Object):
         """
         # get the value from my cache
         value = self._value
-        # and return it
-        return value
+        # if it's not set
+        if value is None:
+            # fall back to my default
+            value = self.default
+        # process it and return it
+        return self.process(value=value)
 
     @value.setter
     def value(self, value):
@@ -47,12 +55,49 @@ class Dataset(Object):
         # use my type name
         return self.typename
 
+    # interface
+    def pyre_read(self):
+        """
+        Read my on-disk value into my cache
+        """
+        # if i'm not mapped to an h5 file
+        if self.pyre_id is None:
+            # make a channel
+            channel = journal.warning("pyre.h5.sync")
+            # complain
+            channel.line(f"'{self.pyre_name}' is not synced to an h5 file")
+            channel.line(f"while attempting to read '{self.pyre_location}'")
+            # flush
+            channel.log()
+            # and return something harmless
+            return None
+        # if all is well, delegate
+        return self.pyre_pull()
+
+    def pyre_write(self):
+        """
+        Write my cache value to disk
+        """
+        # if i'm not mapped to an h5 file
+        if self.pyre_id is None:
+            # make a channel
+            channel = journal.warning("pyre.h5.sync")
+            # complain
+            channel.line(f"'{self.pyre_name}' is not synced to an h5 file")
+            channel.line(f"while attempting to write '{self.pyre_location}'")
+            # flush
+            channel.log()
+            # and bail
+            return
+        # if all is well, delegate
+        return self.pyre_push()
+
     # metamethods
-    def __init__(self, **kwds):
+    def __init__(self, value=None, **kwds):
         # chain up
         super().__init__(**kwds)
         # set up my {value}
-        self._value = self.process(value=self.default)
+        self._value = value
         # all done
         return
 
@@ -79,12 +124,35 @@ class Dataset(Object):
         # otherwise, invoke the handler
         return handler(dataset=self, **kwds)
 
-    def pyre_clone(self, **kwds):
+    def pyre_clone(self, **kwds) -> "Dataset":
         """
         Make as faithful a clone of mine as possible
         """
         # invoke my constructor
-        return super().pyre_clone(default=self.default, **kwds)
+        return super().pyre_clone(value=self.value, default=self.default, **kwds)
+
+    # implementation details
+    def pyre_pull(self):
+        """
+        Pull my on-disk value into my cache
+        """
+        # for now
+        return None
+        # my children know how to do this
+        raise NotImplementedError(
+            f"class '{type(self).__name__}' must implement 'pyre_pull'"
+        )
+
+    def pyre_push(self):
+        """
+        Push my cache value to disk
+        """
+        # for now
+        return None
+        # my children know how to do this
+        raise NotImplementedError(
+            f"class '{type(self).__name__}' must implement 'pyre_push'"
+        )
 
 
 # end of file
