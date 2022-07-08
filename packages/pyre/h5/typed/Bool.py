fabf9b3def0920d67593249d1c65266fb69d00ee
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-08 13:43:18 +0300

    pyre.h5: added {typed}, a module with {dataset} mixins

---
+++ packages/pyre/h5/typed/Bool.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# the {bool} mixin
+class Bool:
+    """
+    Implementation details of the {bool} dataset mixin
+    """
+
+    # value synchronization
+    def pyre_pull(self):
+        """
+        Read my value from disk and update my cache
+        """
+        # read the value; values are stored as strings, for now
+        value = self.pyre_id.str()
+        # store it
+        self.value = value
+        # and return the raw contents
+        return value
+
+
+# end of file
