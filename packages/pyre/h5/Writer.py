c514e05e582e1295298897f84b0478f0632b0664
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-08 13:55:42 +0300

    pyre.h5: the {writer} is no longer a {file} subclass

--- packages/pyre/h5/Writer.py
+++ packages/pyre/h5/Writer.py
@@ -1,32 +1,41 @@
 # -*- coding: utf-8 -*-
 
 
-# superclass
+# parts
 from .File import File
+
+# typing
+import pyre
+from .Group import Group
 
 
 # the base writer
-class Writer(File):
+class Writer:
     """
     The base writer for h5 products
     """
 
-
     # interface
-    def open(self, path, mode="a"):
-        """
-        Access the h5 file at {path}
-        """
-        # set the mode and delegate
-        return super().open(path=path, mode=mode)
-
-
-    def write(self, query, path=None):
+    def write(self, path: pyre.primitives.pathlike, query: Group, mode="w"):
         """
         Open the h5 file at {path} and write the information in {query}
         """
-        # all done
-        return query
+        # open the file
+        file = File().open(path=path, mode=mode)
+        # visit the {query} structure and return the result; the initial {parent} is the {file}
+        # object, therefore {query} must be anchored by an element whose {pyre_location} is a
+        # valid absolute path
+        return query.pyre_identify(authority=self, parent=file)
+
+    # implementation details
+    def schema(self):
+        """
+        Retrieve the schema of the data product
+        """
+        # i don't have one; force subclasses to define
+        raise NotImplementedError(
+            f"class '{type(self).__name__}' must implement 'schema'"
+        )
 
 
 # end of file
