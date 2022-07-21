4a1dc50b46d356a08f270d669e46904494dcb62b
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-07-21 12:24:50 +0300

    pyre.h5: added {float} to the {typed} {dataset} support

---
+++ packages/pyre/h5/typed/Float.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# the {float} mixin
+class Float:
+    """
+    Implementation details of the {float} dataset mixin
+    """
+
+    # value synchronization
+    def pyre_pull(self):
+        """
+        Read my value from disk and update my cache
+        """
+        # read the value
+        value = self.pyre_id.double()
+        # store it
+        self.value = value
+        # and return the raw contents
+        return value
+
+
+# end of file
