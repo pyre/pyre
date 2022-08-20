e0e638bb201c79e3f8e179f2ae1336fcd655b09d
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-08-20 14:58:36 +0300

    cmake: added the {datatypes} to the build of the {h5} bindings

--- .cmake/pyre_h5.cmake
+++ .cmake/pyre_h5.cmake
@@ -12,13 +12,22 @@ function(pyre_h5Module)
   set_target_properties(h5module PROPERTIES LIBRARY_OUTPUT_NAME h5)
   set_target_properties(h5module PROPERTIES SUFFIX ${PYTHON3_SUFFIX})
   # set the libraries to link against
+  if(HDF5_IS_PARALLEL)
   target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5 MPI::MPI_CXX)
+  else(HDF5_IS_PARALLEL)
+  target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5)
+  endif(HDF5_IS_PARALLEL)
   # add the sources
   target_sources(h5module PRIVATE
     h5/h5.cc
     h5/api.cc
     h5/dataset.cc
     h5/dataspace.cc
+    h5/datatypes/compound.cc
+    h5/datatypes/datatype.cc
+    h5/datatypes/datatypes.cc
+    h5/datatypes/native.cc
+    h5/datatypes/predefined.cc
     h5/file.cc
     h5/group.cc
     )
