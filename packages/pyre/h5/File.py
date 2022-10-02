05142d163b2d0beda7b83cfc12c24988772382dd
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-10-02 11:15:53 +0300

    pyre.h5: primed the support for {ros3}

--- packages/pyre/h5/File.py
+++ packages/pyre/h5/File.py
@@ -21,19 +21,57 @@ class File(Group):
     """
 
     # interface
-    def open(self, uri: pyre.primitives.pathlike, mode: str):
+    def open(self, uri: pyre.primitives.pathlike, mode: str) -> "File":
         """
         Access the h5 file at {uri}
+
+        The parameter {mode} can be one of:
+
+            r: read only
+           r+: read/write, file must exist
+            w: create file, truncate if it exists
+           w-: create file, fail if it exists
+            x: alias for w-
         """
         # open the file and attach my handle
         self.pyre_id = pyre.libh5.File(path=str(uri), mode=mode)
         # all done
         return self
 
+    def ros3(
+        self, bucket: str, key: str, profile: typing.Optional[str] = None
+    ) -> "File":
+        """
+        Access the remote dataset {key} in the given S3 {bucket}
+        """
+        # get the hdf5 bindings
+        libh5 = pyre.libh5
+        # if the library doesn't have support for {ros3}
+        if not libh5.ros3():
+            # make a channel
+            channel = journal.error("pyre.h5.file")
+            # complain
+            channel.line("this installation of libhdf5 has no support for ros3")
+            channel.line(f"while looking for key '{key}' in bucket '{bucket}'")
+            # flush
+            channel.log()
+            # and bail
+            return self
+        # make a {ros3} access parameter list
+        fapl = libh5.fapls.ros3(profile)
+        # attach it
+        self.pyre_fapl = fapl
+        # open the file and attach my handle
+        self.pyre_id = libh5.File(bucket, key, fapl)
+        # all done
+        return self
+
     # metamethods
-    def __init__(self, at="/", **kwds):
+    def __init__(self, at="/", fapl=None, **kwds):
         # chain up with root as my location, unless the client has something else to suggest
         super().__init__(at=at, **kwds)
+        # record my access property list
+        self.pyre_fapl = fapl
         # all done
         return
 
--- packages/pyre/h5/__init__.py
+++ packages/pyre/h5/__init__.py
@@ -4,6 +4,10 @@
 # (c) 1998-2022 all rights reserved
 
 
+# framework
+import pyre
+import journal
+
 # internals that mirror the h5 C++ api
 from .Identifier import Identifier as identifier
 from .Location import Location as location
@@ -40,6 +44,68 @@ from .Writer import Writer as writer
 from .Explorer import Explorer as explorer
 from .Tree import Tree as tree
 from .Walker import Walker as walker
+
+
+# file factory
+def open(uri: pyre.primitives.uri, mode: str = "r"):
+    """
+    Open the file given its {uri}
+    """
+    # parse the {uri}
+    uri = pyre.primitives.uri.parse(value=uri, scheme="file")
+
+    # if the scheme is {file}
+    if uri.scheme == "file":
+        # make a local file object whose path is the address in the {uri}
+        f = file().open(uri=uri.address, mode=mode)
+        # and return it
+        return f
+
+    # if the scheme is {s3}
+    if uri.scheme == "s3":
+        # if the library doesn't have support for {ros3}
+        if not pyre.libh5.ros3():
+            # make a channel
+            channel = journal.error("pyre.h5.file")
+            # complain
+            channel.line("this installation of libhdf5 has no support for ros3")
+            channel.line(f"while looking for '{uri}'")
+            # flush
+            channel.log()
+            # and bail
+            return None
+        # we interpret the {authority} field as the bucket name
+        authority = uri.authority
+        # so it must be there
+        if authority is None:
+            # make a channel
+            channel = journal.error("pyre.h5.open")
+            # complain
+            channel.line("couldn't figure out the bucket name")
+            channel.line(f"while looking for '{uri}'")
+            # flush
+            channel.log()
+            # and bail, in case errors aren't fatal
+            return None
+
+        # if it's there, take it apart
+        fields = uri.authority.split("@")
+        # extract the authentication profile and the name of the S3 bucket
+        profile, bucket = fields if len(fields) == 2 else "", uri.authority
+        # get the object key
+        key = uri.address
+        # open the remote dataset with the {ros3} driver
+        f = file().ros3(bucket=bucket, key=key)
+        # and return it
+        return f
+
+    # if we get this far, the {uri} was malformed; make a channel
+    channel = journal.error("pyre.h5")
+    # and complain
+    channel.log(f"unknown scheme in dataset '{uri}'")
+
+    # all done
+    return
 
 
 # end of file
