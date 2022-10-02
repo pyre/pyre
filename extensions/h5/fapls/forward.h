7a6e84dcc5fce5110daf00e5b542d31b3dfa4ad4
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-10-02 11:14:24 +0300

    pyre.h5: primed the {fapl} namespace

---
+++ extensions/h5/fapls/forward.h
@@ -0,0 +1,20 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(h5_py_fapls_forward_h)
+#define h5_py_fapls_forward_h
+
+
+// the local binders
+namespace pyre::h5::py::fapls {
+    // the module initializer
+    void fapls(py::module &);
+} // namespace pyre::h5::py::fapls
+
+
+#endif
+
+// end of file
