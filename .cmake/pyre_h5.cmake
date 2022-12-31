0d2f3e5a9a082aa6f9a0eb156b8717f45612cf32
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-12-31 13:57:00 -0800

    cmake: the {h5} module now builds only if {cmake} was able to find the necessary packages

--- .cmake/pyre_h5.cmake
+++ .cmake/pyre_h5.cmake
@@ -6,37 +6,39 @@
 
 # build the h5 python extension
 function(pyre_h5Module)
-  # h5
-  Python_add_library(h5module MODULE)
-  # adjust the name to match what python expects
-  set_target_properties(h5module PROPERTIES LIBRARY_OUTPUT_NAME h5)
-  set_target_properties(h5module PROPERTIES SUFFIX ${PYTHON3_SUFFIX})
-  # set the libraries to link against
-  if(HDF5_IS_PARALLEL)
-  target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5 MPI::MPI_CXX)
-  else(HDF5_IS_PARALLEL)
-  target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5)
-  endif(HDF5_IS_PARALLEL)
-  # add the sources
-  target_sources(h5module PRIVATE
-    h5/h5.cc
-    h5/api.cc
-    h5/dataset.cc
-    h5/dataspace.cc
-    h5/datatypes/compound.cc
-    h5/datatypes/datatype.cc
-    h5/datatypes/datatypes.cc
-    h5/datatypes/native.cc
-    h5/datatypes/predefined.cc
-    h5/file.cc
-    h5/group.cc
-    )
-  # install
-  install(
-    TARGETS h5module
-    LIBRARY
-    DESTINATION ${PYRE_DEST_PACKAGES}/pyre/extensions
-    )
+  if(HDF5_FOUND)
+    # h5
+    Python_add_library(h5module MODULE)
+    # adjust the name to match what python expects
+    set_target_properties(h5module PROPERTIES LIBRARY_OUTPUT_NAME h5)
+    set_target_properties(h5module PROPERTIES SUFFIX ${PYTHON3_SUFFIX})
+    # set the libraries to link against
+    if(HDF5_IS_PARALLEL)
+      target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5 MPI::MPI_CXX)
+    else(HDF5_IS_PARALLEL)
+      target_link_libraries(h5module PRIVATE journal pybind11::module HDF5::HDF5)
+    endif(HDF5_IS_PARALLEL)
+    # add the sources
+    target_sources(h5module PRIVATE
+      h5/h5.cc
+      h5/api.cc
+      h5/dataset.cc
+      h5/dataspace.cc
+      h5/datatypes/compound.cc
+      h5/datatypes/datatype.cc
+      h5/datatypes/datatypes.cc
+      h5/datatypes/native.cc
+      h5/datatypes/predefined.cc
+      h5/file.cc
+      h5/group.cc
+      )
+    # install
+    install(
+      TARGETS h5module
+      LIBRARY
+      DESTINATION ${PYRE_DEST_PACKAGES}/pyre/extensions
+      )
+  endif(HDF5_FOUND)
 endfunction(pyre_h5Module)
 
 
