ebf5b70282530be2ad3340be579840e3f8e16c86
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-09 20:59:28 +0200

    pyre.h5: trimmed the top level imports

--- packages/pyre/h5/__init__.py
+++ packages/pyre/h5/__init__.py
@@ -4,30 +4,11 @@
 # (c) 1998-2023 all rights reserved
 
 
-# framework
-import pyre
-import journal
-
 # the new implementation
 from . import schema
 from . import api
 
-# internals that mirror the h5 C++ api
-from .Identifier import Identifier as identifier
-from .Location import Location as location
-from .Object import Object as object
-
-# structural objects
-from .Group import Group as group
-from .File import File as file
-from .Dataset import Dataset as dataset
-
-# readers and writers
-from .Reader import Reader as reader
-from .Writer import Writer as writer
-
-# visitors
-from .Walker import Walker as walker
+# convenient access to key factories
 
 
 # end of file
