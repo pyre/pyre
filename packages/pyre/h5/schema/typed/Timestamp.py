dd5e7d4a96e63ac24d92de0fff2b0c2441f83d88
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-22 18:03:03 -0800

    pyre.h5: on read, the {typed} descriptors now do value processing during its extraction from the file

--- packages/pyre/h5/schema/typed/Timestamp.py
+++ packages/pyre/h5/schema/typed/Timestamp.py
@@ -25,8 +25,8 @@ class Timestamp:
         """
         # read the value; timestamps are stored as strings
         value = dataset._pyre_id.str()
-        # and return the raw contents
-        return value[:26]
+        # process it and return it
+        return self.process(value[:26])
 
 
 # end of file
