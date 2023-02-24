7ca35cf181c37b329e7c768cbb770790c43b0638
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-24 09:45:22 -0800

    pyre.h5: fleshed out the {str} bindings in {datatypes}

--- extensions/h5/datatypes/StrType.cc
+++ extensions/h5/datatypes/StrType.cc
@@ -19,7 +19,7 @@ void
 pyre::h5::py::datatypes::str(py::module & m)
 {
     // add the class
-    auto strType = py::class_<StrType, AtomType>(
+    auto cls = py::class_<StrType, AtomType>(
         // in scope
         m,
         // class name
@@ -27,8 +27,32 @@ pyre::h5::py::datatypes::str(py::module & m)
         // docstring
         "an HDF5 string datatype");
 
-    // constructor
-    strType.def(py::init<>());
+    // constructors
+    // default
+    cls.def(py::init<>());
+    // from a predefined float type
+    cls.def(py::init<const PredType &>());
+
+    // properties
+    cls.def_property(
+        // the name
+        "charset",
+        // the getter
+        &StrType::getCset,
+        // the setter
+        &StrType::setCset,
+        // the docstring
+        "get/set the string character set");
+
+    cls.def_property(
+        // the name
+        "strpad",
+        // the getter
+        &StrType::getStrpad,
+        // the setter
+        &StrType::setStrpad,
+        // the docstring
+        "get/set the string padding method");
 
     // all done
     return;
