de1e98ef02bdc59b2874d1c4c9cbc21646b6cfab
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-24 07:33:02 -0800

    pyre.h5: fleshed out the {float} datatype

--- extensions/h5/datatypes/FloatType.cc
+++ extensions/h5/datatypes/FloatType.cc
@@ -19,7 +19,7 @@ void
 pyre::h5::py::datatypes::float_(py::module & m)
 {
     // add the class
-    auto floatType = py::class_<FloatType, AtomType>(
+    auto cls = py::class_<FloatType, AtomType>(
         // in scope
         m,
         // class name
@@ -27,8 +27,73 @@ pyre::h5::py::datatypes::float_(py::module & m)
         // docstring
         "an HDF5 float datatype");
 
-    // constructor
-    floatType.def(py::init<>());
+    // constructors
+    // default
+    cls.def(py::init<>());
+    // from a predefined float type
+    cls.def(py::init<const PredType &>());
+
+    // properties
+    cls.def_property(
+        // the name
+        "bias",
+        // the getter
+        &FloatType::getEbias,
+        // the setter
+        &FloatType::setEbias,
+        // the docstring
+        "get/set the exponent bias");
+
+    cls.def_property(
+        // the name
+        "normalization",
+        // the getter
+        [](const FloatType & self) {
+            // bypass the silly C++ interface that generates a textual representation of the enum
+            return H5Tget_norm(self.getId());
+        },
+        // the setter
+        &FloatType::setNorm,
+        // the docstring
+        "get/set the mantissa normalization strategy");
+
+    cls.def_property(
+        // the name
+        "inpad",
+        // the getter
+        [](const FloatType & self) {
+            // bypass the silly C++ interface that generates a textual representation of the enum
+            return H5Tget_inpad(self.getId());
+        },
+        // the setter
+        &FloatType::setInpad,
+        // the docstring
+        "get/set the internal padding for unused bits");
+
+    cls.def_property(
+        // the name
+        "fields",
+        // the getter
+        [](FloatType & self) {
+            // make some room
+            std::size_t spos, epos, esize, mpos, msize;
+            // read the fields
+            self.getFields(spos, epos, esize, mpos, msize);
+            // and return them
+            return std::make_tuple(spos, epos, esize, mpos, msize);
+        },
+        // the setter
+        [](FloatType & self,
+           std::tuple<std::size_t, std::size_t, std::size_t, std::size_t, std::size_t> fields) {
+            // unpack
+            auto [spos, epos, esize, mpos, msize] = fields;
+            // and set
+            self.setFields(spos, epos, esize, mpos, msize);
+            // all done
+            return;
+        },
+        // the docstring
+        "get/set the padding strategy for (lsb, msb)");
 
     // all done
     return;
