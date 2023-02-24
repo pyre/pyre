5d3c996bb79c6e575b95ab5c020c614b3d38e1ec
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-24 06:26:01 -0800

    pyre.h5: fleshed out the {atomtype} bindings

--- extensions/h5/datatypes/AtomType.cc
+++ extensions/h5/datatypes/AtomType.cc
@@ -19,13 +19,81 @@ void
 pyre::h5::py::datatypes::atom(py::module & m)
 {
     // add the class
-    auto atomType = py::class_<AtomType, DataType>(
+    auto cls = py::class_<AtomType, DataType>(
         // in scope
         m,
         // class name
         "AtomType",
         // docstring
         "an HDF5 atom datatype");
+
+    // properties
+    cls.def_property(
+        // the name
+        "order",
+        // the getter
+        [](const AtomType & self) {
+            // get the byte order and return it
+            return self.getOrder();
+        },
+        // the setter
+        &AtomType::setOrder,
+        // the docstring
+        "get/set the byte order");
+
+    cls.def_property(
+        // the name
+        "offset",
+        // the getter
+        &AtomType::getOffset,
+        // the setter
+        &AtomType::setOffset,
+        // the docstring
+        "get/set the bit offset of the first significant bit");
+
+    cls.def_property(
+        // the name
+        "pad",
+        // the getter
+        [](AtomType & self) {
+            // make some room
+            H5T_pad_t lsb, msb;
+            // read the padding types
+            self.getPad(lsb, msb);
+            // and return them
+            return std::make_pair(lsb, msb);
+        },
+        // the setter
+        [](AtomType & self, std::pair<H5T_pad_t, H5T_pad_t> pad) {
+            // unpack
+            auto [lsb, msb] = pad;
+            // and set
+            self.setPad(lsb, msb);
+            // all done
+            return;
+        },
+        // the docstring
+        "get/set the padding strategy for (lsb, msb)");
+
+    cls.def_property(
+        // the name
+        "precision",
+        // the getter
+        &AtomType::getPrecision,
+        // the setter
+        &AtomType::setPrecision,
+        // the docstring
+        "get/set the precision");
+
+    cls.def_property(
+        // the name
+        "bytes",
+        // the getter
+        &AtomType::getSize,
+        // the setter
+        &AtomType::setSize,
+        // the docstring
+        "get/set the overall size");
 
     // all done
     return;
