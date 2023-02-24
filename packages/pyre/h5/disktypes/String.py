04fc79fb5924119569bf0681fd9c14bd8a6cb16f
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-23 18:10:48 -0800

    pyre:h5: primed the {disktypes}

---
+++ packages/pyre/h5/disktypes/String.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2023 all rights reserved
+
+
+# my superclass is in libh5
+import pyre
+
+
+# arrays
+class String(pyre.libh5.datatypes.StrType):
+    """
+    An h5 StrType
+    """
+
+
+# end of file
