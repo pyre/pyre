a6505fe71fac7a888d650306a5a1da82441c35e7
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-23 18:14:31 -0800

    pyre.h5: pulled {memtypes} and {disktypes} at top level; protected the package against a missing {libh5}

--- packages/pyre/h5/__init__.py
+++ packages/pyre/h5/__init__.py
@@ -3,13 +3,29 @@
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2023 all rights reserved
 
+# support
+import journal
 
-# the new implementation
-from . import schema
-from . import api
+# we may not have h5 runtime support, so
+try:
+    # to pull in the subpackages
+    from . import disktypes
+    from . import memtypes
+    from . import schema
+    from . import api
 
-# shortcuts
-reader = api.reader
+    # shortcuts
+    reader = api.reader
+
+# if anything goes wrong
+except AttributeError as error:
+    # make a channel
+    channel = journal.warning("pyre.h5")
+    # report
+    channel.line(str(error))
+    channel.line("while importing 'pyre.h5'")
+    # flush
+    channel.log()
 
 
 # end of file
