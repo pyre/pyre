9d25e4dc1d9258c9db314b8a452b398dff64fe49
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-26 00:03:22 -0800

    pyre::grid: restructured the way the {grid_t} templates are expanded and bound; added girds on the heap, and the missing unsigned integral types

--- extensions/pyre/grid/grids.h
+++ extensions/pyre/grid/grids.h
@@ -10,50 +10,38 @@
 
 // the {pyre} extension namespace
 namespace pyre::py::grid {
-    // the grid instantiations
-    // 2d
-    void byteConstMapGrid2D(py::module &);
-    void int16ConstMapGrid2D(py::module &);
-    void int32ConstMapGrid2D(py::module &);
-    void int64ConstMapGrid2D(py::module &);
-    void floatConstMapGrid2D(py::module &);
-    void doubleConstMapGrid2D(py::module &);
-    void complexFloatConstMapGrid2D(py::module &);
-    void complexDoubleConstMapGrid2D(py::module &);
-    // 3d
-    void byteConstMapGrid3D(py::module &);
-    void int16ConstMapGrid3D(py::module &);
-    void int32ConstMapGrid3D(py::module &);
-    void int64ConstMapGrid3D(py::module &);
-    void floatConstMapGrid3D(py::module &);
-    void doubleConstMapGrid3D(py::module &);
-    void complexFloatConstMapGrid3D(py::module &);
-    void complexDoubleConstMapGrid3D(py::module &);
-    // 4d
-    void byteConstMapGrid4D(py::module &);
-    void int16ConstMapGrid4D(py::module &);
-    void int32ConstMapGrid4D(py::module &);
-    void int64ConstMapGrid4D(py::module &);
-    void floatConstMapGrid4D(py::module &);
-    void doubleConstMapGrid4D(py::module &);
-    void complexFloatConstMapGrid4D(py::module &);
-    void complexDoubleConstMapGrid4D(py::module &);
+    // the class record factories
+    template <typename storageT, int dim>
+    auto bind(py::module & m);
 
-    // the interface decorators
+    template <typename storageT, int dim>
+    auto bindconst(py::module & m);
+
+    // the make of the record
+    template <typename storageT, int dim>
+    auto makecls(py::module & m);
+
+    // add constructors
     template <class gridT>
-    void constmapInterface(py::class_<gridT> &);
+    void constructors(py::class_<gridT> &);
 
+    // write interface
     template <class gridT>
-    void mapInterface(py::class_<gridT> &);
+    void read(py::class_<gridT> &);
 
+    // read interface
     template <class gridT>
-    void constgridInterface(py::class_<gridT> &);
+    void write(py::class_<gridT> &);
 
-    template <class gridT>
-    void gridInterface(py::class_<gridT> &);
-
+    // the typename table
+    static std::unordered_map<std::type_index, std::string> typenames;
 } // namespace pyre::py::grid
 
+
+// get the inline definitions
+#define pyre_py_grid_grids_icc
+#include "grids.icc"
+#undef pyre_py_grid_grids_icc
 
 #endif
 
