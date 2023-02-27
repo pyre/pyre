1a43c3c8dec937362ee4ae9952d08b83703f0468
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-02-26 18:02:22 -0800

    pyre.h5: {memtypes} now know to which {native} type they correspond

--- packages/pyre/h5/memtypes/Int64.py
+++ packages/pyre/h5/memtypes/Int64.py
@@ -4,6 +4,9 @@
 # (c) 1998-2023 all rights reserved
 
 
+# support
+import pyre
+
 # superclass
 from .MemoryType import MemoryType
 
@@ -16,6 +19,7 @@ class Int64(MemoryType):
 
     # constants
     ctype = "int64_t"
+    htype = pyre.libh5.datatypes.native.int64
 
 
 # end of file
