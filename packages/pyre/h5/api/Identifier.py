f9f3ab33f1eaac8b2a248d4eaa8ea201160b97cf
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-03-17 13:10:08 -0700

    pyre.h5: {identifier} can now ask its handle for its {H5I_type_t}

--- packages/pyre/h5/api/Identifier.py
+++ packages/pyre/h5/api/Identifier.py
@@ -42,18 +42,27 @@ class Identifier:
         return f"an identifier"
 
     # framework hooks
+    # properties
+    @property
+    def _pyre_identifierType(self):
+        """
+        Look up my h5 identifier type
+        """
+        # my handle knows
+        return self._pyre_id.identifierType
+
     # clean up
     def _pyre_close(self) -> "Identifier":
         """
         Detach me from my HDF5 object
         """
         # get my id
-        hid = self._pyre_id
+        h5id = self._pyre_id
         # if it's valid
-        if hid is not None:
+        if h5id is not None:
             # close it
-            hid.close()
-            # reset my id
+            h5id.close()
+            # and invalidate me
             self._pyre_id = None
         # all done
         return self
