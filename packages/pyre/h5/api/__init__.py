33425186ad732fb3e30f9485ef87a553e8d6c487
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-03-19 19:50:14 -0700

    pyre.h5: {api} now exports its {exceptions} and the {inspector}

--- packages/pyre/h5/api/__init__.py
+++ packages/pyre/h5/api/__init__.py
@@ -3,6 +3,8 @@
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2023 all rights reserved
 
+# the local exceptions
+from . import exceptions
 
 # internals that mirror the h5 c++ hierarchy
 from .Identifier import Identifier as identifier
@@ -13,6 +15,9 @@ from .Object import Object as object
 from .Group import Group as group
 from .Dataset import Dataset as dataset
 from .File import File as file
+
+# infrastructure
+from .Inspector import Inspector as inspector
 
 # visitors
 from .Explorer import Explorer as explorer
