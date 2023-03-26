01ee052cfeed664bcc5e218c7f4b2bd5da013c81
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-03-25 19:06:56 -0700

    merlin.assets: {library.assets} is now a property so it is consistent with the {folder} interface, in preparation for an asset container protocol

--- packages/merlin/assets/Library.py
+++ packages/merlin/assets/Library.py
@@ -46,6 +46,19 @@ class Library(
     headers = merlin.properties.set(schema=merlin.protocols.file())
     headers.doc = "the library headers as a set of products in /prefix"
 
+    # derived data
+    @property
+    def assets(self):
+        """
+        Explore my {root} and collect my assets
+        """
+        # N.B.:
+        #   we don't cache the results of the {root} exploration so we can always
+        #   return a result that reflects the current contents of the filesystem
+        yield from self._explore()
+        # all done
+        return
+
     # interface
     def build(self, builder, **kwds):
         """
@@ -61,80 +74,6 @@ class Library(
             header.build(builder=builder, **kwds)
         # all done
         return super().build(builder=builder, **kwds)
-
-    def assets(self) -> collections.abc.Iterable:
-        """
-        Generate the sequence of my source files
-        """
-        # get the workspace folder
-        ws = self.pyre_fileserver["/workspace"]
-        # starting with it
-        root = ws
-        # navigate down to my {root} carefully one step at a time because the filesystem
-        # may not have been fully explored by the time we get here
-        for folder in self.root.parts:
-            # if {root} is empty
-            if not root.contents:
-                # it probably just requires exploring; gently...
-                root.discover(levels=1)
-            # if the {folder} is already among the contents of the current directory,
-            # someone else has visited and explored this level; mark this folder as
-            # the place to explore and move on
-            root = root[folder]
-
-        # if we get his far, my {root} exists; all my sources live here
-        # so let's explore this subtree
-        root.discover()
-
-        # for each asset, there are two interesting projections of its uri:
-        # the first one is relative to the project root; we use this to form the asset name
-        # because it is guaranteed to be globally unique within a given project; in addition,
-        # it is an easily predictable name to use in configuration files
-        # the second projection is relative to the root of the library, which gets folded
-        # with {/prefix/include} and the library name to locate the installation location of
-        # headers, and joined with some special character to form the unique name of the object
-        # modules that form an archive
-
-        # for the library {root}, these are trivial: the projection relative to the workspace
-        # is its own {root}, by definition
-        relWS = self.root
-        # and the projection relative to the root is empty
-        relLib = merlin.primitives.path()
-        # use these to convert the library {root} into an asset and decorate it
-        # the name must be a string, so coerce the root projection; these operations are trivial
-        # for the library root, but they set the pattern for building all of its assets
-        top = self.folder(name=str(relWS / relLib), node=root, path=relLib)
-        # build the asset recognizer
-        classifier = self.assetClassifier()
-        # now, starting with my root
-        todo = [top]
-        # dive into the tree
-        for folder in todo:
-            # grab its contents
-            for entry, node in folder.node.contents.items():
-                # the projection of the asset relative to the library root is given by folding
-                # its name onto the projection of its folder
-                path = folder.path / entry
-                # and the name of this asset is obtained by folding this onto the library root
-                name = str(relWS / path)
-                # folders
-                if node.isFolder:
-                    # become directories
-                    asset = self.folder(name=name, node=node, path=path)
-                    # and get added to the pile of places to visit
-                    todo.append(asset)
-                # everything else is assumed to be a regular file
-                else:
-                    # so they become file based assets
-                    asset = self.file(
-                        name=name, node=node, path=path, classifier=classifier
-                    )
-                # either way, assets are attached to their container
-                folder.add(asset=asset)
-        # publish the top level folder
-        yield top
-        # all done
-        return
 
     def supports(self, requirements: typing.List[str]) -> bool:
         """
@@ -292,6 +231,81 @@ class Library(
             return super().identify(visitor=visitor, **kwds)
         # if it does, invoke it
         return handler(library=self, **kwds)
+
+    # helpers
+    def _explore(self) -> collections.abc.Iterable:
+        """
+        Generate the sequence of my source files
+        """
+        # get the workspace folder
+        ws = self.pyre_fileserver["/workspace"]
+        # starting with it
+        root = ws
+        # navigate down to my {root} carefully one step at a time because the filesystem
+        # may not have been fully explored by the time we get here
+        for folder in self.root.parts:
+            # if {root} is empty
+            if not root.contents:
+                # it probably just requires exploring; gently...
+                root.discover(levels=1)
+            # if the {folder} is already among the contents of the current directory,
+            # someone else has visited and explored this level; mark this folder as
+            # the place to explore and move on
+            root = root[folder]
+
+        # if we get his far, my {root} exists; all my sources live here
+        # so let's explore this subtree
+        root.discover()
+
+        # for each asset, there are two interesting projections of its uri:
+        # the first one is relative to the project root; we use this to form the asset name
+        # because it is guaranteed to be globally unique within a given project; in addition,
+        # it is an easily predictable name to use in configuration files
+        # the second projection is relative to the root of the library, which gets folded
+        # with {/prefix/include} and the library name to locate the installation location of
+        # headers, and joined with some special character to form the unique name of the object
+        # modules that form an archive
+
+        # for the library {root}, these are trivial: the projection relative to the workspace
+        # is its own {root}, by definition
+        relWS = self.root
+        # and the projection relative to the root is empty
+        relLib = merlin.primitives.path()
+        # use these to convert the library {root} into an asset and decorate it
+        # the name must be a string, so coerce the root projection; these operations are trivial
+        # for the library root, but they set the pattern for building all of its assets
+        top = self.folder(name=str(relWS / relLib), node=root, path=relLib)
+        # build the asset recognizer
+        classifier = self.assetClassifier()
+        # now, starting with my root
+        todo = [top]
+        # dive into the tree
+        for folder in todo:
+            # grab its contents
+            for entry, node in folder.node.contents.items():
+                # the projection of the asset relative to the library root is given by folding
+                # its name onto the projection of its folder
+                path = folder.path / entry
+                # and the name of this asset is obtained by folding this onto the library root
+                name = str(relWS / path)
+                # folders
+                if node.isFolder:
+                    # become directories
+                    asset = self.folder(name=name, node=node, path=path)
+                    # and get added to the pile of places to visit
+                    todo.append(asset)
+                # everything else is assumed to be a regular file
+                else:
+                    # so they become file based assets
+                    asset = self.file(
+                        name=name, node=node, path=path, classifier=classifier
+                    )
+                # either way, assets are attached to their container
+                folder.add(asset=asset)
+        # publish the top level folder
+        yield top
+        # all done
+        return
 
 
 # end of file
