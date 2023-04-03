f1e5e73d25d052226ed51eabdcad942dd704b727
Sebastiaan Paasen <s.m.n.vanpaasen@student.tudelft.nl>
2023-04-03 08:51:12 +0200

    cuda: added cuda tests

---
+++ .cmake/pyre_tests_cuda_lib.cmake
@@ -0,0 +1,14 @@
+# -*- cmake -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2023 all rights reserved
+
+
+#
+# pyre
+#
+
+# cuda
+pyre_test_driver_cuda(cuda.lib/managed.cc)
+pyre_test_driver_cuda(cuda.lib/pinned.cc)
+# end of file
