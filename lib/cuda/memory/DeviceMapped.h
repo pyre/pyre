c14402d4eb5d616e1e49054a593df56c7929d1cf
Sebastiaan Paasen <s.m.n.vanpaasen@student.tudelft.nl>
2023-04-06 13:31:39 +0200

    cuda memory: added mapped memory

---
+++ lib/cuda/memory/DeviceMapped.h
@@ -0,0 +1,67 @@
+// -*- c++ -*-
+
+// code guard
+#if !defined(pyre_cuda_memory_DeviceMapped_h)
+#define pyre_cuda_memory_DeviceMapped_h
+
+// a block of cells on mapped memory
+template <class T, bool isConst>
+class pyre::cuda::memory::DeviceMapped {
+    // types
+public:
+    // my cell
+    using cell_type = pyre::memory::cell_t<T, isConst>;
+    // pull the type aliases
+    using value_type = typename cell_type::value_type;
+    // derived types
+    using pointer = typename cell_type::pointer;
+    using const_pointer = typename cell_type::const_pointer;
+    using reference = typename cell_type::reference;
+    using const_reference = typename cell_type::const_reference;
+    // distances
+    using difference_type = typename cell_type::difference_type;
+    // sizes of things
+    using size_type = typename cell_type::size_type;
+    using cell_count_type = typename cell_type::cell_count_type;
+    // my handle
+    using handle_type = std::shared_ptr<value_type>;
+
+    // metamethods
+public:
+    // set the device pointer to the host pointer
+    inline DeviceMapped(pointer);
+
+    // accessors
+public:
+    // access to the raw data pointer
+    inline auto data() const -> pointer;
+    // the shared pointer
+    inline auto handle() const -> handle_type;
+
+    // iterator support
+public:
+    inline auto begin() const -> pointer;
+
+    // implementation details: data
+private:
+    handle_type _data;
+
+    // default metamethods
+public:
+    // destructor
+    ~DeviceMapped() = default;
+    // constructors
+    DeviceMapped(const DeviceMapped &) = default;
+    DeviceMapped(DeviceMapped &&) = default;
+    DeviceMapped & operator=(const DeviceMapped &) = default;
+    DeviceMapped & operator=(DeviceMapped &&) = default;
+};
+
+// get the inline definitions
+#define pyre_cuda_memory_DeviceMapped_icc
+#include "DeviceMapped.icc"
+#undef pyre_cuda_memory_DeviceMapped_icc
+
+#endif
+
+// end of file
