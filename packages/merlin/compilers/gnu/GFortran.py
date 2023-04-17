315a4619f2121f78c11301a492f19536f50fc089
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-04-17 12:28:06 -0700

    merlin.compilers: narrative improvements in the {gnu} compiler suite

--- packages/merlin/compilers/gnu/GFortran.py
+++ packages/merlin/compilers/gnu/GFortran.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis <nichael.aivazis@para-sim.com>
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2023 all rights reserved
 
 
 # framework
 import merlin
+
 # superclass
 from .GNU import GNU
 
@@ -15,7 +16,6 @@ class GFortran(GNU, family="merlin.compilers.gnu.gfortran"):
     """
     The FORTRAN compiler from the GNU compiler suite
     """
-
 
     # constants
     language = "fortran"
