c9f0a80a9ffb101c449651659cc882563cf05d3d
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-04-20 13:53:23 +0300

    merlin.assets: added visitor support to {project}

--- packages/merlin/assets/Project.py
+++ packages/merlin/assets/Project.py
@@ -23,5 +23,21 @@ class Project(
     libraries = merlin.properties.tuple(schema=merlin.protocols.library())
     libraries.doc = "the collection of project libraries"
 
+    # hooks
+    def identify(self, visitor, **kwds):
+        """
+        Ask {visitor} to process a project
+        """
+        # attempt to
+        try:
+            # ask the {visitor} for a handler for my type
+            handler = visitor.project
+        # if it doesn't exist
+        except AttributeError:
+            # chain up
+            return super().identify(visitor=visitor, **kwds)
+        # if it does, invoke it
+        return handler(project=self, **kwds)
+
 
 # end of file
