5791594e69ec4e192d9865418200a0775d6c8b49
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-04-20 22:18:53 +0300

    merlin.assets: protocol reorganization adjustments

--- packages/merlin/assets/Folder.py
+++ packages/merlin/assets/Folder.py
@@ -18,14 +18,14 @@ from .Directory import Directory
 class Folder(
     RealAsset,
     family="merlin.assets.folders.folder",
-    implements=merlin.protocols.folder,
+    implements=merlin.protocols.assets.folder,
 ):
     """
     Encapsulation of an asset container
     """
 
     # required configurable state
-    category = merlin.protocols.assetCategory()
+    category = merlin.protocols.assets.category()
     category.default = Directory
     category.doc = "a clue about the type of this asset"
 
