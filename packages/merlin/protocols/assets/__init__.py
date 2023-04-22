fe54241acd99ad59ca28ac843c234596b5b449bf
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-04-22 11:49:50 +0300

    merlin.protocols: the specifications in {assets} that require {languages} now get access from their new home

--- packages/merlin/protocols/assets/__init__.py
+++ packages/merlin/protocols/assets/__init__.py
@@ -9,13 +9,10 @@ from .Category import Category as category
 
 # and assets
 from .Asset import Asset as asset
-from .Folder import Folder as folder
 from .File import File as file
+from .Folder import Folder as folder
 from .Library import Library as library
 from .Project import Project as project
-
-#  miscellaneous parts
-from .Language import Language as language
 
 
 # end of file
