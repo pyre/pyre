c3420757eb760067341cb4941dbf9e908973c41b
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-04-26 09:48:43 +0200

    .cmake: added support for pyre benchmarks

---
+++ .cmake/pyre_benchmarks.cmake
@@ -0,0 +1,28 @@
+# -*- cmake -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2021 all rights reserved
+#
+
+
+# register a benchmark case based on a compiled driver
+function(pyre_benchmark_driver benchmarkfile)
+
+  # generate the name of the target
+  pyre_target(target ${benchmarkfile})
+
+  # schedule it to be compiled
+  add_executable(${target} ${benchmarkfile})
+  # in release mode
+  target_compile_options(${target} PRIVATE $<$<CONFIG:Release>:-O3>)
+  # with some macros
+  target_compile_definitions(${target} PRIVATE PYRE_CORE)
+  # link against my libraries
+  target_link_libraries(${target} PUBLIC pyre journal)
+
+  # all done
+endfunction()
+
+
+# end of file
