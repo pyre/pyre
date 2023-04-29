ca5cec686759e4fc55fca771eef67815160ca25e
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-04-29 18:49:32 +0200

    benchmarks: added 3x3 case to determinant benchmark

--- benchmarks/pyre.lib/tensor/matrix_determinant.cc
+++ benchmarks/pyre.lib/tensor/matrix_determinant.cc
@@ -19,7 +19,7 @@
 using timer_t = pyre::timers::process_timer_t;
 
 
-void determinant() 
+void determinant_2D() 
 {
     // make a channel
     pyre::journal::info_t channel("tests.timer.determinant");
@@ -30,7 +30,7 @@ void determinant()
     // number of times to do operation
     int N = 1<<25;
 
-    channel << "Computing " << N << " determinants" << pyre::journal::endl;
+    channel << "Computing " << N << " determinants (2x2)" << pyre::journal::endl;
 
 
     // ARRAY
@@ -92,9 +92,88 @@ void determinant()
 }
 
 
+void determinant_3D() 
+{
+    // make a channel
+    pyre::journal::info_t channel("tests.timer.determinant");
+
+    // make a timer
+    timer_t t("tests.timer");
+    
+    // number of times to do operation
+    int N = 1<<25;
+
+    channel << "Computing " << N << " determinants (3x3)" << pyre::journal::endl;
+
+
+    // ARRAY
+
+    // array tensor
+    std::array<double, 9> tensor_c {1.0, -1.0, 2.0, 1.0, 0.0, 1.0, -1.0, 2.0, -2.0};
+    double result_c = 0;
+
+    // reset timer
+    t.reset();
+    // start timer
+    t.start();
+
+    for (int n = 0; n < N; ++n) { 
+        // determinant (array)
+        result_c += tensor_c[0] * (tensor_c[4] * tensor_c[8] - tensor_c[5] * tensor_c[7]) 
+                  - tensor_c[1] * (tensor_c[3] * tensor_c[8] - tensor_c[5] * tensor_c[6])
+                  + tensor_c[2] * (tensor_c[3] * tensor_c[7] - tensor_c[4] * tensor_c[6]);
+    }
+
+    // stop the timer
+    t.stop();
+
+    // report
+    channel 
+        << "array (for loop)" << pyre::journal::newline
+        << pyre::journal::indent(1) 
+        << "result = " << result_c << pyre::journal::newline
+        << "process time = " << t.ms() << " ms " << pyre::journal::newline
+        << pyre::journal::outdent(1) << pyre::journal::endl;
+
+
+    // PYRE TENSOR
+    // tensor vector
+    pyre::tensor::matrix_t<3> tensor {1.0, -1.0, 2.0, 1.0, 0.0, 1.0, -1.0, 2.0, -2.0};
+    pyre::tensor::real result_tensor = 0.;
+
+    // reset timer
+    t.reset();
+    // start timer
+    t.start();
+
+    for (int n = 0; n < N; ++n) { 
+        // determinant (tensor)
+        result_tensor += pyre::tensor::determinant(tensor);
+    }
+
+    // stop the timer
+    t.stop();
+
+    // report
+    channel
+        << "pyre tensor" << pyre::journal::newline
+        << pyre::journal::indent(1) 
+        << "result = " << result_tensor << pyre::journal::newline
+        << "process time = " << t.ms() << " ms " << pyre::journal::newline
+        << pyre::journal::outdent(1) << pyre::journal::endl;
+
+    // all done
+    return;
+}
+
+
 int main() {
 
-    determinant();
+    // run benchmark for 2D matrix
+    determinant_2D();
+
+    // run benchmark for 3D matrix
+    determinant_3D();
 
     // all done
     return 0;
