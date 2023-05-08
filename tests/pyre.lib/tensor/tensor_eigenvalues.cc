74be71f8f9b522a439e2ac94326dcc45da9c8e98
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2023-05-08 20:21:37 +0200

    tests/pyre.lib/tensor/tensor_eigenvalues.cc: narrative improvements

--- tests/pyre.lib/tensor/tensor_eigenvalues.cc
+++ tests/pyre.lib/tensor/tensor_eigenvalues.cc
@@ -16,46 +16,75 @@ using namespace pyre::tensor;
 
 
 // main program
-int main(int argc, char* argv[]) {
+int
+main(int argc, char * argv[])
+{
+    {
+        // the packing-agnostic canonical basis of R^2x2
+        constexpr auto e_00 = matrix_t<2>::unit<0, 0>;
+        constexpr auto e_01 = matrix_t<2>::unit<0, 1>;
+        constexpr auto e_10 = matrix_t<2>::unit<1, 0>;
+        constexpr auto e_11 = matrix_t<2>::unit<1, 1>;
+        constexpr auto e_01s = symmetric(e_01 + e_10);
 
-    // 2D symmetric matrix
-    constexpr symmetric_matrix_t<2> A { 1, 2, /*2,*/ 2 };
+        // a symmetric matrix in R^2x2
+        constexpr auto A = 1.0 * e_00 + 2.0 * e_01s + 2.0 * e_11;
 
-    // the matrix eigenvalues 
-    constexpr auto lambda_A = eigenvalues(A); 
-    constexpr auto eigenvalues_A = matrix_diagonal(lambda_A);
+        // the matrix eigenvalues
+        constexpr auto lambda_A = eigenvalues(A);
+        constexpr auto eigenvalues_A = matrix_diagonal(lambda_A);
 
-    // the matrix eigenvectors
-    constexpr auto eigenvectors_A = eigenvectors(A);
+        // the matrix eigenvectors
+        constexpr auto eigenvectors_A = eigenvectors(A);
 
-    // 2D symmetric matrix: product of eigenvalues is equal to determinant
-    static_assert(lambda_A[0] * lambda_A[1] == determinant(A));
+        // product of eigenvalues is equal to determinant
+        static_assert(lambda_A[0] * lambda_A[1] == determinant(A));
 
-    // 2D symmetric matrix: sum of eigenvalues is equal to trace
-    static_assert(lambda_A[0] + lambda_A[1] == trace(A));
+        // sum of eigenvalues is equal to trace
+        static_assert(lambda_A[0] + lambda_A[1] == trace(A));
 
-    // 2D symmetric matrix: definition of eigenvalues/vectors
-    static_assert(A * col<0>(eigenvectors_A) == lambda_A[0] * col<0>(eigenvectors_A));
-    static_assert(A * col<1>(eigenvectors_A) == lambda_A[1] * col<1>(eigenvectors_A));
-    static_assert(A * eigenvectors_A == eigenvectors_A * eigenvalues_A);
+        // definition of eigenvalues/vectors pair
+        static_assert(A * col<0>(eigenvectors_A) == lambda_A[0] * col<0>(eigenvectors_A));
+        static_assert(A * col<1>(eigenvectors_A) == lambda_A[1] * col<1>(eigenvectors_A));
 
-    // 3D symmetric matrix
-    constexpr symmetric_matrix_t<3> B { 1, -1, -2, 
-                                         /*-1,*/  1,  1, 
-                                         /*-2,*/ /*1,*/ 2 };
+        // spectral decomposition
+        static_assert(A * eigenvectors_A == eigenvectors_A * eigenvalues_A);
+    }
 
-    // the matrix eigenvalues 
-    constexpr auto lambda_B = eigenvalues(B); 
-    constexpr auto eigenvalues_B = matrix_diagonal(lambda_B);
 
-    // the matrix eigenvectors
-    constexpr auto eigenvectors_B = eigenvectors(B);
+    {
+        // the packing-agnostic canonical basis of R^3x3
+        constexpr auto e_00 = matrix_t<3>::unit<0, 0>;
+        constexpr auto e_01 = matrix_t<3>::unit<0, 1>;
+        constexpr auto e_02 = matrix_t<3>::unit<0, 2>;
+        constexpr auto e_10 = matrix_t<3>::unit<1, 0>;
+        constexpr auto e_11 = matrix_t<3>::unit<1, 1>;
+        constexpr auto e_12 = matrix_t<3>::unit<1, 2>;
+        constexpr auto e_20 = matrix_t<3>::unit<2, 0>;
+        constexpr auto e_21 = matrix_t<3>::unit<2, 1>;
+        constexpr auto e_22 = matrix_t<3>::unit<2, 2>;
+        constexpr auto e_01s = symmetric(e_01 + e_10);
+        constexpr auto e_02s = symmetric(e_02 + e_20);
+        constexpr auto e_12s = symmetric(e_12 + e_21);
 
-    // 3D symmetric matrix: product of eigenvalues is equal to determinant
-    static_assert(lambda_B[0] * lambda_B[1] * lambda_B[2] == determinant(B));
+        // a symmetric matrix in R^3x3
+        constexpr auto B =
+            1.0 * e_00 - 1.0 * e_01s - 2.0 * e_02s + 1.0 * e_11 + 1.0 * e_12s + 2.0 * e_22;
 
-    // 3D symmetric matrix: sum of eigenvalues is equal to trace
-    static_assert(lambda_B[0] + lambda_B[1] + lambda_B[2] == trace(B));
+        // the matrix eigenvalues
+        constexpr auto lambda_B = eigenvalues(B);
+        constexpr auto eigenvalues_B = matrix_diagonal(lambda_B);
+
+        // the matrix eigenvectors
+        constexpr auto eigenvectors_B = eigenvectors(B);
+
+        // product of eigenvalues is equal to determinant
+        static_assert(lambda_B[0] * lambda_B[1] * lambda_B[2] == determinant(B));
+
+        // sum of eigenvalues is equal to trace
+        static_assert(lambda_B[0] + lambda_B[1] + lambda_B[2] == trace(B));
+    }
+
 
     // all done
     return 0;
