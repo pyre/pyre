09d933e9b7acf1a3f88a01ed0c48179e1c581cce
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-15 12:24:00 +0300

    pyre::journal: narrative improvements in the {manipulators}

--- lib/journal/manipulators.h
+++ lib/journal/manipulators.h
@@ -112,14 +112,14 @@ template <typename severityT, template <class> typename proxyT>
 auto
 pyre::journal::indent(Channel<severityT, proxyT> & channel) -> Channel<severityT, proxyT> &
 {
-    // ask the channel entry to mark the end of a line of output
+    // push in by one step
     return channel.indent();
 }
 
 auto
 pyre::journal::indent(dent_t level) -> Dent
 {
-    // easy enough
+    // push in by {level} steps
     return Dent(level);
 }
 
@@ -128,14 +128,14 @@ template <typename severityT, template <class> typename proxyT>
 auto
 pyre::journal::outdent(Channel<severityT, proxyT> & channel) -> Channel<severityT, proxyT> &
 {
-    // ask the channel entry to mark the end of a line of output
+    // pull out by one step
     return channel.outdent();
 }
 
 auto
 pyre::journal::outdent(dent_t level) -> Dent
 {
-    // easy enough
+    // pull out by {level} steps
     return Dent(-level);
 }
 
