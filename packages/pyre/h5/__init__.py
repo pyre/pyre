59ffe84eb1fa9000dfe5c9576fb7c97a21410de2
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-28 16:14:56 +0300

    pyre.h5: the convenience {read} and {write} functions use the new access patterns for the corresponding factories

--- packages/pyre/h5/__init__.py
+++ packages/pyre/h5/__init__.py
@@ -4,6 +4,9 @@
 # (c) 1998-2023 all rights reserved
 
 
+# support
+from .. import primitives
+
 # get the bindings
 from ..extensions import libh5
 
@@ -20,12 +23,19 @@ if libh5 is not None:
     writer = api.writer
 
     # convenience
-    def read(**kwds):
+    def read(uri: primitives.uri, mode: str = "r", **kwds):
         """
         Ask a generic reader to read a data product
         """
         # easy enough
-        return reader().read(**kwds)
+        return reader(uri=uri, mode=mode).read(**kwds)
+
+    def write(uri: primitives.uri, mode: str = "w", **kwds):
+        """
+        Ask a generic reader to read a data product
+        """
+        # easy enough
+        return writer(uri=uri, mode=mode).write(**kwds)
 
 
 # end of file
