75267aa1c110b9cfae3d1d15582b7d3e748eb2e2
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-28 16:58:29 +0300

    pyre.h5.typed: {container} types now record their {shape} and know how to build an appropriate {dataspace}

--- packages/pyre/h5/typed/Container.py
+++ packages/pyre/h5/typed/Container.py
@@ -4,6 +4,10 @@
 # (c) 1998-2023 all rights reserved
 
 
+# access the bindings
+from .. import libh5
+
+
 # the mixin for containers
 class Container:
     """
@@ -11,14 +15,24 @@ class Container:
     """
 
     # metamethods
-    def __init__(self, schema, **kwds):
+    def __init__(self, schema, shape=None, **kwds):
         # extract my {memtype} and {disktype} from my {schema}
         memtype = schema.memtype
         disktype = schema.disktype
         # chain up
         super().__init__(schema=schema, memtype=memtype, disktype=disktype, **kwds)
+        # save the shape
+        self.shape = shape
         # all done
         return
+
+    # framework hooks
+    def _pyre_dataspace(self, value):
+        """
+        Create a diskspace instance that describes scalar types
+        """
+        # easy
+        return libh5.DataSpace(shape=value.shape)
 
 
 # end of file
