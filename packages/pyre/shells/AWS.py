a6e899f5b21201c88a9d6148283b20a6151053fa
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-31 21:30:00 +0300

    pyre.shells: the {user} proxy now has access to {aws} credentials

---
+++ packages/pyre/shells/AWS.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2023 all rights reserved
+#
+
+
+# externals
+import os
+import pyre
+import collections
+import configparser
+
+
+# declaration
+class AWS(pyre.component):
+    """
+    Access to the user's AWS credentials
+    """
+
+    # interface
+    def profile(self, name: str = "default"):
+        """
+        Access the contents of the named profile
+        """
+        # look up the profile
+        profile = self._profiles[name]
+        # and hand it off
+        return profile
+
+    # meta-methods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # the cache for aws credentials
+        self._profiles = self._loadCredentials()
+        # all done
+        return
+
+    # implementation details
+    def _loadCredentials(self) -> dict:
+        """
+        Load the AWS credentials
+        """
+        # prime
+        credentials = collections.defaultdict(dict)
+        # locate the file with the shared credentials
+        path = pyre.primitives.path(
+            os.environ.get("AWS_SHARED_CREDENTIALS_FILE", "~/.aws/credentials")
+        )
+        # if it's there
+        if path.exists():
+            # make an {ini} parser
+            cfg = configparser.ConfigParser()
+            # and parse the file
+            cfg.read(path)
+            # go through the sections
+            for section, keys in cfg.items():
+                # prime the profile
+                profile = {}
+                # go through the keys
+                for key in keys:
+                    # extract the value
+                    value = cfg.get(section=section, option=key)
+                    # and set it
+                    profile[key.lower()] = value
+                # attach the profile to the pile
+                credentials[section.lower()] = profile
+        # check whether there an access key in the user environment
+        key = os.environ.get("AWS_ACCESS_KEY_ID", None)
+        # if it's there
+        if key is not None:
+            # override the value in the default profile
+            credentials["default"]["aws_access_key_id"] = key
+        # check whether there is a secret key in the environment
+        key = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
+        # if there
+        if key is not None:
+            # override the value in the default profile
+            credentials["default"]["aws_secret_access_key"] = key
+
+        # NYI: there may be profile configurations in ~/.aws.config
+
+        # all done
+        return credentials
+
+
+# end of file
--- packages/pyre/shells/User.py
+++ packages/pyre/shells/User.py
@@ -10,6 +10,9 @@
 import os
 import pyre
 
+# my parts
+from .AWS import AWS
+
 
 # declaration
 class User(pyre.component):
@@ -17,33 +20,60 @@ class User(pyre.component):
     Encapsulation of user specific information
     """
 
-
     # configurable state
     # administrative
     name = pyre.properties.str()
-    name.doc = 'the full name of the user'
+    name.doc = "the full name of the user"
 
     username = pyre.properties.str()
-    username.default = os.environ.get('LOGNAME')
-    username.doc =  "the username"
+    username.default = os.environ.get("LOGNAME")
+    username.doc = "the username"
 
     uid = pyre.properties.str()
     uid.default = os.getuid()
     uid.doc = "the user's system id"
 
     home = pyre.properties.path()
-    home.default = os.environ.get('HOME')
+    home.default = os.environ.get("HOME")
     home.doc = "the location of the user's home directory"
 
     email = pyre.properties.str()
-    email.doc = 'the email address of the user'
+    email.doc = "the email address of the user"
 
     affiliation = pyre.properties.str()
-    affiliation.doc = 'the affiliation of the user'
+    affiliation.doc = "the affiliation of the user"
 
     # choices and defaults
     externals = pyre.externals.dependencies()
-    externals.doc = 'the database of preferred instances for each external package category'
+    externals.doc = (
+        "the database of preferred instances for each external package category"
+    )
+
+    # on-demand state
+    @property
+    def aws(self):
+        """
+        AWS credentials
+        """
+        # get the cached credentials
+        aws = self._aws
+        # if the cache is empty
+        if aws is None:
+            # load the credentials
+            aws = AWS()
+            # cache them
+            self._aws = aws
+        # and hand them off
+        return aws
+
+    # meta-methods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # the cache for aws credentials
+        self._aws = None
+        # all done
+        return
 
 
 # end of file
