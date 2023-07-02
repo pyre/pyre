2caa635a155e1a8d0ea851395ea7aebbe44aa491
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-07-02 17:06:28 +0300

    cmake: updated the file list for the {h5} bindings

--- .cmake/pyre_h5.cmake
+++ .cmake/pyre_h5.cmake
@@ -24,12 +24,19 @@ function(pyre_h5Module)
     target_sources(h5module PRIVATE
       extensions/h5/__init__.cc
       extensions/h5/api.cc
+      extensions/h5/DAPL.cc
       extensions/h5/DataSet.cc
       extensions/h5/DataSpace.cc
+      extensions/h5/DCPL.cc
+      extensions/h5/DXPL.cc
       extensions/h5/enums.cc
+      extensions/h5/FAPL.cc
+      extensions/h5/FCPL.cc
       extensions/h5/File.cc
-      extensions/h5/FileAccessPropertyList.cc
       extensions/h5/Group.cc
+      extensions/h5/LAPL.cc
+      extensions/h5/LCPL.cc
+      extensions/h5/PropList.cc
       extensions/h5/datatypes/__init__.cc
       extensions/h5/datatypes/ArrayType.cc
       extensions/h5/datatypes/AtomType.cc
