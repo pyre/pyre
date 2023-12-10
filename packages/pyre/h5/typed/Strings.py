c379ca4c645a690024d6e31c987f56e6b6fb49a9
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-12-09 17:58:55 -0800

    pyre.h5: tiny readability improvement in the way {strings} compute the dataset element width

--- packages/pyre/h5/typed/Strings.py
+++ packages/pyre/h5/typed/Strings.py
@@ -84,8 +84,8 @@ class Strings(Dataset.list):
         """
         # get the dataset value; it should be a list of strings
         value = dataset.value
-        # the type width is the max of the lengths
-        width = max([1] + list(map(len, value)))
+        # the type width is the max of the lengths of the strings in the list
+        width = max(1, max(len(item) for item in value))
         # the space is determined by the number of strings
         shape = [len(value)]
         # build the type
