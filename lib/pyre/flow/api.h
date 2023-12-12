d4e702a61d5c6136986beea541a47031cfc2251d
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-12-12 13:13:22 -0800

    pyre::flow: published weak pointers to the node classes

--- lib/pyre/flow/api.h
+++ lib/pyre/flow/api.h
@@ -14,10 +14,15 @@ namespace pyre::flow {
     using factory_t = Factory;
     using product_t = Product;
 
-    // pointers
+    // shared pointers
     using node_ref_t = std::shared_ptr<Node>;
     using factory_ref_t = std::shared_ptr<Factory>;
     using product_ref_t = std::shared_ptr<Product>;
+
+    // weak pointers
+    using node_weakref_t = std::weak_ptr<Node>;
+    using factory_weakref_t = std::weak_ptr<Factory>;
+    using product_weakref_t = std::weak_ptr<Product>;
 
 } // namespace pyre::flow
 
