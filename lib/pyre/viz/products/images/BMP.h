aca5878d1c8997dca10c754e907f9f4964e265f4
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-12-22 08:10:23 +0200

    pyre::viz: fleshed out {products::images::bmp_t}

--- lib/pyre/viz/products/images/BMP.h
+++ lib/pyre/viz/products/images/BMP.h
@@ -10,7 +10,7 @@ class pyre::viz::products::images::BMP : public pyre::flow::product_t {
     // type aliases
 public:
     // my shape
-    using shape_type = std::tuple<int, int>;
+    using shape_type = pyre::grid::shape_t<2, int>;
     // my cell type
     using cell_type = byte_t;
     // access to my data buffer
@@ -21,37 +21,48 @@ public:
 
     // factory
 public:
-    inline static auto create(shape_type = shape_type { 512, 512 }) -> ref_type;
+    inline static auto create(shape_type shape = shape_type { 512, 512 }, bool topdown = true)
+        -> ref_type;
 
     // metamethods
 public:
     // destructor
     virtual ~BMP();
     // constructor: DON'T CALL
-    inline BMP(sentinel_type, shape_type);
+    inline BMP(sentinel_type, shape_type, bool);
 
     // accessors
 public:
     inline auto shape() const -> shape_type;
-
-    // mutators
-public:
-    inline auto shape(shape_type) -> ref_type;
+    inline auto padding() const -> int;
 
     // interface
 public:
     // access to my data
     inline auto read() -> constview_type;
     inline auto write() -> view_type;
-    // invalidate me
-    virtual auto flush() -> void override;
     // debugging support
     auto dump() -> ref_type;
 
-    // implementation details - data
-protected:
+    // implementation details
+private:
+    // data
+    // the bitmap shape
     shape_type _shape;
+    // file layout
+    int _topdown;
+    int _padBytesPerLine;
+    int _payloadSz;
+    int _bitmapSz;
+    // the data buffer
     cell_type * _data;
+
+    // constants known at compile time
+    static constexpr int _fileHeaderSz = 14;
+    static constexpr int _infoHeaderSz = 40;
+    static constexpr int _payloadOffset = _fileHeaderSz + _infoHeaderSz;
+    static constexpr int _pixelSz = 3 * sizeof(cell_type);
+    static constexpr int _bitsPerPixel = 8 * _pixelSz;
 
     // metamethods
 private:
