f11be27f50481530b8c21d9d0f28a6fa64adc569
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-12-26 05:22:29 +0200

    pyre::viz: added {products::memory::tile_t}, templated on the packing and storage strategies of a {pyre::grid::grid_t}

---
+++ lib/pyre/viz/products/memory/Tile.h
@@ -0,0 +1,69 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+// code guard
+#pragma once
+
+template <class packingT, class storageT>
+class pyre::viz::products::memory::Tile : public pyre::flow::product_t {
+    // type aliases
+public:
+    // my template parameters
+    using packing_type = packingT;
+    using storage_type = storageT;
+    // my cell type
+    using cell_type = typename storage_type::value_type;
+    // shape
+    using shape_type = typename packing_type::shape_type;
+    // memory
+    using grid_type = pyre::grid::grid_t<packing_type, storage_type>;
+
+    // shared pointers to my instances
+    using ref_type = std::shared_ptr<Tile>;
+
+    // factory
+public:
+    inline static auto create(const name_type & name, shape_type shape, cell_type value = 0)
+        -> ref_type;
+
+    // metamethods
+public:
+    // destructor
+    inline virtual ~Tile();
+    // constructor; DON'T CALL
+    inline Tile(sentinel_type, const name_type &, shape_type, cell_type);
+
+    // accessors
+public:
+    inline auto shape() const -> shape_type;
+
+    // mutators
+public:
+    inline auto value(cell_type) -> void;
+
+    // interface
+public:
+    // value access by factories
+    inline auto read() -> const grid_type &;
+    inline auto write() -> grid_type &;
+
+    // implementation details - data
+private:
+    // the data buffer
+    grid_type _data;
+
+    // metamethods
+private:
+    // suppressed constructors
+    Tile(const Tile &) = delete;
+    Tile & operator=(const Tile &) = delete;
+    Tile(Tile &&) = delete;
+    Tile & operator=(Tile &&) = delete;
+};
+
+// get the inline definitions
+#include "Tile.icc"
+
+// end of file
