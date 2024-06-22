b708e8434f06a8fd1aed0c2f9975bec9df375af7
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-06-22 16:45:47 +0300

    pyre::memory: added type aliases for the basic types

--- lib/pyre/memory/externals.h
+++ lib/pyre/memory/externals.h
@@ -15,6 +15,8 @@
 #include <utility>
 
 // low level stuff
+#include <complex>    // for {complex}
+#include <cstdint>    // for type aliases for the basic types
 #include <cstring>    // for {strerror}
 #include <fcntl.h>    // for {open}
 #include <unistd.h>   // for {close}
@@ -40,6 +42,24 @@ namespace pyre::memory {
     using uri_t = std::string;
     // file information
     using info_t = struct stat;
+
+    // the basic types
+    // unsigned
+    using uint8_t = std::uint8_t;
+    using uint16_t = std::uint16_t;
+    using uint32_t = std::uint32_t;
+    using uint64_t = std::uint64_t;
+    // signed
+    using int8_t = std::int8_t;
+    using int16_t = std::int16_t;
+    using int32_t = std::int32_t;
+    using int64_t = std::int64_t;
+    // floating point
+    using float32_t = float;
+    using float64_t = double;
+    // complex
+    using complex64_t = std::complex<float>;
+    using complex128_t = std::complex<double>;
 } // namespace pyre::memory
 
 
