e7db2c462b3da6e21f75bf1f98d8e2e105ee3e87
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-10-26 16:43:56 +0300

    pyre.viz: primed the data {tiles}

---
+++ packages/pyre/viz/tiles/Tile.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# support
+import pyre
+
+
+# the protocol for data tiles
+class Tile(pyre.flow.specification, family="pyre.viz.tiles"):
+    """
+    The protocol for typed data tiles with a specific storage strategy
+    """
+
+
+# end of file
---
+++ packages/pyre/viz/tiles/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# the protocol
+from .Tile import Tile as tile
+
+
+# end of file
