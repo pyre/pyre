1eb9bd13f643e308baa050536116296483c02134
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2024-11-30 08:53:17 +0100

    tensor: implemented move and copy constructors and assignment operators explicitly
    The {default} implementation was buggy as it was dealing with the underlying tensor data irrespectively of the different tensor packings.

--- lib/pyre/tensor/Tensor.h
+++ lib/pyre/tensor/Tensor.h
@@ -77,17 +77,25 @@ namespace pyre::tensor {
         template <class T2>
         constexpr Tensor(T2 (&&)[S]);
 
-        // copy constructor
-        constexpr Tensor(const Tensor &) = default;
+        // copy constructor from a tensor with (potentially) different packing
+        template <tensor_c tensorT>
+        constexpr Tensor(const tensorT & rhs)
+            requires(std::is_same_v<typename sum<tensor_t, tensorT>::type, tensor_t>);
 
-        // move constructor
-        constexpr Tensor(Tensor &&) noexcept = default;
+        // move constructor from a tensor with exact same packing
+        template <tensor_c tensorT>
+        constexpr Tensor(tensorT &&)
+            requires(std::is_same_v<tensor_t, tensorT>);
 
-        // copy assignment operator
-        constexpr Tensor & operator=(const Tensor &) = default;
+        // copy assignment operator from a tensor with (potentially) different packing
+        template <tensor_c tensorT>
+        constexpr Tensor & operator=(const tensorT & rhs)
+            requires(std::is_same_v<typename sum<tensor_t, tensorT>::type, tensor_t>);
 
-        // move assignment operator
-        constexpr Tensor & operator=(Tensor &&) noexcept = default;
+        // move assignment operator from a tensor with exact same packing
+        template <tensor_c tensorT>
+        constexpr Tensor & operator=(tensorT &&)
+            requires(std::is_same_v<tensor_t, tensorT>);
 
         // destructor
         constexpr ~Tensor();
