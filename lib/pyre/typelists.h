cffcfd3bc3d9d8c588ec1640c002c1fcea552ff3
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-01-28 23:11:54 +0200

    pyre::typelists: added convenience header at top level

---
+++ lib/pyre/typelists.h
@@ -0,0 +1,12 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+// code guard
+#pragma once
+
+#include "typelists/public.h"
+
+
+// end of file
