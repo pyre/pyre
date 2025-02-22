45c0db202027be9b8440e1312d93177da6cdab4f
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-22 13:08:36 +0200

    pyre::memory: restructured the package headers and template expansions

--- lib/pyre/memory/expansions.h
+++ lib/pyre/memory/expansions.h
@@ -4,40 +4,51 @@
 // (c) 1998-2025 all rights reserved
 
 // code guard
-#if !defined(pyre_memory_expansions_h)
-#define pyre_memory_expansions_h
+#pragma once
+
+// external
+#include "externals.h"
+// forward declaration
+#include "forward.h"
+// the api
+#include "api.h"
 
 
-// template expansion machinery
+// low level entities; you should probably stay away from them
 namespace pyre::memory {
-    // a compile-time container with type choices
-    template <typename... typeT>
-    struct Types;
+    // access rights
+    using const_t = std::integer_sequence<bool, true, false>;
+    // cell types
+    using basetypes_t = pyre::typelists::types_t<
+        // signed integers
+        int8_t, int16_t, int32_t, int64_t,
+        // unsigned integers
+        uint8_t, uint16_t, uint32_t, uint64_t,
+        // floating point
+        float32_t, float64_t,
+        // complex
+        complex64_t, complex128_t>;
 
-    // the container with the strategy choices
-    template <template <typename typeT> class... strategyT>
-    struct StorageStrategies;
+    // the cell expander
+    template <typename... baseT>
+    struct celltypes_t<pyre::typelists::types_t<baseT...>> {
+        using type = typename pyre::typelists::concat_t<
+            pyre::typelists::types_t<cell_t<baseT, false>...>,
+            pyre::typelists::types_t<cell_t<baseT, true>...>>::type;
+    };
 
-    // type list concatenation
-    template <typename... listsT>
-    struct Concat;
+    // the pile of cell types
+    using cells_t = typename celltypes_t<basetypes_t>::type;
 
-    // compose a storage strategy with a set of types
-    template <template <typename> class strategyT, typename... cellsT>
-    struct ComposeStorageStrategy;
+    // storage strategies by category
+    using heaps_t = pyre::typelists::templates_t<constheap_t, heap_t>;
+    using maps_t = pyre::typelists::templates_t<constmap_t, map_t>;
+    using views_t = pyre::typelists::templates_t<constview_t, view_t>;
 
-    // a helper that expands a set of strategies and a set of cells
-    template <typename strategiesT, typename cellsT>
-    struct ExpandStorageStrategies;
+    // all storage strategies
+    using storageStrategies =
+        pyre::typelists::templates_t<constheap_t, constmap_t, constview_t, heap_t, map_t, view_t>;
 } // namespace pyre::memory
 
-
-// get the inline definitions
-#define pyre_memory_expansions_icc
-#include "expansions.icc"
-#undef pyre_memory_expansions_icc
-
-
-#endif
 
 // end of file
