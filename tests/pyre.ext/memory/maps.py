2d0b14386599514e316c2420288f5a4e523d9941
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-23 16:55:53 +0200

    pyre::memory: added sanity checks to ensure that all storage strategy expansions have bindings

---
+++ tests/pyre.ext/memory/maps.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import itertools
+
+
+# the driver
+def maps():
+    """
+    Verify that the map expansions are all present
+    """
+    # get the module
+    from pyre.extensions.pyre.memory import maps
+
+    # given access rights
+    permissions = ["", "Const"]
+    # and basic types
+    types = [
+        # ints
+        "Int8",
+        "Int16",
+        "Int32",
+        "Int64",
+        "UInt8",
+        "UInt16",
+        "UInt32",
+        "UInt64",
+        # floats
+        "Float",
+        "Double",
+        # complex
+        "ComplexFloat",
+        "ComplexDouble",
+    ]
+    # build all possible names
+    names = [
+        f"{permission}Map{type}"
+        for permission, type in itertools.product(permissions, types)
+    ]
+    # go through them
+    for name in names:
+        # and verify they are all present
+        assert getattr(maps, name)
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    maps()
+
+
+# end of file
