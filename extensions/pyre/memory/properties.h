2fc1ada7283dbba891a4733b2b51902b42725bab
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-23 16:03:52 +0200

    pyre::memory: refactored the decorators that add interface to the storage strategy bindings

---
+++ extensions/pyre/memory/properties.h
@@ -0,0 +1,25 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+// code guard
+#pragma once
+
+
+// externals
+#include "external.h"
+
+
+// decorator that adds properties common to all storage strategies
+namespace pyre::py::memory {
+    template <class T>
+    auto properties(shared_holder_t<T> &) -> void;
+}
+
+
+// implementations
+#include "properties.icc"
+
+
+// end of file
