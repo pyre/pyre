15c2f3e2245b6d14787abfde3f013969c1a3ca5a
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-23 16:44:42 +0200

    pyre::memory: added a test driver to verify the {cell_t} expansions

---
+++ tests/pyre.ext/memory/cells.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import itertools
+
+
+# the drivers
+def intCells() -> None:
+    """
+    Sanity test: make sure the bindings for integral type cells
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # integral types
+    signs = ["", "U"]
+    sizes = [8, 16, 32, 64]
+
+    # build the integral type names
+    for perm, sign, size in itertools.product(const, signs, sizes):
+        # assemble the name
+        name = f"{perm}{sign}Int{size}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name
+        assert name == cell.name
+        # check the size, in bits
+        assert size == cell.bits()
+        # check the size, in bytes
+        assert size == 8 * cell.bytes()
+        # check the access rights
+        assert perm == ("Const" if cell.readonly else "")
+        # both ways
+        assert perm == ("" if cell.writable else "Const")
+
+    # all done
+    return
+
+
+def floatCells() -> None:
+    """
+    Sanity test: make sure the bindings for float type cells
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # float types
+    sizes = [("Float", 32), ("Double", 64)]
+
+    # build the integral type names
+    for perm, (marker, size) in itertools.product(const, sizes):
+        # assemble the name
+        name = f"{perm}{marker}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name
+        assert name == cell.name
+        # check the size, in bits
+        assert size == cell.bits()
+        # check the size, in bytes
+        assert size == 8 * cell.bytes()
+        # check the access rights
+        assert perm == ("Const" if cell.readonly else "")
+        # both ways
+        assert perm == ("" if cell.writable else "Const")
+
+    # all done
+    return
+
+
+def complexCells() -> None:
+    """
+    Sanity test: make sure the bindings for complex type cells
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # float types
+    sizes = [("ComplexFloat", 64), ("ComplexDouble", 128)]
+
+    # build the integral type names
+    for perm, (marker, size) in itertools.product(const, sizes):
+        # assemble the name
+        name = f"{perm}{marker}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name
+        assert name == cell.name
+        # check the size, in bits
+        assert size == cell.bits()
+        # check the size, in bytes
+        assert size == 8 * cell.bytes()
+        # check the access rights
+        assert perm == ("Const" if cell.readonly else "")
+        # both ways
+        assert perm == ("" if cell.writable else "Const")
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the tests
+    intCells()
+    floatCells()
+    complexCells()
+
+
+# end of file
