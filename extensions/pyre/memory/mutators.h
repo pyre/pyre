0f69d792d39c3cadb9768edd4fdd80ef89a8a465
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-23 16:03:52 +0200

    pyre::memory: refactored the decorators that add interface to the storage strategy bindings

---
+++ extensions/pyre/memory/mutators.h
@@ -0,0 +1,25 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+// code guard
+#pragma once
+
+
+// externals
+#include "external.h"
+
+
+// decorator that adds interface common to all storage strategies
+namespace pyre::py::memory {
+    template <class T>
+    auto mutators(shared_holder_t<T> &) -> void;
+}
+
+
+// implementations
+#include "mutators.icc"
+
+
+// end of file
