0119a088d6f2acfc19d0d362c7dfa89c58615ba9
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-02-23 16:04:51 +0200

    pyre::memory: cleaned up the decorator that adds support for the python {buffer_protocol}

--- extensions/pyre/memory/buffer_protocol.h
+++ extensions/pyre/memory/buffer_protocol.h
@@ -4,24 +4,23 @@
 // (c) 1998-2025 all rights reserved
 
 // code guard
-#if !defined(pyre_py_memory_buffer_protocol_h)
-#define pyre_py_memory_buffer_protocol_h
+#pragma once
+
+
+// externals
+#include "external.h"
 
 
 // the {pyre} extension namespace
 namespace pyre::py::memory {
     // add support for the python buffer protocol
-    template <class memT>
-    void bindBufferProtocol(shared_holder_t<memT> &);
-
+    template <class T>
+    void bufferProtocol(shared_holder_t<T> &);
 } // namespace pyre::py::memory
 
 
 // get the implementation
-#define pyre_py_memory_buffer_protocol_icc
 #include "buffer_protocol.icc"
-#undef pyre_py_memory_buffer_protocol_icc
 
-#endif
 
 // end of file
