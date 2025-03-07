7f18bc88dd99ac68d8c3f532dfac400be8d30567
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-07 20:34:33 +0200

    pyre::memory added bindings for the buffer {interface}

---
+++ extensions/pyre/memory/interface.h
@@ -0,0 +1,28 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+// code guard
+#pragma once
+
+
+// externals
+#include "external.h"
+// local declarations
+#include "__init__.h"
+
+
+// decorator that adds interface common to all storage strategies
+namespace pyre::py::memory {
+    // read-only interface
+    template <class T>
+    auto interface(pymem_t<T> &) -> void;
+} // namespace pyre::py::memory
+
+
+// implementations
+#include "interface.icc"
+
+
+// end of file
