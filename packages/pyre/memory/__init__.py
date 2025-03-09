7b45dd50ee0ac31d5080a30a389a9543a1cf95a8
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-09 10:54:54 +0200

    pyre.memory: added a {heap} factory at top level

--- packages/pyre/memory/__init__.py
+++ packages/pyre/memory/__init__.py
@@ -5,8 +5,26 @@
 # (c) 1998-2025 all rights reserved
 
 
-# publish the cell types
+# support
+import pyre
+
+# publish
 from . import cells
+
+
+# convenience
+def heap(cells: int, type: cells.cell):
+    """
+    Allocate a buffer of the given number of {cells} of the given {type}
+    """
+    # get the module with the heap factories
+    module = pyre.libpyre.memory.heaps
+    # look up the factory
+    factory = getattr(module, f"Heap{type.__class__.__name__}")
+    # allocate
+    heap = factory(cells=cells)
+    # and return it
+    return heap
 
 
 # end of file
