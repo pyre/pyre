bed4e904582ccb8026dbf86b01d3e4835492d0d8
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-11 15:41:52 +0200

    pyre::typelists: removed the {tuple} test code from the {list} driver

--- tests/pyre.lib/typelists/list.cc
+++ tests/pyre.lib/typelists/list.cc
@@ -4,21 +4,8 @@
 // (c) 1998-2025 all rights reserved
 
 
-// STL
-#include <utility>
-#include <tuple>
-
 // declarations
 #include <pyre/typelists/list.h>
-
-// a tuple maker
-template <typename...>
-struct tuple_t;
-// from {types_t}
-template <typename... T>
-struct tuple_t<pyre::typelists::types_t<T...>> {
-    using type = std::tuple<T...>;
-};
 
 
 // driver
@@ -34,15 +21,6 @@ main()
             // check
             ints_t, pyre::typelists::types_t<int, int, int>>,
         "mismatch in ints_t");
-
-    // place the {ints_t} in a tuple
-    using my_t = typename tuple_t<ints_t>::type;
-    // instantiate
-    constexpr auto t = my_t { 0, 1, 2 };
-    // check
-    static_assert(std::get<0>(t) == 0);
-    static_assert(std::get<1>(t) == 1);
-    static_assert(std::get<2>(t) == 2);
 
     // all done
     return 0;
