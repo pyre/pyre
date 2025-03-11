a560fa9ef4e9220021ad00b8b763f20e67887215
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-11 15:29:48 +0200

    pyre::typelists: added {list_t}, a {types_t} generator with {N} copies of a type {T}

--- lib/pyre/typelists/types.h
+++ lib/pyre/typelists/types.h
@@ -15,5 +15,19 @@
 template <typename...>
 struct pyre::typelists::types_t {};
 
+// type declarator
+template <typename T, int>
+struct pyre::typelists::type_t {
+    // declare the type
+    using type = T;
+};
+
+// a typelist with {N} copies of the given type {T}
+template <typename T, int N, int... I>
+struct pyre::typelists::list_t<T, N, std::index_sequence<I...>> {
+    // put {N} copies of {T} in a typelists
+    using type = types_t<typename type_t<T, I>::type...>;
+};
+
 
 // end of file
