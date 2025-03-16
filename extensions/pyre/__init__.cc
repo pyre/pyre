36e2c3e5c067b20a4fad60fdf826f39ae9629f9c
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-16 12:52:46 +0200

    pyre::grid: the temporary {_grid_} module contains explorations of the automatic template expansions
    (cherry picked from commit 3263cbf39faa2e1687f17c8c869f826977b0b8cf)

--- extensions/pyre/__init__.cc
+++ extensions/pyre/__init__.cc
@@ -15,6 +15,7 @@
 // subpackages
 #include "memory/__init__.h"
 #include "grid/__init__.h"
+#include "_grid_/__init__.h"
 #include "timers/__init__.h"
 #include "viz/__init__.h"
 #include "chroma/__init__.h"
@@ -32,6 +33,7 @@ PYBIND11_MODULE(pyre, m)
     pyre::py::memory::__init__(m);
     // grid
     pyre::py::grid::__init__(m);
+    pyre::py::_grid_::__init__(m);
     // timers
     pyre::py::timers::__init__(m);
     // viz
