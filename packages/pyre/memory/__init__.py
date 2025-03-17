727e1216bfbf6afa272e85c9bf4ab1d679cbe856
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-03-17 17:57:43 +0200

    pyre.memory: added factories for {map} based storage buffers and the corresponding grids

--- packages/pyre/memory/__init__.py
+++ packages/pyre/memory/__init__.py
@@ -12,19 +12,82 @@ import pyre
 from . import cells
 
 
-# convenience
-def heap(cells: int, type: cells.cell):
+# memory buffers
+# on the heap
+def heap(type: cells.cell, cells: int):
     """
     Allocate a buffer of the given number of {cells} of the given {type}
     """
     # get the module with the heap factories
     module = pyre.libpyre.memory.heaps
     # look up the factory
-    factory = getattr(module, f"Heap{type.__class__.__name__}")
+    heapFactory = getattr(module, f"Heap{type.__class__.__name__}")
     # allocate
-    heap = factory(cells=cells)
+    heap = heapFactory(cells=cells)
     # and return it
     return heap
+
+
+# file backed
+def map(uri: pyre.primitives.path, type: cells.cell, cells: int = 0):
+    """
+    Allocate a file backed buffer
+    """
+    # get the module with the map factories
+    module = pyre.libpyre.memory.maps
+    # look up the factory
+    mapFactory = getattr(module, f"Map{type.__class__.__name__}")
+    # if the {type} is mutable and i know how many {cells} to allocate
+    if type.mutable and cells > 0:
+        # allocate and return
+        return mapFactory(uri=str(uri), cells=cells)
+    # if the {type} is immutable:
+    if not type.mutable:
+        # allocate and return
+        return mapFactory(uri=str(uri))
+    # anything else is an error
+    import journal
+
+    # make a channel
+    channel = journal.error("pyre.memory.map")
+    # complain
+    channel.line("unsupported request for a memory mapped buffer")
+    # and flush
+    channel.log()
+
+    # just in case errors aren't fatal
+    return
+
+
+# grids
+def mapgrid(uri, shape, type):
+    """
+    Allocate a file backed grid of the given {shape} and cell {type} at the given {uri}
+    """
+    # get the grid factories
+    grids = pyre.libpyre.grid
+    # deduce the rank of the grid
+    rank = len(shape)
+    # get the shape factory
+    shapeFactory = getattr(grids, f"Shape{rank}D")
+    # instantiate the shape descriptor
+    shape = shapeFactory(shape=shape)
+    # ask it for the total number of cells
+    cells = shape.cells
+
+    # allocate the file backed buffer
+    storage = map(uri=uri, type=type, cells=cells)
+    # get the packing factory
+    packingFactory = getattr(grids, f"Canonical{rank}D")
+    # convert the shape into a packing
+    packing = packingFactory(shape=shape)
+
+    # get the grid factory
+    gridFactory = getattr(grids, f"{type.__class__.__name__}MapGrid{rank}D")
+    # put it all together
+    grid = gridFactory(packing=packing, storage=storage)
+    # and return the grid
+    return grid
 
 
 # end of file
