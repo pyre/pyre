fa1a478fa62e624291dbaf313e0270a4f2b5271f
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-09-03 18:38:31 +0300

    pyre::_grid_: primed the {grid} replacement

---
+++ lib/pyre/_grid_/public.h
@@ -0,0 +1,27 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+// code guard
+#pragma once
+
+
+// external packages
+#include "externals.h"
+// get the forward declarations
+#include "forward.h"
+
+// published type aliases; this is the file you are looking for...
+#include "api.h"
+
+// implementation
+// packing order
+#include "Order.h"
+// shape
+#include "Shape.h"
+// the grid
+#include "Grid.h"
+
+
+// end of file
