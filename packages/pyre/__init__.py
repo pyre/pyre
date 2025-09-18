bf6ce6eedfcee3e3f0869be0d295bfebd5bbd6ea
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-09-18 14:55:34 +0300

    pyre: updated the front matter in the top level package

--- packages/pyre/__init__.py
+++ packages/pyre/__init__.py
@@ -1,10 +1,8 @@
 # -*- Python -*-
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2025 all rights reserved
-#
 
 
 """
