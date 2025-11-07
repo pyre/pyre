12ca1aa960d5227f0d993829e738cbfd695e2897
Bianca Giovanardi <bianca.giovanardi@gmail.com>
2025-11-07 18:09:16 +0100

    tensor: define in {tensor} namespace type aliases for packings that set to {int} the index type
    For tensors, we use `int` indices (we typically do not work in higher dimensions than 3D or 4D).

--- lib/pyre/tensor/algebra.h
+++ lib/pyre/tensor/algebra.h
@@ -171,12 +171,12 @@ namespace pyre::tensor {
         -> dyadic_product<vectorT1, vectorT2>::type;
 
     // builds a square matrix with all zeros except the K-th row is equal to v
-    template <int K, class packingT = pyre::grid::canonical_t<2>, vector_c vectorT>
+    template <int K, class packingT = canonical_packing_t<2>, vector_c vectorT>
     constexpr auto matrix_row(const vectorT & v)
         -> matrix_t<vectorT::size, vectorT::size, typename vectorT::scalar_type, packingT>;
 
     // builds a square matrix with all zeros except the K-th column is equal to v
-    template <int K, class packingT = pyre::grid::canonical_t<2>, vector_c vectorT>
+    template <int K, class packingT = canonical_packing_t<2>, vector_c vectorT>
     constexpr auto matrix_column(const vectorT & v)
         -> matrix_t<vectorT::size, vectorT::size, typename vectorT::scalar_type, packingT>;
 
@@ -191,7 +191,7 @@ namespace pyre::tensor {
         -> vector_t<matrixT::dims[0], typename matrixT::scalar_type>;
 
     // the skew symmetric matrix representing vector {a}
-    template <vector_c vectorT, class packingT = pyre::grid::canonical_t<2>>
+    template <vector_c vectorT, class packingT = canonical_packing_t<2>>
     constexpr auto skew(const vectorT & a)
         -> square_matrix_t<3, typename vectorT::scalar_type, packingT>
         requires(vectorT::size == 3);
