5c7056976f808a3cd8897fece52469000e3786b3
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-11-16 15:11:36 -0800

    pyre.components: reimplemented {foundry} in terms of the new {decorator}

--- packages/pyre/components/Foundry.py
+++ packages/pyre/components/Foundry.py
@@ -4,9 +4,7 @@
 # (c) 1998-2025 all rights reserved
 
 
-# externals
-import collections
-# framework
+# support
 import pyre
 
 
@@ -16,23 +14,20 @@ class Foundry(pyre.patterns.decorator):
     A decorator for callables that return component classes
     """
 
-
     # public data
-    pyre_tip = '' # a short description of what this component does
-    pyre_implements = None # the protocols implemented by this component
-
+    pyre_tip = ""  # a short description of what this component does
+    pyre_implements = None  # the protocols implemented by this component
 
     # meta-methods
-    def __init__(self, factory, implements=None, tip='', **kwds):
+    def __init__(self, method, implements=None, tip="", **kwds):
         # chain up
-        super().__init__(factory=factory, **kwds)
+        super().__init__(method=method, **kwds)
         # save the tip
         self.pyre_tip = tip
         # and the sequence of implemented protocols
         self.pyre_implements = pyre.patterns.sequify(implements)
         # all done
         return
-
 
     def __call__(self, *args, **kwds):
         # invoke the factory to get the component class
