fd54310a448e7359969fb7f4d76390c57e41ca2c
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-11-16 15:10:26 -0800

    pyre.patterns: reimplemented the {decorator} so it can stack properly

--- packages/pyre/patterns/Decorator.py
+++ packages/pyre/patterns/Decorator.py
@@ -4,74 +4,74 @@
 # (c) 1998-2025 all rights reserved
 
 
+# support
+import functools
+import types
+
+
 # the base class for pyre decorators
 class Decorator:
     """
-    A decorator that can be invoked with keyword arguments to collect metadata
-
-    This class assumes it is at the base of any hierarchy that involves it. This is a departure
-    from {pyre} norms, but it unavoidable in this case because of the double instantiation
-    pattern in {__new__}
-
-    Note that the signature of {__init__} is required to have {factory} as the first positional
-    argument: python invokes the decorator with the callable as a positional argument when the
-    decorator is invoked raw, i.e. without any arguments
-
-    Subclasses should avoid overriding {__new__}; it should be possible to accomplish everything
-    in {__init__}; please update this note with any exceptions
+    The base for decorators that accept arbitrary optional metadata
     """
 
-
     # public data
-    pyre_factory = None # the callable i decorate
-
+    pyre_method = None  # the method i decorate
 
     # meta-methods
-    def __new__(cls, factory=None, **kwds):
+    def __new__(cls, method=None, **kwds):
         """
-        Trap the invocation with metadata and delay the decoration of the callable
+        Trap the instantiation of the decorator so we can detect how it was invoked
         """
-        # if the {factory} is known, it is because the constructor we build below was invoked
-        if factory is not None:
-            # check that the user gave us something we can decorate
-            assert callable(factory), 'please invoke with keyword arguments'
-            # and chain up to do the normal thing; swallow any extra arguments, but don't
-            # worry, we'll see them again in {__init__}
+        # if the {method} being wrapped is known, i was invoked with no extra arguments
+        if method is not None:
+            # there's nothing special to do here; proceed with building the decorator instance
             return super().__new__(cls)
 
-        # if we don't know the factory, we were invoked with keyword arguments; the strategy
-        # here is to return a {Decorator} constructor as the value of this invocation, which
-        # accomplishes two things: it gives python something to call when the method
-        # declaration is done, and prevents my {__init__} from getting invoked prematurely
+        # if we don't know {method}, we were invoked with keyword arguments; the strategy here is
+        # to return a closure over my constructor as the value of this invocation, which
+        # accomplishes two things: it gives python something to call when the method declaration is
+        # done, and prevents my {__init__} from getting invoked prematurely
 
-        # here is the constructor closure
-        def build(factory):
+        # build the constructor closure
+        def closure(method):
             """
-            Convert a component factory into a foundry
+            Decorate {method}
             """
             # just build one of my instances
-            return cls(factory=factory, **kwds)
+            return cls(method=method, **kwds)
 
-        # to hand over
-        return build
+        # and hand it over
+        return closure
 
-
-    def __init__(self, factory, **kwds):
-        # swallow any extra arguments and chain up
-        super().__init__()
-        # appropriate the factory's docstring
-        self.__doc__ = factory.__doc__
-        # record the callable
-        self.pyre_factory = factory
+    def __init__(self, method, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # record the method
+        self.pyre_method = method
+        # appropriate its metadata; but careful not to destroy the __dict__
+        # not sure whether this is a python bug, yet. gotta find some time to explore
+        functools.update_wrapper(wrapper=self, wrapped=method, updated=())
         # all done
         return
 
+    def __get__(self, instance, cls):
+        """
+        Bind my {__call__} method to {instance}, if available
+        """
+        # if the {instance} is trivial
+        if instance is None:
+            # someone is asking questions of my {cls}; hand me off for inspection
+            return self
+        # otherwise, rebind {self} to {instance} and return it
+        return types.MethodType(self.__call__, instance)
 
     def __call__(self, *args, **kwds):
-        # invoke the factory
-        value = self.pyre_factory(*args, **kwds)
-        # and return the result
-        return value
+        """
+        By default, invoke the wrapped {method}
+        """
+        # invoke the wrapped {method} and return the result
+        return self.pyre_method(*args, **kwds)
 
 
 # end of file
