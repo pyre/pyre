56fe259d184fd774abbb2f55d5a92492777fbf75
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-11-19 22:47:42 -0800

    pyre.filesystem: added support for {s3} discovery that is depth controlled and starts at an arbitrary location within span of the filesystem

--- packages/pyre/filesystem/S3.py
+++ packages/pyre/filesystem/S3.py
@@ -22,6 +22,9 @@ class S3(Filesystem):
     A filesystem built out of the contents of an S3 bucket
     """
 
+    # exceptions
+    from .exceptions import DirectoryListingError
+
     # interface
     def location(self):
         """
@@ -38,14 +41,98 @@ class S3(Filesystem):
         # build the location and return it
         return pyre.primitives.uri(scheme=scheme, authority=authority, address=address)
 
-    def discover(self, prefix=None, **kwds):
+    def discover(self, root=None, levels=0, **kwds):
         """
         Fill my structure with contents that match {prefix}
         """
-        # if the user didn't supply a search prefix
-        if prefix is None:
-            # use mine
-            prefix = self.prefix
+        # establish the starting point
+        root = self if root is None else root
+        # if it's not a folder
+        if not root.isFolder:
+            # complain
+            raise self.DirectoryListingError(uri=root.uri, error="not a directory")
+
+        # get my bucket
+        bucket = self.bucket
+        # make a paginator
+        paginator = self.s3.get_paginator("list_objects_v2")
+        # prime the workload
+        todo = [(root, 0)]
+        # start walking the bucket contents
+        for folder, level in todo:
+            # check whether we are deeper in the bucket than the user limit
+            if levels is not None and level >= levels:
+                # move on
+                continue
+            # compute the actual location of this folder
+            location = self.vnodes[folder].uri
+            # project this location relative to my bucket
+            prefix = location.address.relativeTo(bucket.address)
+            # while the paginator is able to retrieve more contents
+            for page in paginator.paginate(
+                Bucket=bucket.address.name, Prefix=str(prefix) + "/", Delimiter="/"
+            ):
+                # get the items that are stored at this prefix
+                items = [entry["Key"] for entry in page.get("Contents", [])]
+                # and extract the prefixes to the rest of the bucket contents, given our delimiter
+                # these will become folders in the s3 filesystem
+                prefixes = [entry["Prefix"] for entry in page.get("CommonPrefixes", [])]
+                # go through the items
+                for entry in items:
+                    # build nodes for them
+                    node = folder.node()
+                    # assemble their uri
+                    uri = bucket / entry
+                    # connect them to the folder we are visiting
+                    folder[uri.address.name] = node
+                    # build their metadata
+                    meta = node.metadata(uri=uri)
+                    # and add the metadata to my {vnode} table
+                    self.vnodes[node] = meta
+                # go through the prefixes
+                for entry in prefixes:
+                    # these will be folders
+                    node = folder.folder()
+                    # assemble their uri
+                    uri = bucket / entry
+                    # connect them to the folder we are visiting
+                    folder[uri.address.name] = node
+                    # build their metadata
+                    meta = node.metadata(uri=uri)
+                    # and add the metadata to my {vnode} table
+                    self.vnodes[node] = meta
+                    # also, add them to the to-do pile along with a level marker so we can
+                    # visit them to get their contents
+                    todo.append((node, level + 1))
+
+        return self
+
+    # metamethods
+    def __init__(self, root, **kwds):
+        # deconstruct my root and fill out the missing information
+        profile, region, bucket, prefix = self._parse(uri=root)
+        # reassemble my {root} uri
+        root = pyre.primitives.uri(
+            scheme="s3",
+            authority=f"{profile}@{region}",
+            address=bucket.address / prefix,
+        )
+        # build my metadata
+        metadata = self.metadata(uri=root)
+        # and chain up
+        super().__init__(metadata=metadata, **kwds)
+
+        # save my parts
+        # scheme
+        self.scheme = "s3"
+        # authority
+        self.profile = profile
+        self.region = region
+        # the complete uri of my bucket
+        self.bucket = bucket
+        # and the path to my prefix
+        self.prefix = prefix
+
         # attempt to
         try:
             # connect
@@ -62,49 +149,10 @@ class S3(Filesystem):
             # flush
             channel.log()
             # and bail, in case errors aren't fatal
-            return self
+            return
+        # if all goes well, save the s3 connection
+        self.s3 = s3
 
-        # make a paginator
-        paginator = s3.get_paginator("list_objects_v2")
-        # set up its options
-        options = {}
-        # add the bucket
-        options["Bucket"] = self.bucket
-        # if the search prefix is non-trivial
-        if prefix:
-            # add it to the options
-            options["Prefix"] = str(prefix)
-        # go through the pages
-        for page in paginator.paginate(**options):
-            # retrieve the contents
-            contents = page.get("Contents", [])
-            # go through them
-            for entry in contents:
-                # get each key
-                key = entry["Key"]
-                # if the key ends in a slash, make a folder; otherwise, a plain file
-                node = self.folder() if key.endswith("/") else self.node()
-                # and add it to my pile
-                self[key] = node
-
-        # all done
-        return self
-
-    # metamethods
-    def __init__(self, root, **kwds):
-        # build my metadata
-        metadata = self.metadata(uri=pyre.primitives.path())
-        # chain up
-        super().__init__(metadata=metadata, **kwds)
-
-        # deconstruct my root
-        profile, region, bucket, prefix = self._parse(uri=root)
-        # save my parts
-        self.scheme = "s3"
-        self.profile = profile
-        self.region = region
-        self.bucket = bucket
-        self.prefix = prefix
         # all done
         return
 
@@ -126,12 +174,16 @@ class S3(Filesystem):
             region = config.get("region", "")
         # get the address
         address = pyre.primitives.path(uri.address)
-        # the bucket is the root
-        bucket = address[1]
+        # the bucket is the root; convert it into a complete uri
+        bucket = pyre.primitives.uri(
+            scheme=uri.scheme,
+            authority=f"{profile}@{region}",
+            address=pyre.primitives.path(address[:2]),
+        )
         # and the rest is the prefix path
         prefix = address[2:]
         # if it's non-trivial, turn it into a path
-        prefix = pyre.primitives.path(prefix) if prefix else None
+        prefix = pyre.primitives.path(prefix) if prefix else pyre.primitives.path.root
         # all done
         return profile, region, bucket, prefix
 
