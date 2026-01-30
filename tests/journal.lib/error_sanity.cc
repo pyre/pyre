b62bb7c8257db8826fbe100af08d912c7b30acc6
Brad Aagaard <baagaard@usgs.gov>
2026-01-30 10:06:42 -0700

    Update journal C++ sanity tests to include detail and dent option arguments.

--- tests/journal.lib/error_sanity.cc
+++ tests/journal.lib/error_sanity.cc
@@ -23,10 +23,30 @@ main()
 
     // check its name
     assert(channel.name() == "tests.journal.error");
+    // by default, detail should be 1
+    assert(channel.detail() == 1);
+    // by default, dent should be 0
+    assert(channel.dent() == 0);
     // by default, it should be active
     assert(channel);
     // and fatal
     assert(channel.fatal());
+
+    // make an error channel with detail and dent
+    const pyre::journal::detail_t detail = 2;
+    const pyre::journal::dent_t dent = 1;
+    pyre::journal::error_t channel2("tests.journal.error2", detail, dent);
+
+    // check its name
+    assert(channel2.name() == "tests.journal.error2");
+    // detail should match specified value
+    assert(channel2.detail() == detail);
+    // dent should match specified value
+    assert(channel2.dent() == dent);
+    // by default, it should be active
+    assert(channel2);
+    // and fatal
+    assert(channel2.fatal());
 
     // all done
     return 0;
