9cfa5e88074a309a01fdde44508428eadfd94027
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-02-08 15:54:46 +0200

    pyre.nexus: replaced {application} by {app} in the {server} signatures; noise by {black}

--- packages/pyre/nexus/Service.py
+++ packages/pyre/nexus/Service.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2026 all rights reserved
-#
 
 
 # externals
@@ -16,7 +14,6 @@ class Service(pyre.protocol, family="pyre.services"):
     Protocol definition for components that handle events in communication channels
     """
 
-
     # default implementation
     @classmethod
     def pyre_default(cls, **kwds):
@@ -26,33 +23,32 @@ class Service(pyre.protocol, family="pyre.services"):
         # no opinions just yet
         return None
 
-
     # behaviors
     @pyre.provides
-    def activate(self, application, dispatcher):
+    def activate(self, app, dispatcher):
         """
         Prepare to start receiving information from the network
         """
 
-    @pyre.provides(tip='acknowledge a peer that has initiated a connection')
+    @pyre.provides(tip="acknowledge a peer that has initiated a connection")
     def acknowledge(self, dispatcher, channel):
         """
         A peer has attempted to establish a connection
         """
 
-    @pyre.provides(tip='determine whether to start a conversation with the peer')
+    @pyre.provides(tip="determine whether to start a conversation with the peer")
     def validate(self, channel, address):
         """
         Examine the peer {address} and determine whether to continue the conversation
         """
 
-    @pyre.provides(tip='indicate interest in continuing to interact with the peer')
+    @pyre.provides(tip="indicate interest in continuing to interact with the peer")
     def connect(self, dispatcher, channel, address):
         """
         Prepare to start accepting requests from a new peer
         """
 
-    @pyre.provides(tip='try to understand and respond to the peer request')
+    @pyre.provides(tip="try to understand and respond to the peer request")
     def process(self, dispatcher, channel):
         """
         Start or continue a conversation with a peer over {channel}
