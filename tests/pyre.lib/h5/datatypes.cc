9dcc4245e78b482950da19fa253930b29d96a164
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-04-21 14:34:47 +0300

    pyre::h5: the {datatypes} test driver now exercises only the types with explicitly known bit width

--- tests/pyre.lib/h5/datatypes.cc
+++ tests/pyre.lib/h5/datatypes.cc
@@ -15,20 +15,21 @@ int
 main()
 {
     // instantiate the native types that are supported by {hdf5} out of the box
-    auto charTp = pyre::h5::datatype<char>();
-    auto shortTp = pyre::h5::datatype<short>();
-    auto intTp = pyre::h5::datatype<int>();
-    auto longTp = pyre::h5::datatype<long>();
-    auto floatTp = pyre::h5::datatype<float>();
-    auto doubleTp = pyre::h5::datatype<double>();
+    // unsigned
+    assert((pyre::h5::datatype<std::uint8_t>() == H5::PredType::NATIVE_UINT8));
+    assert((pyre::h5::datatype<std::uint16_t>() == H5::PredType::NATIVE_UINT16));
+    assert((pyre::h5::datatype<std::uint32_t>() == H5::PredType::NATIVE_UINT32));
+    assert((pyre::h5::datatype<std::uint64_t>() == H5::PredType::NATIVE_UINT64));
 
-    // check them against the {H5} singletons
-    assert((charTp == H5::PredType::NATIVE_CHAR));
-    assert((shortTp == H5::PredType::NATIVE_SHORT));
-    assert((intTp == H5::PredType::NATIVE_INT));
-    assert((longTp == H5::PredType::NATIVE_LONG));
-    assert((floatTp == H5::PredType::NATIVE_FLOAT));
-    assert((doubleTp == H5::PredType::NATIVE_DOUBLE));
+    // signed
+    assert((pyre::h5::datatype<std::int8_t>() == H5::PredType::NATIVE_INT8));
+    assert((pyre::h5::datatype<std::int16_t>() == H5::PredType::NATIVE_INT16));
+    assert((pyre::h5::datatype<std::int32_t>() == H5::PredType::NATIVE_INT32));
+    assert((pyre::h5::datatype<std::int64_t>() == H5::PredType::NATIVE_INT64));
+
+    // floating point
+    assert((pyre::h5::datatype<float>() == H5::PredType::NATIVE_FLOAT));
+    assert((pyre::h5::datatype<double>() == H5::PredType::NATIVE_DOUBLE));
 
     // all done
     return 0;
