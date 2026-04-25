8e9cf4d7a13ef6582b91431f7e073dbeb8335c7f
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-04-25 12:58:31 +0300

    mm: added {projects.mm} to dictate the build order

---
+++ .mm/projects.mm
@@ -0,0 +1,10 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+# establish the build order
+projects := pyre journal merlin pyre-mpi pyre-gsl pyre-cuda
+
+# end of file
+
