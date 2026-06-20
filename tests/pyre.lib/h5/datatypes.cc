18f5935204b5c437b40fd632c0f16b7d869a1802
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-06-20 22:03:12 +0300

    tests/pyre.lib/h5: compare deduced datatypes with H5Tequal, not the dropped H5:: api

--- tests/pyre.lib/h5/datatypes.cc
+++ tests/pyre.lib/h5/datatypes.cc
@@ -10,26 +10,27 @@
 #include <pyre/h5.h>
 
 
-// check we can build the native datatypes
+// check that {datatype<T>()} deduces the correct native hdf5 datatype
 int
 main()
 {
-    // instantiate the native types that are supported by {hdf5} out of the box
+    // each deduced type is a pyre-owned copy of a native hdf5 type; {H5Tequal} confirms it
+    // describes the same type as the matching {H5T_NATIVE_*} constant
     // unsigned
-    assert((pyre::h5::datatype<std::uint8_t>() == H5::PredType::NATIVE_UINT8));
-    assert((pyre::h5::datatype<std::uint16_t>() == H5::PredType::NATIVE_UINT16));
-    assert((pyre::h5::datatype<std::uint32_t>() == H5::PredType::NATIVE_UINT32));
-    assert((pyre::h5::datatype<std::uint64_t>() == H5::PredType::NATIVE_UINT64));
+    assert((H5Tequal(pyre::h5::datatype<std::uint8_t>().id(), H5T_NATIVE_UINT8) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::uint16_t>().id(), H5T_NATIVE_UINT16) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::uint32_t>().id(), H5T_NATIVE_UINT32) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::uint64_t>().id(), H5T_NATIVE_UINT64) > 0));
 
     // signed
-    assert((pyre::h5::datatype<std::int8_t>() == H5::PredType::NATIVE_INT8));
-    assert((pyre::h5::datatype<std::int16_t>() == H5::PredType::NATIVE_INT16));
-    assert((pyre::h5::datatype<std::int32_t>() == H5::PredType::NATIVE_INT32));
-    assert((pyre::h5::datatype<std::int64_t>() == H5::PredType::NATIVE_INT64));
+    assert((H5Tequal(pyre::h5::datatype<std::int8_t>().id(), H5T_NATIVE_INT8) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::int16_t>().id(), H5T_NATIVE_INT16) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::int32_t>().id(), H5T_NATIVE_INT32) > 0));
+    assert((H5Tequal(pyre::h5::datatype<std::int64_t>().id(), H5T_NATIVE_INT64) > 0));
 
     // floating point
-    assert((pyre::h5::datatype<float>() == H5::PredType::NATIVE_FLOAT));
-    assert((pyre::h5::datatype<double>() == H5::PredType::NATIVE_DOUBLE));
+    assert((H5Tequal(pyre::h5::datatype<float>().id(), H5T_NATIVE_FLOAT) > 0));
+    assert((H5Tequal(pyre::h5::datatype<double>().id(), H5T_NATIVE_DOUBLE) > 0));
 
     // all done
     return 0;
