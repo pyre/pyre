f85d9b24e5dce3f84eba687b03e4a746af804610
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-04 10:28:47 +0300

    build: parse the {merlin} project ahead of {pyre}

--- .mm/projects.mm
+++ .mm/projects.mm
@@ -3,8 +3,9 @@
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2026 all rights reserved
 
-# establish the build order
-projects := pyre journal merlin pyre-mpi pyre-gsl pyre-cuda
+# establish the build order; {merlin} leads so it stages the {lib/mm} portinfo headers
+# into {include/mm} before any project with C++ sources compiles against them
+projects := merlin pyre journal pyre-mpi pyre-gsl pyre-cuda
 
 # end of file
 
