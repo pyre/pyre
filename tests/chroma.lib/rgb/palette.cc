2b2a081ef133b1c3d9a0217893cc06d30155dcd4
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-05 19:28:14 +0300

    chroma: add color-name palette generated from rgb.txt

---
+++ tests/chroma.lib/rgb/palette.cc
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
+// support
+#include <cassert>
+// get the color palette; it is an opt-in header, not part of {pyre/chroma.h}
+#include <pyre/chroma/rgb/palette.h>
+
+
+// bring the color type into scope
+using rgb_t = pyre::chroma::rgb_t;
+
+
+// verify that the palette resolves canonical names to their exact triplets
+int
+main(int argc, char * argv[])
+{
+    // the achromatic extremes
+    assert((pyre::chroma::rgb::palette::find("black") == rgb_t { 0, 0, 0 }));
+    assert((pyre::chroma::rgb::palette::find("white") == rgb_t { 1, 1, 1 }));
+
+    // the primaries, each of which wins its triplet over later aliases
+    assert((pyre::chroma::rgb::palette::find("red") == rgb_t { 1, 0, 0 }));
+    assert((pyre::chroma::rgb::palette::find("green") == rgb_t { 0, 1, 0 }));
+    assert((pyre::chroma::rgb::palette::find("blue") == rgb_t { 0, 0, 1 }));
+
+    // a name that is not in the table reports a miss
+    assert(!pyre::chroma::rgb::palette::find("not-a-color"));
+
+    // all done
+    return 0;
+}
+
+
+// end of file
