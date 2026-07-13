c2883f751212a0946f318c11d8c0313e7386c2c2
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-13 07:48:07 +0300

    docker: add the conda-clang and conda-gcc cells

---
+++ etc/docker/conda-gcc/config.mm
@@ -0,0 +1,11 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# nothing to configure here: with {pkgdb: conda} every external dependency is resolved from the
+# active conda environment; leave this file as the place for any local overrides
+
+
+# end of file
