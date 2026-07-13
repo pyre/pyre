269235a3a2eeab8ed1f9e5d5eccae367c1125a6e
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-13 06:47:09 +0300

    docker: add the lts-clang test and dev container

---
+++ etc/docker/lts-clang/config.mm
@@ -0,0 +1,11 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# nothing to configure here: with {pkgdb: dpkg} every external dependency is discovered
+# from the installed ubuntu packages; leave this file as the place for any local overrides
+
+
+# end of file
