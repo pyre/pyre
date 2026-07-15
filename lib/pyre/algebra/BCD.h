13c1ccbecc3d747b2a219bb3eecda24d1c6ec497
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-15 14:36:11 +0300

    pyre::algebra: add self_type alias to BCD

--- lib/pyre/algebra/BCD.h
+++ lib/pyre/algebra/BCD.h
@@ -11,29 +11,36 @@
 #include <cstdlib>
 
 namespace pyre::algebra {
-    template <size_t scale, typename precision_t = size_t>
+    template <size_t scale, typename precisionT = size_t>
     class BCD;
 } // namespace pyre::algebra
 
 
 // global arithmetic operators
 // binary +
-template <size_t scale, typename precision_t>
-pyre::algebra::BCD<scale, precision_t>
+template <size_t scale, typename precisionT>
+pyre::algebra::BCD<scale, precisionT>
 operator+(
-    const pyre::algebra::BCD<scale, precision_t> &, const pyre::algebra::BCD<scale, precision_t> &);
+    const pyre::algebra::BCD<scale, precisionT> &, const pyre::algebra::BCD<scale, precisionT> &);
 
 
 // binary -
-template <size_t scale, typename precision_t>
-pyre::algebra::BCD<scale, precision_t>
+template <size_t scale, typename precisionT>
+pyre::algebra::BCD<scale, precisionT>
 operator-(
-    const pyre::algebra::BCD<scale, precision_t> &, const pyre::algebra::BCD<scale, precision_t> &);
+    const pyre::algebra::BCD<scale, precisionT> &, const pyre::algebra::BCD<scale, precisionT> &);
 
 
 // the BCD class
-template <size_t scale, typename precision_t>
+template <size_t scale, typename precisionT>
 class pyre::algebra::BCD {
+    // type aliases
+public:
+    // me
+    using self_type = BCD<scale, precisionT>;
+    // my parts
+    using precision_type = precisionT;
+
     // interface
 public:
     // convert to double
@@ -42,22 +49,26 @@ public:
     // arithmetic
     BCD operator+() const;
     BCD operator-() const;
-
     BCD & operator+=(const BCD &);
     BCD & operator-=(const BCD &);
 
     // meta methods
 public:
-    inline ~BCD();
+    // destructor
+    ~BCD() = default;
 
-    inline BCD(precision_t msw = 0, precision_t lsw = 0);
-    BCD(const BCD &);
-    const BCD & operator=(const BCD &);
+    // constructor
+    inline BCD(precision_type msw = 0, precision_type lsw = 0);
+
+    inline BCD(const BCD &) = default;
+    inline self_type & operator=(const BCD &) = default;
+    inline BCD(BCD &&) = default;
+    inline self_type & operator=(BCD &&) = default;
 
     // data members
 public:
-    precision_t _msw;
-    precision_t _lsw;
+    precision_type _msw;
+    precision_type _lsw;
 
     static const size_t _scale = scale;
 };
