35c5161fb1e34bf46b27402f4adfaa3baf170bd7
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-17 10:16:48 +0300

    pyre::memory: added test drivers for the buffer bindings

---
+++ tests/pyre.ext/memory/cells.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# support
+import itertools
+
+
+# the drivers
+def intCells() -> None:
+    """
+    Sanity test: make sure the bindings for integral type cells are present
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # integral types
+    signs = ["", "U"]
+    sizes = [8, 16, 32, 64]
+
+    # build the integral type names
+    for perm, sign, size in itertools.product(const, signs, sizes):
+        # assemble the name
+        name = f"{sign}Int{size}{perm}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name of the class
+        assert name == cell.__name__
+        # check the size, in bits
+        assert size == cell.bits
+        # check the size, in bytes
+        assert size == 8 * cell.bytes
+        # check the access rights
+        assert perm == ("" if cell.mutable else "Const")
+
+    # all done
+    return
+
+
+def floatCells() -> None:
+    """
+    Sanity test: make sure the bindings for float type cells are present
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # float types
+    sizes = [("Float", 32), ("Double", 64)]
+
+    # build the float type names
+    for perm, (marker, size) in itertools.product(const, sizes):
+        # assemble the name
+        name = f"{marker}{perm}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name of the class
+        assert name == cell.__name__
+        # check the size, in bits
+        assert size == cell.bits
+        # check the size, in bytes
+        assert size == 8 * cell.bytes
+        # check the access rights
+        assert perm == ("" if cell.mutable else "Const")
+
+    # all done
+    return
+
+
+def complexCells() -> None:
+    """
+    Sanity test: make sure the bindings for complex type cells are present
+    """
+    # access the memory bindings
+    from pyre.extensions.pyre.memory import cells
+
+    # access rights
+    const = ["", "Const"]
+    # complex types
+    sizes = [("ComplexFloat", 64), ("ComplexDouble", 128)]
+
+    # build the complex type names
+    for perm, (marker, size) in itertools.product(const, sizes):
+        # assemble the name
+        name = f"{marker}{perm}"
+        # verify that the bindings exist
+        cell = getattr(cells, name)
+        # check the name of the class
+        assert name == cell.__name__
+        # check the size, in bits
+        assert size == cell.bits
+        # check the size, in bytes
+        assert size == 8 * cell.bytes
+        # check the access rights
+        assert perm == ("" if cell.mutable else "Const")
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the tests
+    intCells()
+    floatCells()
+    complexCells()
+
+
+# end of file
---
+++ tests/pyre.ext/memory/maps.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# support
+import itertools
+
+
+# the driver
+def maps():
+    """
+    Verify that the map expansions are all present
+    """
+    # get the module
+    from pyre.extensions.pyre.memory import maps
+
+    # given access rights
+    permissions = ["", "Const"]
+    # and basic types
+    types = [
+        # ints
+        "Int8",
+        "Int16",
+        "Int32",
+        "Int64",
+        "UInt8",
+        "UInt16",
+        "UInt32",
+        "UInt64",
+        # floats
+        "Float",
+        "Double",
+        # complex
+        "ComplexFloat",
+        "ComplexDouble",
+    ]
+    # build all possible names
+    names = [f"Map{type}{permission}" for permission, type in itertools.product(permissions, types)]
+    # go through them
+    for name in names:
+        # and verify they are all present
+        assert getattr(maps, name)
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    maps()
+
+
+# end of file
