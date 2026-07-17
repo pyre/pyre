edaa936804e510088d39305dcaa21d73fd74b4c5
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-17 07:57:51 +0300

    pyre::typelists: corrected the preamble

--- lib/pyre/typelists/prepend.h
+++ lib/pyre/typelists/prepend.h
@@ -1,4 +1,8 @@
-// -*- c++ -*-
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
 
 // code guard
 #pragma once
