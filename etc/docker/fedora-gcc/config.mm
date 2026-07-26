a3e256b7d627bbe98d2ecfb140f5443a7d25e07f
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-26 11:30:05 +0300

    etc/docker/fedora-gcc: discover the externals through the rpm backend

--- etc/docker/fedora-gcc/config.mm
+++ etc/docker/fedora-gcc/config.mm
@@ -4,30 +4,8 @@
 # (c) 1998-2026 all rights reserved
 
 
-# with {pkgdb: adhoc} the externals are declared here; fedora puts everything in the system
-# prefix with libraries under {lib64}, and openmpi in its own home activated by environment
-# modules that this cell bypasses
-
-# gsl
-gsl.dir := /usr
-gsl.libpath := /usr/lib64
-
-# hdf5: the serial flavor
-hdf5.dir := /usr
-hdf5.libpath := /usr/lib64
-
-# python
-python.dir := /usr
-python.libpath := /usr/lib64
-
-# pybind11 is header only
-pybind11.dir := /usr
-
-# the flavor logic gates the library list on the version
-mpi.version := 5.0
-# openmpi lives in its own home
-mpi.dir := /usr/lib64/openmpi
-mpi.flavor := openmpi
+# nothing to configure here: with {pkgdb: rpm} every external dependency is discovered
+# from the installed fedora packages; leave this file as the place for any local overrides
 
 
 # end of file
