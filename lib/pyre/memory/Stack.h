10b45edb188fddab1783f3a31d5eb3ff0aabf081
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-07-29 14:13:04 +0300

    pyre::memory: drop the {WITH_CXX20} guard; defaulted destructors are implicitly constexpr

--- lib/pyre/memory/Stack.h
+++ lib/pyre/memory/Stack.h
@@ -95,11 +95,7 @@ private:
     // default metamethods
 public:
     // destructor
-#ifdef WITH_CXX20
-    constexpr ~Stack() = default;
-#else
     ~Stack() = default;
-#endif
     // constructors
     Stack(const Stack &) = default;
     Stack(Stack &&) = default;
