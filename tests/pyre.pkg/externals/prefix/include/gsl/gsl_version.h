9871bb4f0009407748b51728484e238031c3d040
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-09-26 12:08:54 +0300

    tests: the canned external headers carry the preamble and closing marker of every other source

--- tests/pyre.pkg/externals/prefix/include/gsl/gsl_version.h
+++ tests/pyre.pkg/externals/prefix/include/gsl/gsl_version.h
@@ -1,2 +1,12 @@
+// -*- C -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
 /* canned gsl version header */
 #define GSL_VERSION "2.8"
+
+
+// end of file
---
+++ tests/pyre.pkg/externals/prefix/include/vampich/mpi.h
@@ -0,0 +1,8 @@
+// -*- C -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
+// end of file
