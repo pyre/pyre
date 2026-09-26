614cef9c8c1884c4dcd701b5ee2497f1db24b131
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-09-26 12:08:54 +0300

    tests: the canned external headers carry the preamble and closing marker of every other source

--- tests/pyre.pkg/externals/prefix/include/hdf5/mpich/H5pubconf.h
+++ tests/pyre.pkg/externals/prefix/include/hdf5/mpich/H5pubconf.h
@@ -1,3 +1,13 @@
+// -*- C -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
 /* canned mpich hdf5 build configuration */
 #define H5_HAVE_PARALLEL 1
 #define H5_VERSION "1.14.5"
+
+
+// end of file
--- tests/pyre.pkg/externals/prefix/include/hdf5/serial/H5pubconf.h
+++ tests/pyre.pkg/externals/prefix/include/hdf5/serial/H5pubconf.h
@@ -1,2 +1,12 @@
+// -*- C -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
 /* canned serial hdf5 build configuration */
 #define H5_VERSION "1.14.5"
+
+
+// end of file
